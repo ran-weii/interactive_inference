ca59f17175f5143f015d0fc63d2c9cf316b85a95
rw422 <rw422@tamu.edu>
2022-01-12 15:50:47 -0600

    add kalman data subsampling, vel grad, kf init params

--- scripts/train_kalman.py
+++ scripts/train_kalman.py
@@ -1,9 +1,14 @@
 import argparse
 import os
 import glob
-from typing_extensions import ParamSpec
+import time
 import numpy as np
 import pandas as pd
+from pykalman import KalmanFilter
+
+# set pandas display option
+pd.set_option('display.max_columns', None)
+pd.options.mode.chained_assignment = None
 
 """ TODO
 write tests:
@@ -21,6 +26,7 @@ def parse_args():
     parser.add_argument("--num_files", type=int, default=3, help="number of files used to sample trajectories, default=3")
     parser.add_argument("--min_len", type=int, default=100, help="min track length, default=100")
     parser.add_argument("--num_tracks", type=int, default=50, help="number of tracks used to train kalman filter, default=50")
+    parser.add_argument("--epochs", type=int, default=1, help="number of em epochs, default=1")
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--debug", type=bool_, default=True)
     arglist = parser.parse_args()
@@ -42,9 +48,8 @@ def main(arglist):
     )
     track_paths = [track_paths[i] for i in track_id]
     
-    
     # load data
-    df_tracks = []
+    df_all_tracks = []
     num_cars = 0
     for i, track_path in enumerate(track_paths):
         df_track = pd.read_csv(track_path)
@@ -53,34 +58,83 @@ def main(arglist):
         df_track["track_id"] = pd.Categorical(df_track["track_id"]).codes
         df_track["track_id"] += num_cars
         
-        df_tracks.append(df_track)
+        df_all_tracks.append(df_track)
         num_cars += len(np.unique(df_track["track_id"]))
         
-    df_tracks = pd.concat(df_tracks, axis=0)
+    df_all_tracks = pd.concat(df_all_tracks, axis=0)
     
     # subsample tracks
-    df_tracks = df_tracks.groupby("track_id").filter(lambda x: x.shape[0] >= arglist.min_len)
+    df_all_tracks = df_all_tracks.groupby("track_id").filter(lambda x: x.shape[0] >= arglist.min_len)
     
-    assert len(df_tracks["track_id"].unique()) > arglist.num_tracks
+    assert len(df_all_tracks["track_id"].unique()) > arglist.num_tracks
     id = np.random.choice(
-        len(df_tracks["track_id"].unique()), 
+        len(df_all_tracks["track_id"].unique()), 
         size=(arglist.num_tracks,), replace=False
     )
-    track_id = df_tracks["track_id"].unique()[id]
+    track_id = df_all_tracks["track_id"].unique()[id]
     
-    df_tracks = df_tracks.loc[df_tracks["track_id"].isin(track_id)].reset_index(drop=True)
+    df_all_tracks = df_all_tracks.loc[df_all_tracks["track_id"].isin(track_id)].reset_index(drop=True)
     
-    df_tracks = df_tracks.groupby("track_id").head(arglist.min_len).reset_index(drop=True)
-    
+    df_all_tracks = df_all_tracks.groupby("track_id").head(arglist.min_len).reset_index(drop=True)
+
     # derive acceleration
-    dt = df_track.groupby("track_id")["timestamp_ms"].diff().mean()
-    f_grad = lambda x: pd.DataFrame(np.gradient(x, dt), index=x.index) * 1000
-    df_track["vx_grad"] = df_track.groupby("track_id")["vx"].apply(f_grad)
-    df_track["vy_grad"] = df_track.groupby("track_id")["vy"].apply(f_grad)
+    dt = df_track.groupby("track_id")["timestamp_ms"].diff().mean() / 1000
+    f_grad = lambda x: pd.DataFrame(np.gradient(x, dt), index=x.index)
+    df_all_tracks["vx_grad"] = df_all_tracks.groupby("track_id")["vx"].apply(f_grad)
+    df_all_tracks["vy_grad"] = df_all_tracks.groupby("track_id")["vy"].apply(f_grad)
     
-    # init parameters
+    # normalize data
+    f_norm = lambda x: x - x.iloc[0]
+    df_all_tracks["x"] = df_all_tracks.groupby("track_id")["x"].apply(f_norm)
+    df_all_tracks["y"] = df_all_tracks.groupby("track_id")["y"].apply(f_norm)
+    
+    # pack observations
+    obs = df_all_tracks[["x", "y", "vx", "vy", "vx_grad", "vy_grad"]]
+    obs = obs.values
+    
+    # init parameters with const acc model
+    eps = 1e-4
+    transition_matrix = np.array(
+        [[1, 0, dt,  0,  0,  0],
+         [0, 1,  0, dt,  0,  0],
+         [0, 0,  1,  0, dt,  0],
+         [0, 0,  0,  1,  0, dt],
+         [0, 0,  0,  0,  1,  0],
+         [0, 0,  0,  0,  0,  1]]
+    ) + eps
+    observation_matrix = np.eye(6) + eps
+    transition_covariance = 0.01 * np.eye(6) + eps
+    observation_covariance = 0.01 * np.eye(6) + eps
+    transition_offset = np.zeros((6)) + eps
+    observation_offset = np.zeros((6)) + eps
+    initial_state_mean = np.zeros((6,))
+    initial_state_covariance = 0.01 * np.eye(6) + eps
     
     # train
+    # kf = KalmanFilter(
+    #     transition_matrix,
+    #     observation_matrix,
+    #     transition_covariance,
+    #     observation_covariance,
+    #     transition_offset,
+    #     observation_offset,
+    #     initial_state_mean,
+    #     initial_state_covariance,
+    #     random_state=arglist.seed,
+    #     em_vars=[
+    #     "transition_matrices", "observation_matrices",
+    #     "transition_covariance", "observation_covariance",
+    #     "transition_offsets", "observation_offsets", 
+    #     "initial_state_mean", "initial_state_covariance"
+    #     ]
+    # )
+    
+    # loss = []
+    # for i in range(arglist.epochs):
+    #     kf = kf.em(X=obs, n_iter=1)    
+    #     loss.append(kf.loglikelihood(obs))
+    
+    # print(loss)
     return 
 
 if __name__ == "__main__":
