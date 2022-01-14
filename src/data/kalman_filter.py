5356f5b9e0bb6d945f629e1dac3a6bbfa0750af0
rw422 <rw422@tamu.edu>
2022-01-13 20:50:19 -0600

    delete redundant batch functions from kalman filter

--- src/data/kalman_filter.py
+++ src/data/kalman_filter.py
@@ -2,73 +2,6 @@ import numpy as np
 from pykalman import KalmanFilter
 from pykalman.standard import _filter, _smooth, _smooth_pair, _em
 
-def _batch_filter(transition_matrices, observation_matrices, transition_covariance,
-            observation_covariance, transition_offsets, observation_offsets,
-            initial_state_mean, initial_state_covariance, observations):
-    
-    predicted_state_means = []
-    predicted_state_covariances = []
-    kalman_gains = []
-    filtered_state_means = []
-    filtered_state_covariances = []
-    for obs in observations:
-        out = _filter(transition_matrices, observation_matrices, transition_covariance,
-            observation_covariance, transition_offsets, observation_offsets,
-            initial_state_mean, initial_state_covariance, obs)
-        
-        predicted_state_means.append(out[0])
-        predicted_state_covariances.append(out[1])
-        kalman_gains.append(out[2])
-        filtered_state_means.append(out[3])
-        filtered_state_covariances.append(out[4])
-        
-    predicted_state_means = np.stack(predicted_state_means)
-    predicted_state_covariances = np.stack(predicted_state_covariances)
-    kalman_gains = np.stack(kalman_gains)
-    filtered_state_means = np.stack(filtered_state_means)
-    filtered_state_covariances = np.stack(filtered_state_covariances)
-    return (predicted_state_means, predicted_state_covariances,
-            kalman_gains, filtered_state_means,
-            filtered_state_covariances)
-
-def _batch_smooth(transition_matrices, filtered_state_means,
-            filtered_state_covariances, predicted_state_means,
-            predicted_state_covariances):
-    batch_size = len(filtered_state_means)
-    
-    smoothed_state_means = []
-    smoothed_state_covariances = []
-    kalman_smoothing_gains = []
-    for i in range(batch_size):
-        out = _smooth(
-                transition_matrices, filtered_state_means[i],
-                filtered_state_covariances[i], predicted_state_means[i],
-                predicted_state_covariances[i]
-            )
-        
-        smoothed_state_means.append(out[0])
-        smoothed_state_covariances.append(out[1])
-        kalman_smoothing_gains.append(out[2])
-        
-    smoothed_state_means = np.stack(smoothed_state_means)
-    smoothed_state_covariances = np.stack(smoothed_state_covariances)
-    kalman_smoothing_gains = np.stack(kalman_smoothing_gains)
-    return (smoothed_state_means, smoothed_state_covariances,
-            kalman_smoothing_gains)
-
-def _batch_smooth_pair(smoothed_state_covariances, kalman_smoothing_gain):
-    batch_size = len(smoothed_state_covariances)
-    
-    pairwise_covariances = []
-    for i in range(batch_size):
-        out = _smooth_pair(
-            smoothed_state_covariances[i], kalman_smoothing_gain[i]
-        )
-        pairwise_covariances.append(out)
-        
-    pairwise_covariances = np.stack(pairwise_covariances)
-    return pairwise_covariances
-
 class BatchKalmanFilter(KalmanFilter):
     def __init__(self, transition_matrices=None, observation_matrices=None,
             transition_covariance=None, observation_covariance=None,
@@ -85,17 +18,6 @@ class BatchKalmanFilter(KalmanFilter):
             random_state,
             em_vars,
             n_dim_state, n_dim_obs)
-        
-    def batch_filter(self, X):
-        filtered_state_means = []
-        filtered_state_covariances = []
-        for _, x in enumerate(X):
-            means, covs = self.filter(x)
-            filtered_state_means.append(means)
-            filtered_state_covariances.append(covs)
-        filtered_state_means = np.stack(filtered_state_means)
-        filtered_state_covariances = np.stack(filtered_state_covariances)
-        return (filtered_state_means, filtered_state_covariances)
     
     def batch_em(self, X, n_iter=1, em_vars=None):
         """ Apply the EM algorithm in batch
