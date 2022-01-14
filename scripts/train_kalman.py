870b8737b16af40cb65967610a06e096885032a8
rw422 <rw422@tamu.edu>
2022-01-13 21:15:52 -0600

    implement batch em to train kalman filter

--- scripts/train_kalman.py
+++ scripts/train_kalman.py
@@ -1,21 +1,16 @@
 import argparse
 import os
 import glob
-import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from pykalman import KalmanFilter
+import seaborn as sns
+from src.data.kalman_filter import BatchKalmanFilter
 
 # set pandas display option
 pd.set_option('display.max_columns', None)
 pd.options.mode.chained_assignment = None
 
-""" TODO
-write tests:
-check how original repo handle tracks to render
-train kalman filter
-"""
 def parse_args():
     bool_ = lambda x: x if isinstance(x, bool) else x == "True"
     
@@ -26,25 +21,80 @@ def parse_args():
     parser.add_argument("--scenario", type=str, default="DR_CHN_Merging_ZS")
     parser.add_argument("--num_files", type=int, default=3, help="number of files used to sample trajectories, default=3")
     parser.add_argument("--min_len", type=int, default=100, help="min track length, default=100")
-    parser.add_argument("--num_tracks", type=int, default=50, help="number of tracks used to train kalman filter, default=50")
-    parser.add_argument("--epochs", type=int, default=20, help="number of em epochs, default=1")
+    parser.add_argument("--num_train", type=int, default=50, help="number of tracks used to train kalman filter, default=50")
+    parser.add_argument("--num_test", type=int, default=10, help="number of tracks used to test kalman filter, default=10")
+    parser.add_argument("--epochs", type=int, default=10, help="number of em epochs, default=25")
     parser.add_argument("--plot_history", type=bool_, default=True, help="plot em learning curve, default=True")
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--debug", type=bool_, default=True)
     arglist = parser.parse_args()
     return arglist
 
 def derive_acc(df, dt):
+    """ differentiate vel """
     f_grad = lambda x: pd.DataFrame(np.gradient(x, dt), index=x.index)
     df["vx_grad"] = df.groupby("track_id")["vx"].apply(f_grad)
     df["vy_grad"] = df.groupby("track_id")["vy"].apply(f_grad)
     return df
 
 def normalize_pos(df):
+    """ subtrack all pos by initial pos """
     f_norm = lambda x: x - x.iloc[0]
     df["x"] = df.groupby("track_id")["x"].apply(f_norm)
     df["y"] = df.groupby("track_id")["y"].apply(f_norm)
     return df
+
+def prepare_data(df_all_tracks, dt, num_train, num_test, max_len):
+    # random sample train and test sequences
+    train_id = np.random.choice(
+        len(df_all_tracks["track_id"].unique()),
+        size=(num_train + num_test,), replace=False
+    )
+    df_train = df_all_tracks.loc[
+        df_all_tracks["track_id"].isin(train_id[:num_train])
+    ].reset_index(drop=True)
+    df_test = df_all_tracks.loc[
+        df_all_tracks["track_id"].isin(train_id[num_train:])
+    ].reset_index(drop=True)
+    
+    df_train = df_train.groupby("track_id").head(max_len)
+    df_test = df_test.groupby("track_id").head(max_len)
+        
+    # derive acceleration
+    df_train = derive_acc(df_train, dt)
+    df_test = derive_acc(df_test, dt)
+    
+    # normalize data
+    df_train = normalize_pos(df_train)
+    df_test = normalize_pos(df_test)
+    return df_train, df_test
+
+def plot_kf_params(kf):
+    fig, ax = plt.subplots(3, 2, figsize=(10, 6))
+    sns.heatmap(kf.transition_matrices, annot=True, cbar=False, ax=ax[0,0])
+    sns.heatmap(kf.transition_covariance, annot=True, cbar=False, ax=ax[0,1])
+    sns.heatmap(kf.observation_matrices, annot=True, cbar=False, ax=ax[1,0])
+    sns.heatmap(kf.observation_covariance, annot=True, cbar=False, ax=ax[1,1])
+    sns.heatmap(kf.initial_state_mean.reshape(-1, 1), annot=True, cbar=False, ax=ax[2,0])
+    sns.heatmap(kf.initial_state_covariance, annot=True, cbar=False, ax=ax[2,1])
+    
+    ax[0,0].set_title("transition_matrix")
+    ax[0,1].set_title("transition_cov")
+    ax[1,0].set_title("observation_matrix")
+    ax[1,1].set_title("observation_cov")
+    ax[2,0].set_title("init_state_matrix")
+    ax[2,1].set_title("init_state_cov")
+    
+    plt.tight_layout()
+    return fig
+    
+def plot_history(df_history):
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    ax.plot(df_history["epoch"], df_history["train_loss"], "-o", label="train_loss")
+    ax.plot(df_history["epoch"], df_history["test_loss"], "-o", label="test_loss")
+    ax.set_xlabel("epoch")
+    ax.legend()
+    ax.grid()
+    return fig
 
 def main(arglist):
     np.random.seed(arglist.seed)
@@ -83,49 +133,52 @@ def main(arglist):
     df_all_tracks = pd.concat(df_all_tracks, axis=0)
     dt = df_all_tracks.groupby("track_id")["timestamp_ms"].diff().mean() / 1000
     
-    # get train and test sequences
-    track_duration = df_all_tracks.groupby("track_id").size().values
-    id_sort = np.argsort(track_duration)[::-1]
+    df_train, df_test = prepare_data(
+        df_all_tracks, 
+        dt, 
+        arglist.num_train, 
+        arglist.num_test, 
+        arglist.min_len
+    )
     
-    df_train = df_all_tracks.loc[
-        df_all_tracks["track_id"] == id_sort[0]
-    ].reset_index(drop=True)
-    df_test = df_all_tracks.loc[
-        df_all_tracks["track_id"] == id_sort[1]
-    ].reset_index(drop=True)
-    
-    # derive acceleration
-    df_train = derive_acc(df_train, dt)
-    df_test = derive_acc(df_test, dt)
-    
-    # normalize data
-    df_train = normalize_pos(df_train)
-    df_test = normalize_pos(df_test)
+    # check interpolation quality
+    # fig, ax = plt.subplots(3, 2, figsize=(6, 6))
+    # ax[0, 0].plot(df_train["x"])
+    # ax[0, 1].plot(df_train["y"])
+    # ax[1, 0].plot(df_train["vx"])
+    # ax[1, 1].plot(df_train["vy"])
+    # ax[2, 0].plot(df_train["vx_grad"])
+    # ax[2, 1].plot(df_train["vy_grad"])
+    # plt.tight_layout()
+    # plt.show()
     
     # pack observations
-    obs_train = df_train[["x", "y", "vx", "vy", "vx_grad", "vy_grad"]].values
-    obs_test = df_test[["x", "y", "vx", "vy", "vx_grad", "vy_grad"]].values
+    obs_cols = ["x", "y", "vx", "vy", "vx_grad", "vy_grad"]
+    f_df_to_batch = lambda df: df.groupby("track_id")[obs_cols].\
+        apply(lambda x: x.values.tolist()).tolist()
+    obs_train = np.array(f_df_to_batch(df_train))
+    obs_test = np.array(f_df_to_batch(df_test))
     
     # init parameters with const acc model
-    eps = 1e-4
+    eps = 1e-3
     transition_matrix = np.array(
-        [[1, 0, dt,  0,  0,  0],
-         [0, 1,  0, dt,  0,  0],
+        [[1, 0, dt,  0,  0.5 * dt**2,  0],
+         [0, 1,  0, dt,  0,  0.5 * dt**2],
          [0, 0,  1,  0, dt,  0],
          [0, 0,  0,  1,  0, dt],
          [0, 0,  0,  0,  1,  0],
          [0, 0,  0,  0,  0,  1]]
-    ) + eps
-    observation_matrix = np.eye(6) + eps
+    ) 
+    observation_matrix = np.eye(6) 
     transition_covariance = 0.01 * np.eye(6) + eps
     observation_covariance = 0.01 * np.eye(6) + eps
-    transition_offset = np.zeros((6)) + eps
-    observation_offset = np.zeros((6)) + eps
+    transition_offset = np.zeros((6))
+    observation_offset = np.zeros((6))
     initial_state_mean = np.zeros((6,))
-    initial_state_covariance = 0.01 * np.eye(6) + eps
+    initial_state_covariance = np.diag([1, 0.5, 1, 0.5, 1, 1])
     
     # train
-    kf = KalmanFilter(
+    kf = BatchKalmanFilter(
         transition_matrix,
         observation_matrix,
         transition_covariance,
@@ -136,18 +189,22 @@ def main(arglist):
         initial_state_covariance,
         random_state=arglist.seed,
         em_vars=[
-        "transition_matrices", "observation_matrices",
-        "transition_covariance", "observation_covariance",
-        "transition_offsets", "observation_offsets", 
-        "initial_state_mean", "initial_state_covariance"
+        # "transition_matrices", 
+        # "observation_matrices",
+        "transition_covariance", 
+        "observation_covariance",
+        # "transition_offsets", 
+        # "observation_offsets", 
+        # "initial_state_mean", 
+        # "initial_state_covariance"
         ]
     )
     
     history = []
     for e in range(arglist.epochs):
-        kf = kf.em(X=obs_train, n_iter=1)
-        train_loss = kf.loglikelihood(obs_train)
-        test_loss = kf.loglikelihood(obs_test) 
+        kf = kf.batch_em(X=obs_train, n_iter=1)
+        train_loss = kf.batch_loglikelihood(obs_train)
+        test_loss = kf.batch_loglikelihood(obs_test)
         history.append(
             {"epoch": e + 1, "train_loss": train_loss, "test_loss": test_loss}   
         )
@@ -156,12 +213,13 @@ def main(arglist):
             np.round(train_loss, 2),
             np.round(test_loss, 2),
         ))
-        break
+        
+    df_history = pd.DataFrame(history)
     
-    # fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    # ax.plot(loss)
-    # ax.grid()
-    # plt.show()
+    fig_params = plot_kf_params(kf)
+    fig_history = plot_history(df_history)
+    
+    plt.show()
     return 
 
 if __name__ == "__main__":
