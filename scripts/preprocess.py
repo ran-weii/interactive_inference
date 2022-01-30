8409d90fa021047815e84e5fe640f5a3ff2c4961
rw422 <rw422@tamu.edu>
2022-01-29 21:21:05 -0600

    move agent filtering function to new module agent_filter.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -7,10 +7,10 @@ import numpy as np
 from numpy.core.fromnumeric import trace
 import pandas as pd
 import matplotlib.pyplot as plt
+from src.data.lanelet import load_lanelet_df
 from src.data.kalman_filter import BatchKalmanFilter
+from src.data.agent_filter import get_lane_pos, get_neighbor_vehicles
 from src.data.utils import derive_acc, normalize_pos
-from src.data.utils import dist_two_points, closest_point_on_line
-from src.data.utils import is_above_line, get_cardinal_direction
 
 # set pandas display option
 pd.set_option('display.max_columns', None)
@@ -59,166 +59,6 @@ def kalman_smooth(df, kf):
     df_out = pd.concat([df, df_s_mean], axis=1)
     return df_out
 
-def load_map(map_path, scenario):
-    map_file = os.path.join(map_path, scenario + ".json")
-    with open(map_file, "r") as f:
-        lane_dict = json.load(f)
-    
-    # remove unlabeled lanes
-    lane_dict = {
-        k: v for (k, v) in lane_dict.items() if v["label"] is not None
-    }
-    
-    # dict to df
-    df_lanelet = []
-    for i, (lane_id, lane_val) in enumerate(lane_dict.items()):
-        label = lane_val["label"]
-        num_ways = lane_val["len"]
-        way_dict = lane_val["way_dict"]
-        
-        df_way = []
-        for j, (way_id, way_val) in enumerate(way_dict.items()):
-            way_val["way_id"] = way_id
-            df_way.append(way_val)
-            
-        df_way = pd.DataFrame(df_way)
-        df_way["lane_id"] = lane_id
-        df_way["lane_label"] = [label for i in range(len(df_way))]
-        df_way["num_ways"] = num_ways
-        
-        df_lanelet.append(df_way)
-        
-    df_lanelet = pd.concat(df_lanelet, axis=0).reset_index(drop=True)
-    return df_lanelet
-    
-def min_dist_to_way(x, y, heading, way_x, way_y):
-    """ Find the min absolute distance from the vehicle to 
-    every line segment in a way
-
-    Args:
-        x (float): vehicle x coor
-        y (float): vehicle y coor
-        heading (float): vehicle heading in radians
-        way_x (list): list of x coor of the way
-        way_y (list): list of y coor of the way
-        
-    Returns:
-        min_dist (float): signed minimum distance from the vehile to the way
-            if right lane: sign=1, if left lane: sign=-1
-    """    
-    dists = []
-    for i in range(len(way_x) - 1):
-        a, b = closest_point_on_line(x, y, way_x[i:i+2], way_y[i:i+2])
-        card = get_cardinal_direction(x, y, heading, a, b)
-        sign = 1 if card < 0 and card > -np.pi else -1
-        dist = dist_two_points(x, y, a, b)
-        dists.append(sign * dist)
-    
-    dists = np.array(dists)
-    sort_id = np.argsort(dists ** 2)
-    min_dist = dists[sort_id[0]]
-    return min_dist
-
-def assign_lane_pos(x, y, heading, df_lanelet):
-    """ Assign lane position and labels to vehicle
-
-    Args:
-        x (float): vehicle x coor
-        y (float): vehicle y coor
-        heading (float): vehicle heading in radians
-        df_lanelet (pd.dataframe): lanelet dataframe, each row is a way
-
-    Returns:
-        df_lane_assigned (pd.series): left and right lane attribute dataframe
-    """
-    df_lanelet = df_lanelet.copy()
-    
-    # calculate distance between vehicle and all ways
-    min_dists = np.zeros((len(df_lanelet)))
-    for i in range(len(df_lanelet)):
-        min_dists[i] = min_dist_to_way(
-            x, y, heading, df_lanelet.iloc[i]["x"], df_lanelet.iloc[i]["y"]
-        )
-    
-    df_lanelet["min_dist"] = min_dists
-    df_lanelet["abs_min_dist"] = np.abs(min_dists)
-    df_lanelet = df_lanelet.sort_values("abs_min_dist").reset_index(drop=True)
-    
-    # find closest ways above and below
-    df_left = df_lanelet.loc[df_lanelet["min_dist"] > 0].\
-        sort_values("abs_min_dist").head(1).reset_index(drop=True)
-    df_right = df_lanelet.loc[df_lanelet["min_dist"] <= 0].\
-        sort_values("abs_min_dist").head(1).reset_index(drop=True)
-    
-    label_left = df_left["lane_label"].iloc[0]
-    label_right = df_right["lane_label"].iloc[0]
-    
-    # determine merging lane labels
-    label1 = label_left[0]
-    label2 = label_right[0]
-    if abs(label_left[0] - label_right[0]) > 1:
-        if len(label_left) > 1:
-            label1 = label_left[1]
-            label2 = label_right[0]
-        elif len(label_right) > 1:
-            label1 = label_left[0]
-            label2 = label_right[1]
-    
-    df_lane_assigned = pd.Series({
-        "lane_left_label": label1,
-        "lane_left_type": df_left["type"].iloc[0],
-        "lane_left_subtype": df_left["subtype"].iloc[0],
-        "lane_left_way_id": df_left["way_id"].iloc[0],
-        "lane_left_min_dist": df_left["min_dist"].iloc[0],
-        "lane_right_label": label2,
-        "lane_right_type": df_right["type"].iloc[0],
-        "lane_right_subtype": df_right["subtype"].iloc[0],
-        "lane_right_way_id": df_right["way_id"].iloc[0],
-        "lane_right_min_dist": df_right["min_dist"].iloc[0],
-    })
-    return df_lane_assigned
-
-def find_neighbor_vehicles(df_ego, df_track, r):
-    """ Find all vehicle in adjacent lane within a radius
-
-    Args:
-        df_ego (pd.series): ego vehicle series
-        df_track (pd.dataframe): entire track dataframe
-        r (float): scan radius centered on ego vehicle. 
-            Vehicles outside of the radus will be removed
-            
-    Returns:
-        dict_frame (list): list of observation dict. Each element in list is a vehicle.
-            The ego vehicle is the first element. 
-    """ 
-    frame_id = df_ego["frame_id"]
-    df_frame = df_track.loc[df_track["frame_id"] == frame_id]
-    
-    df_frame["dist_to_ego"] = np.sqrt(
-        (df_frame["x"] - df_ego["x"]) ** 2 + (df_frame["y"] - df_ego["y"]) ** 2
-    )
-    df_frame = df_frame.loc[df_frame["dist_to_ego"] <= r]
-    
-    # compare avg lanes and remove non neighbors
-    df_ego["avg_lane"] = 0.5 * (df_ego["lane_left_label"] + df_ego["lane_right_label"])
-    df_frame["avg_lane"] = 0.5 * (df_frame["lane_left_label"] + df_frame["lane_right_label"])
-    df_frame = df_frame.loc[np.abs(df_frame["avg_lane"] - df_ego["avg_lane"]) <= 1]
-    df_frame = df_frame.sort_values(by="dist_to_ego").reset_index(drop=True)
-    
-    # find neighbor cardianl directions
-    df_frame["card"] = [
-        get_cardinal_direction(
-            df_ego["x"], df_ego["y"], df_ego["psi_rad"], 
-            df_frame.iloc[i]["x"], df_frame.iloc[i]["y"]
-        ) for i in range(len(df_frame))
-    ]
-    df_frame["is_ego"] = df_frame["dist_to_ego"] == 0
-    df_frame["is_lead"] = (df_frame["card"] < np.pi/2) * (df_frame["card"] > -np.pi/2)
-    df_frame["is_left"] = (df_frame["card"] > 0) * (df_frame["card"] < np.pi)
-    
-    dict_frame = df_frame.to_dict(orient="records")
-    return dict_frame
-
 def main(arglist):
     # load kalman filter 
     with open(os.path.join(arglist.kf_path, "model.p"), "rb") as f:
@@ -241,7 +81,10 @@ def main(arglist):
                 os.path.join(scenario_path, "*.csv")
             )
             
-            df_lanelet = load_map(arglist.map_path, scenario)
+            # df_lanelet = load_map(arglist.map_path, scenario)
+            df_lanelet = load_lanelet_df(
+                os.path.join(arglist.map_path, scenario + ".json")
+            )
             
             for j, track_path in enumerate(track_paths):
                 filename = os.path.basename(track_path)
@@ -273,16 +116,17 @@ def main(arglist):
                 """ test find neighbors """
                 df_track = df_track.loc[df_track["frame_id"] == 1].reset_index(drop=True)
                 df_lane_assigned = df_track.apply(
-                    lambda x: assign_lane_pos(
+                    lambda x: get_lane_pos(
                         x["x"], x["y"], x["psi_rad"], df_lanelet
                     ), axis=1
                 )
                 df_track = pd.concat([df_track, df_lane_assigned], axis=1)
                 
                 df_ego = df_track.loc[df_track["track_id"] == 2].iloc[0]
+                df_frame = df_track.loc[df_track["frame_id"] == df_ego["frame_id"]]
                 
-                find_neighbor_vehicles(df_ego, df_track, r=50)
-                
+                df_neighbor = get_neighbor_vehicles(df_ego, df_frame, r=50)
+                print(df_neighbor)
                 # find neighbouring vehicle
                 
                 if arglist.debug:
---
+++ src/data/agent_filter.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+from .utils import closest_point_on_line, get_cardinal_direction, dist_two_points
+
+def min_dist_to_way(x, y, heading, way_x, way_y):
+    """ Find the min absolute distance from the vehicle to 
+    every line segment in a way
+
+    Args:
+        x (float): vehicle x coor
+        y (float): vehicle y coor
+        heading (float): vehicle heading in radians
+        way_x (list): list of x coor of the way
+        way_y (list): list of y coor of the way
+        
+    Returns:
+        min_dist (float): signed minimum distance from the vehile to the way
+            if right lane: sign=1, if left lane: sign=-1
+    """    
+    dists = []
+    for i in range(len(way_x) - 1):
+        a, b = closest_point_on_line(x, y, way_x[i:i+2], way_y[i:i+2])
+        card = get_cardinal_direction(x, y, heading, a, b)
+        sign = 1 if card < 0 and card > -np.pi else -1
+        dist = dist_two_points(x, y, a, b)
+        dists.append(sign * dist)
+    
+    dists = np.array(dists)
+    sort_id = np.argsort(dists ** 2)
+    min_dist = dists[sort_id[0]]
+    return min_dist
+
+""" TODO: should be avoid using df in this function? """
+def get_lane_pos(x, y, heading, df_lanelet):
+    """ Assign lane position and labels to vehicle
+
+    Args:
+        x (float): vehicle x coor
+        y (float): vehicle y coor
+        heading (float): vehicle heading in radians
+        df_lanelet (pd.dataframe): lanelet dataframe, each row is a way
+
+    Returns:
+        df_lane_assigned (pd.series): left and right lane attribute dataframe
+    """
+    df_lanelet = df_lanelet.copy()
+    
+    # calculate distance between vehicle and all ways
+    min_dists = np.zeros((len(df_lanelet)))
+    for i in range(len(df_lanelet)):
+        min_dists[i] = min_dist_to_way(
+            x, y, heading, df_lanelet.iloc[i]["x"], df_lanelet.iloc[i]["y"]
+        )
+    
+    df_lanelet["min_dist"] = min_dists
+    df_lanelet["abs_min_dist"] = np.abs(min_dists)
+    df_lanelet = df_lanelet.sort_values("abs_min_dist").reset_index(drop=True)
+    
+    # find closest ways above and below
+    df_left = df_lanelet.loc[df_lanelet["min_dist"] > 0].\
+        sort_values("abs_min_dist").head(1).reset_index(drop=True)
+    df_right = df_lanelet.loc[df_lanelet["min_dist"] <= 0].\
+        sort_values("abs_min_dist").head(1).reset_index(drop=True)
+    
+    label_left = df_left["lane_label"].iloc[0]
+    label_right = df_right["lane_label"].iloc[0]
+    
+    # determine merging lane labels
+    label1 = label_left[0]
+    label2 = label_right[0]
+    if abs(label_left[0] - label_right[0]) > 1:
+        if len(label_left) > 1:
+            label1 = label_left[1]
+            label2 = label_right[0]
+        elif len(label_right) > 1:
+            label1 = label_left[0]
+            label2 = label_right[1]
+    
+    df_lane_assigned = pd.Series({
+        "lane_left_label": label1,
+        "lane_left_type": df_left["type"].iloc[0],
+        "lane_left_subtype": df_left["subtype"].iloc[0],
+        "lane_left_way_id": df_left["way_id"].iloc[0],
+        "lane_left_min_dist": df_left["min_dist"].iloc[0],
+        "lane_right_label": label2,
+        "lane_right_type": df_right["type"].iloc[0],
+        "lane_right_subtype": df_right["subtype"].iloc[0],
+        "lane_right_way_id": df_right["way_id"].iloc[0],
+        "lane_right_min_dist": df_right["min_dist"].iloc[0],
+    })
+    return df_lane_assigned
+
+def get_neighbor_vehicles(df_ego, df_frame, r):
+    """ Find all vehicle in adjacent lane within a radius
+
+    Args:
+        df_ego (pd.series): pd series of ego vehicle 
+        df_frame (pd.dataframe): dataframe of all vehicels in the same frame
+        r (float): scan radius centered on ego vehicle. 
+            Vehicles outside of the radus will be removed
+            
+    Returns:
+        df_neighbors (pd.dataframe): dataframe of neighbor vehicles.
+            The ego vehicle is the first row. 
+    """ 
+    assert df_ego["frame_id"] == df_frame["frame_id"].iloc[0]
+    assert len(df_frame["frame_id"].unique()) == 1
+    
+    df_neighbors = df_frame.copy()
+    
+    df_neighbors["dist_to_ego"] = np.sqrt(
+        (df_neighbors["x"] - df_ego["x"]) ** 2 + (df_neighbors["y"] - df_ego["y"]) ** 2
+    )
+    df_neighbors = df_neighbors.loc[df_neighbors["dist_to_ego"] <= r]
+    
+    # compare avg lanes and remove non neighbors
+    df_ego["avg_lane"] = 0.5 * (df_ego["lane_left_label"] + df_ego["lane_right_label"])
+    df_neighbors["avg_lane"] = 0.5 * (df_neighbors["lane_left_label"] + df_neighbors["lane_right_label"])
+    df_neighbors = df_neighbors.loc[np.abs(df_neighbors["avg_lane"] - df_neighbors["avg_lane"]) <= 1]
+    df_neighbors = df_neighbors.sort_values(by="dist_to_ego").reset_index(drop=True)
+    
+    # find neighbor cardianl directions
+    df_neighbors["card"] = [
+        get_cardinal_direction(
+            df_ego["x"], df_ego["y"], df_ego["psi_rad"], 
+            df_neighbors.iloc[i]["x"], df_neighbors.iloc[i]["y"]
+        ) for i in range(len(df_neighbors))
+    ]
+    df_neighbors["is_ego"] = df_neighbors["dist_to_ego"] == 0
+    df_neighbors["is_lead"] = (df_neighbors["card"] < np.pi/2) * (df_neighbors["card"] > -np.pi/2)
+    df_neighbors["is_left"] = (df_neighbors["card"] > 0) * (df_neighbors["card"] < np.pi)
+    return df_neighbors
