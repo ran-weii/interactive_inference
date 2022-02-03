e61ab39972cbce917a08520222c94ae5a42c5a8a
rw422 <rw422@tamu.edu>
2022-02-03 15:33:45 -0600

    add SimpleEgoDataset

--- src/data/ego_dataset.py
+++ src/data/ego_dataset.py
@@ -1,7 +1,6 @@
 from enum import unique
 import numpy as np
 import pandas as pd
-import torch
 from torch.utils.data.dataset import Dataset
 from src.data.agent_filter import get_neighbor_vehicles
 
@@ -29,20 +28,8 @@ def filter_car_follow_eps(df_track, min_eps_len):
     df_track["eps_id"].loc[slice_id == False] = -1
     df_track["eps_len"].loc[slice_id == False] = -1
     return df_track
-
-""" TODO: implement this for initial model """
-class SimpleEgoDataset(Dataset):
-    """ Dataset for lead vehicle following only """
-    def __init__(self):
-        super().__init__()
-        
-    def __len__(self):
-        return 
     
-    def __getitem__(self):
-        return 
-
-""" TODO: add acc to fields """
+    
 class EgoDataset(Dataset):
     """ Dataset for general car following """
     def __init__(
@@ -65,12 +52,12 @@ class EgoDataset(Dataset):
         
         self.meta_fields = ["scenario", "record_id", "track_id", "agent_type"]
         self.ego_fields = [
-            "x", "y", "vx", "vy", "psi_rad", "length", "width", 
+            "x", "y", "vx", "vy", "psi_rad", "length", "width", "track_id",
             "lane_left_type", "lane_left_min_dist", 
             "lane_right_type", "lane_right_min_dist"
         ]
         self.agent_fields = self.ego_fields[:-4] + ["is_lead", "is_left", "dist_to_ego"]
-        self.act_fields = [""]
+        self.act_fields = ["ax", "ay"]
             
     def __len__(self):
         return len(self.unique_eps)
@@ -80,23 +67,26 @@ class EgoDataset(Dataset):
         
         obs_meta = df_ego[self.meta_fields].iloc[0].to_numpy()
         obs_ego = df_ego[self.ego_fields].to_numpy()
-        obs_agents = self.get_neighbor_vehicles(df_ego)
+        obs_agents = self.get_agent_obs(df_ego)
+        act_ego = df_ego[self.act_fields].to_numpy()
         
         out_dict = {
             "meta": obs_meta,
             "ego": obs_ego,
             "agents": obs_agents,
-            "act": None
+            "act": act_ego
         }
         return out_dict
     
-    def get_neighbor_vehicles(self, df_ego):
+    def get_agent_obs(self, df_ego):
         scenario = df_ego["scenario"].iloc[0]
         record_id = df_ego["record_id"].iloc[0]
+        ego_id = df_ego["track_id"].iloc[0]
         T = len(df_ego)
         
         df_scenario = self.df_track.loc[self.df_track["scenario"] == scenario]
         df_record = df_scenario.loc[df_scenario["record_id"] == record_id]
+        df_record = df_record.loc[df_record["track_id"] != ego_id]
         
         obs_agents = -1 * np.ones((T, self.max_agents, len(self.agent_fields)))
         for t in range(T):
@@ -109,27 +99,34 @@ class EgoDataset(Dataset):
             num_agents = min(len(df_neighbor), self.max_agents)
             obs_agents[t, :num_agents] = df_neighbor.to_numpy()[:num_agents]
         return obs_agents
-    
-if __name__ == "__main__":
-    import os
-    from src.data.lanelet import load_lanelet_df
-    pd.set_option('display.max_columns', None)
-    pd.options.mode.chained_assignment = None
 
-    lanelet_path = "../../exp/lanelet"
-    data_path = "../../interaction-dataset-master"
-    scenario = "DR_CHN_Merging_ZS"
-    filename = "vehicle_tracks_007.csv"
+
+class SimpleEgoDataset(EgoDataset):
+    """ Dataset for lead vehicle following only """
+    def __init__(self, df_track, df_lanelet, min_eps_len=10, max_dist=50.):
+        super().__init__(
+            df_track, df_lanelet, min_eps_len, 
+            max_dist, max_agents=1, car_following=True
+        )
     
-    df_lanelet = load_lanelet_df(os.path.join(lanelet_path, scenario + ".json"))
-    df_processed = pd.read_csv(
-        os.path.join(data_path, "processed_trackfiles", scenario, filename)
-    )
-    df_track = pd.read_csv(
-        os.path.join(data_path, "recorded_trackfiles", scenario, filename)
-    )
-    df_track = df_track.merge(df_processed, on=["track_id", "frame_id"])
-    
-    min_eps_len = 50
-    ego_dataset = EgoDataset(df_track, df_lanelet, min_eps_len)
-    obs = ego_dataset[0]
+    def get_agent_obs(self, df_ego):
+        scenario = df_ego["scenario"].iloc[0]
+        record_id = df_ego["record_id"].iloc[0]
+        ego_id = df_ego["track_id"].iloc[0]
+        lead_track_id = df_ego["lead_track_id"].iloc[0]
+        T = len(df_ego)
+        
+        df_scenario = self.df_track.loc[self.df_track["scenario"] == scenario]
+        df_record = df_scenario.loc[df_scenario["record_id"] == record_id]
+        df_agents = df_record.loc[df_record["track_id"] == lead_track_id]
+        
+        df_agents = df_agents.loc[df_agents["frame_id"].isin(df_ego["frame_id"].values)]
+        df_agents["is_lead"] = True
+        df_agents["is_left"] = False
+        df_agents["dist_to_ego"] = np.sqrt(
+            (df_agents["x"] - df_ego["x"])**2 + (df_agents["y"] - df_ego["y"])**2
+        )
+        
+        obs_agents = -1 * np.ones((T, self.max_agents, len(self.agent_fields)))
+        obs_agents[:, 0, :] = df_agents[self.agent_fields].values
+        return obs_agents
