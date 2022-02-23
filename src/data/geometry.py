a2131627befcc0b218db41a8b5a73209814d4e61
rw422 <rw422@tamu.edu>
2022-02-23 16:34:22 -0600

    add vector_projection()

--- src/data/geometry.py
+++ src/data/geometry.py
@@ -1,22 +1,4 @@
 import numpy as np
-import pandas as pd
-
-def derive_acc(df, dt):
-    """ differentiate vel and add to df """
-    df_ = df.copy()
-    f_grad = lambda x: pd.DataFrame(np.gradient(x, dt), index=x.index)
-    df_["vx_grad"] = df_.groupby("track_id")["vx"].apply(f_grad)
-    df_["vy_grad"] = df_.groupby("track_id")["vy"].apply(f_grad)
-    return df_
-
-def normalize_pos(df):
-    """ subtrack all pos by initial pos """
-    df_ = df.copy()
-    
-    f_norm = lambda x: x - x.iloc[0]
-    df_["x"] = df_.groupby("track_id")["x"].apply(f_norm)
-    df_["y"] = df_.groupby("track_id")["y"].apply(f_norm)
-    return df_
 
 def dist_two_points(x1, y1, x2, y2):
     """ Two point distance formula """
@@ -105,3 +87,31 @@ def is_above_line(x, y, heading, a, b):
         return -1
     else: 
         return 1
+    
+def vector_projection(x1, y1, x2, y2, return_vec=False):
+    """ Project vector (x1, y1) onto vector (x12, y2)
+    
+    Args:
+        x1 (np.array): vector 1 x coor
+        y1 (np.array): vector 1 y coor
+        x2 (np.array): vector 2 x coor
+        y2 (np.array): vector 3 y coor
+        return_vec (bool, optional): return projection vector 
+            in world coordinate, default=False
+    
+    Returns:
+        x3 (np.array): projection vector x coor
+        y3 (np.array): projection vector y coor
+    """
+    a = np.stack([x1, y1]).T
+    b = np.stack([x2, y2]).T
+    if not return_vec:
+        b_norm = np.linalg.norm(b, ord=1, axis=1)
+        x3 = x1*x2 + y1*y2
+        y3 = (y1*x2 - x1*y2) / b_norm
+    else:
+        b_norm = np.linalg.norm(b, axis=1, keepdims=True)
+        b_unit = b / b_norm
+        x3 = (x1*x2 + y1*y2).reshape(-1, 1) * b_unit
+        y3 = a - x3
+    return x3, y3
