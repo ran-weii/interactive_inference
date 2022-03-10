c093f63ba3261cfde2ab33c274e0c9ead09329a8
rw422 <rw422@tamu.edu>
2022-03-10 10:46:45 -0600

    add inference tests to test_active_inference_agent()

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from src.agents.active_inference import ActiveInference
+from src.evaluation.offline_metrics import mean_absolute_error
 
 def test_active_inference_agent():
     torch.manual_seed(0)
@@ -17,8 +18,9 @@ def test_active_inference_agent():
     T = 24
     obs = torch.randn(T, batch_size, obs_dim)
     u = torch.randn(T, batch_size, ctl_dim)
-    mask = torch.zeros(batch_size, T)
+    mask = torch.randint(0, 2, (T, batch_size)).to(torch.float32)
     
+    """ test learning """
     # test with supplied parameters
     agent = ActiveInference(state_dim, act_dim, obs_dim, ctl_dim, H)
     theta = {
@@ -29,7 +31,7 @@ def test_active_inference_agent():
         "F": nn.Parameter(torch.randn(batch_size, sum(agent.ctl_model.parameter_size))),
         "tau": nn.Parameter(torch.randn(batch_size, 1))
     }
-    logp_pi, logp_obs = agent(obs, u, theta=theta)
+    logp_pi, logp_obs = agent(obs, u, theta=theta, inference=False)
     loss = logp_pi.sum()
     loss.backward()
     
@@ -50,7 +52,7 @@ def test_active_inference_agent():
     
     # test with self parameters
     agent = ActiveInference(state_dim, act_dim, obs_dim, ctl_dim, H)
-    logp_pi, logp_obs = agent(obs, u)
+    logp_pi, logp_obs = agent(obs, u, inference=False)
     loss = logp_pi.sum()
     loss.backward()
     
@@ -61,6 +63,41 @@ def test_active_inference_agent():
     assert agent.ctl_model.lv.grad.norm() != 0
     assert agent.hmm.B.grad.norm() != 0
     assert agent.tau.grad.norm() != 0
+    
+    """ test inference """
+    # test with supplied parameters
+    agent = ActiveInference(state_dim, act_dim, obs_dim, ctl_dim, H)
+    theta = {
+        "A": nn.Parameter(torch.randn(batch_size, sum(agent.obs_model.parameter_size))),
+        "B": nn.Parameter(torch.randn(batch_size, agent.hmm.parameter_size[0])),
+        "C": nn.Parameter(torch.randn(batch_size, state_dim)),
+        "D": nn.Parameter(torch.randn(batch_size, state_dim)),
+        "F": nn.Parameter(torch.randn(batch_size, sum(agent.ctl_model.parameter_size))),
+        "tau": nn.Parameter(torch.randn(batch_size, 1))
+    }
+    with torch.no_grad():
+        G, b = agent(obs, u, theta=theta, inference=True)
+        ctl = agent.choose_action(obs, u, theta=theta)
+        mae = mean_absolute_error(
+            u.numpy(), ctl.numpy(), mask=mask.numpy(), 
+            speed=obs[:, :, 0].numpy(), cumulative=False
+        )
+        
+    assert list(ctl.shape) == [T, batch_size, ctl_dim]
+    assert list(mae.shape) == [ctl_dim]
+    
+    # test with self parameters
+    agent = ActiveInference(state_dim, act_dim, obs_dim, ctl_dim, H)
+    with torch.no_grad():
+        G, b = agent(obs, u, inference=False)
+        ctl = agent.choose_action(obs, u)
+        mae = mean_absolute_error(
+            u.numpy(), ctl.numpy(), mask=mask.numpy(), 
+            speed=obs[:, :, 0].numpy(), cumulative=False
+        )
+        
+    assert list(ctl.shape) == [T, batch_size, ctl_dim]
+    assert list(mae.shape) == [ctl_dim]
     
     print("test_active_inference_agent passed")
 
