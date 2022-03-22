e2aaf9290f63fa7e710b3f02c04db70e9c66746e
rw422 <rw422@tamu.edu>
2022-03-22 11:31:50 -0500

    add cell property to Lane class and associated updates

--- src/map_api/lanelet_layers.py
+++ src/map_api/lanelet_layers.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 from shapely.geometry import Point, LineString, Polygon
-from shapely.ops import unary_union
+from shapely import ops
 from src.data.geometry import get_cardinal_direction
 
 class L2Point:
@@ -33,16 +33,17 @@ class L2Polygon:
         self.type_ = type_
         self.subtype = subtype
             
-            
+
+class Cell:
+    """ Section of a lane, represented as a Shapely polygon, with a defined heading """
+    def __init__(self, polygon, heading, left_bound, right_bound):
+        self.polygon = polygon  # Shapely polygon
+        self.heading = heading  # radians clockwise from y-axis
+        self.left_bound = left_bound
+        self.right_bound = right_bound
+
+       
 class Lanelet:
-    class Cell:
-        """ Section of a lane, represented as a Shapely polygon, with a defined heading """
-        def __init__(self, polygon, heading, left_bound, right_bound):
-            self.polygon = polygon  # Shapely polygon
-            self.heading = heading  # radians clockwise from y-axis
-            self.left_bound = left_bound
-            self.right_bound = right_bound
-            
     def __init__(self, id_, subtype, region, location, one_way, 
         turn_direction, vehicle_participant, pedestrian_participant, 
         bicycle_participant, left_bound=None, right_bound=None, 
@@ -163,18 +164,34 @@ class Lanelet:
             delta_x = shorter_points[i+1].x - shorter_points[i].x
             cell_heading = np.arctan2(delta_y, delta_x)
             
-            cell = self.Cell(cell_polygon, cell_heading, cell_left_bound, cell_right_bound)
+            cell = Cell(cell_polygon, cell_heading, cell_left_bound, cell_right_bound)
             self._cells.append(cell)    
         return self._cells
 
 
 class Lane:
-    def __init__(self, id_, lanelets):
-        self.id = id_
+    def __init__(self, id_, lanelets, cell_distance=5.):
+        self.id_ = id_
         self.lanelets = [l for l in lanelets]
+        self.buffer_ = 0
+        self.cell_distance = cell_distance
+        
+        self.left_bound = None
+        self.right_bound = None
         
         self._polygon = None
+        self._cells = []
         self._align_lanelets()
+    
+    def has_opposing_linestrings(self):
+        """ Determines if a lane's left and right bounds have opposing headings """
+        left_bound_coords = list(self.left_bound.linestring.coords)
+        right_bound_coords = list(self.right_bound.linestring.coords)
+
+        left_head = Point(left_bound_coords[-1])  # last point of the left bound 
+        right_tail = Point(right_bound_coords[0])  # first point of the right bound
+        right_head = Point(right_bound_coords[-1])  # last point of the right bound
+        return True if left_head.distance(right_head) > left_head.distance(right_tail) else False
     
     @property
     def polygon(self):
@@ -182,11 +199,56 @@ class Lane:
             return self._polygon
         
         lanelet_polygons = [l.polygon for l in self.lanelets if l.subtype != "crosswalk"]
-        self._polygon = unary_union(lanelet_polygons)
+        self._polygon = ops.unary_union(lanelet_polygons)
         return self._polygon
     
+    @property
+    def cells(self):
+        if self._cells:
+            return self._cells
+        
+        left_bound_linestr = self.left_bound.linestring
+        right_bound_linestr = self.right_bound.linestring
+        if self.has_opposing_linestrings():
+            left_bound_linestr = LineString(self.left_bound.linestring.coords[::-1]) 
+        
+        # determine which linestring is longer
+        right_is_longer = True
+        if right_bound_linestr.length > left_bound_linestr.length:
+            longer_linestr = right_bound_linestr
+            shorter_linestr = left_bound_linestr
+        else:
+            longer_linestr = left_bound_linestr
+            shorter_linestr = right_bound_linestr
+            right_is_longer = False
+        
+        # interpolate shorter linestring by cell distance
+        shorter_distances = np.arange(0, shorter_linestr.length, self.cell_distance)
+        shorter_points = [shorter_linestr.interpolate(d) for d in shorter_distances] + [shorter_linestr.boundary[-1]]
+        longer_distances = [0] + [longer_linestr.project(p) for p in shorter_points[1:-1]]
+        longer_points = [longer_linestr.interpolate(d) for d in longer_distances] + [longer_linestr.boundary[-1]]
+        for i in range(len(shorter_points) - 1):
+            cell_coords = [(p.x, p.y) for p in [shorter_points[i], shorter_points[i+1], longer_points[i+1], longer_points[i]]]
+            cell_polygon = Polygon(cell_coords).buffer(self.buffer_)
+            
+            if right_is_longer:
+                cell_left_bound = LineString([shorter_points[i], shorter_points[i+1]])
+                cell_right_bound = LineString([longer_points[i], longer_points[i+1]])
+            else:
+                cell_left_bound = LineString([longer_points[i], longer_points[i+1]])
+                cell_right_bound = LineString([shorter_points[i], shorter_points[i+1]])
+                
+            # compute cell heading
+            delta_y = shorter_points[i+1].y - shorter_points[i].y
+            delta_x = shorter_points[i+1].x - shorter_points[i].x
+            cell_heading = np.arctan2(delta_y, delta_x)
+            
+            cell = Cell(cell_polygon, cell_heading, cell_left_bound, cell_right_bound)
+            self._cells.append(cell)    
+        return self._cells
+    
     def _align_lanelets(self):
-        """ Sort lanelets by heading direction """
+        """ Sort lanelets by heading direction and add lane bounds as properties """
         def get_order(node1, node2):
             """ Return topological order between node1 and node2 """
             left_bound_linestr1 = node1.left_bound.linestring
@@ -218,3 +280,8 @@ class Lane:
                     G.add_edge((node_id2, node_value2), (node_id1, node_value1))
         sorted_nodes = nx.topological_sort(G)
         self.lanelets = [n[1] for n in sorted_nodes]
+        
+        left_bound_linestr = ops.linemerge([l.left_bound.linestring for l in self.lanelets])
+        right_bound_linestr = ops.linemerge([l.right_bound.linestring for l in self.lanelets])
+        self.left_bound = L2Linestring(self.id_, left_bound_linestr, None, None)
+        self.right_bound = L2Linestring(self.id_, right_bound_linestr, None, None)
