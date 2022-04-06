8ca7772110039992b0d3ff9ab86f08821e63b57b
Wei Ran <rw422@tamu.edu>
2022-04-06 17:08:46 -0500

    finish test_simulator() loop

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,30 +1,29 @@
 import os
-import numpy as np
+import torch
 import pandas as pd
+
 from src.data.lanelet import load_lanelet_df
 from src.data.ego_dataset import EgoDataset
-from src.map_api.lanelet import MapData
+from src.map_api.lanelet import MapReader
 from src.simulation.simulator import InteractionSimulator
-from src.visualization.animation import animate
+from src.visualization.animation import animate, save_animation
+from src.simulation.observers import RelativeObserver
 
 lanelet_path = "../exp/lanelet"
 data_path = "../interaction-dataset-master"
 scenario1 = "DR_CHN_Merging_ZS"
 scenario2 = "DR_DEU_Merging_MT"
-scenario = scenario2
+scenario = scenario1
 filename = "vehicle_tracks_007.csv"
 
 def test_simulator():
     # load map
-    # filepath = os.path.join(data_path, "maps", scenario + ".osm")
-    # map_data = MapData(cell_len=10)
-    # map_data.parse(filepath, verbose=True)
+    filepath = os.path.join(data_path, "maps", scenario + ".osm")
+    map_data = MapReader(cell_len=10)
+    map_data.parse(filepath, verbose=True)
     df_lanelet = load_lanelet_df(os.path.join(lanelet_path, scenario + ".json"))
 
     # load tracks
-    # df_processed = pd.read_csv(
-    #     os.path.join(data_path, "processed_trackfiles", scenario, "test.csv")
-    # )
     df_processed = pd.read_csv(
         os.path.join(data_path, "processed_trackfiles", scenario, filename)
     )
@@ -37,15 +36,18 @@ def test_simulator():
     track_data = ego_dataset[0]
     
     env = InteractionSimulator(ego_dataset, df_lanelet)
-    obs = env.reset(0)
+    observer = RelativeObserver(map_data)
+    obs_env = env.reset(1)
+    obs = observer.observe(obs_env)
     for t in range(env.T):
-        action = env.get_action()
-        obs, r, done, info = env.step(action)
+        ctl = torch.tensor([0, -0.5])
+        ctl_env = observer.control(ctl, obs_env)
+        obs_env, r, done, info = env.step(ctl_env)
+        obs = observer.observe(obs_env)
         if done:
             break
-    
-    osm_path = os.path.join(data_path, "maps", scenario + ".osm")
-    animate(osm_path, env._states, env._track)
+    ani = animate(map_data, env._states, env._track)
+    save_animation(ani, "/Users/rw422/Documents/render_ani.mp4")
 
 if __name__ == "__main__":
     test_simulator()
