58d2868c554b00f9f7a72d971791f1b77d875b87
Wei Ran <rw422@tamu.edu>
2022-04-09 14:16:36 -0500

    complete GeneralizedLinearModel class and MixtureDensityNetwork class, add mixture_log_prob() method to unify implementation

--- src/distributions/models.py
+++ src/distributions/models.py
@@ -183,6 +183,12 @@ class ConditionalDistribution(nn.Module):
         distribution = self.get_distribution_class(params)
         return distribution.log_prob(x.unsqueeze(-2))
     
+    def mixture_log_prob(self, pi, x, params=None):
+        logp_pi = torch.log(pi + 1e-6)
+        logp_x = self.log_prob(x, params=params)
+        logp = torch.logsumexp(logp_pi + logp_x, dim=-1)
+        return logp
+
     def sample(self, sample_shape, params=None):
         distribution = self.get_distribution_class(params)
         return distribution.sample(sample_shape)
@@ -202,11 +208,7 @@ class ConditionalDistribution(nn.Module):
         return x
 
 
-""" TODO: 
-subclass ConditionalDistribution 
-add test case
-"""
-class GeneralizedLinearModel(nn.Module):
+class GeneralizedLinearModel(ConditionalDistribution):
     def __init__(self, x_dim, z_dim, dist="mvn", cov="full", batch_norm=False):
         """
         Args:
@@ -216,99 +218,33 @@ class GeneralizedLinearModel(nn.Module):
             cov (str): covariance type ["diag", "full"]
             batch_norm (bool, optional): use input batch normalization. default=True
         """
-        super().__init__()
-        self.x_dim = x_dim
-        self.z_dim = z_dim
-        self.dist = dist
-        self.cov = cov
-        self.parameter_size = [
-            z_dim * x_dim,
-            z_dim * x_dim,
-            z_dim * x_dim * x_dim,
-            z_dim * x_dim
-        ]
-        self.batch_norm = batch_norm
+        super().__init__(x_dim, z_dim, dist=dist, cov=cov, batch_norm=batch_norm)
+        self.b_lv = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
+        self.b_tl = nn.Parameter(torch.randn(1, z_dim, x_dim, x_dim), requires_grad=True)
+        self.b_sk = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
         
-        self.mu = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
-        self.lv = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
-        self.tl = nn.Parameter(torch.randn(1, z_dim, x_dim, x_dim), requires_grad=True)
-        self.sk = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
+        nn.init.normal_(self.b_lv, mean=0, std=0.01)
+        nn.init.normal_(self.b_tl, mean=0, std=0.01)
+        nn.init.normal_(self.b_sk, mean=0, std=0.01)
         
-        nn.init.normal_(self.mu, mean=0, std=1)
-        nn.init.normal_(self.lv, mean=0, std=0.01)
-        nn.init.normal_(self.tl, mean=0, std=1)
-        nn.init.normal_(self.sk, mean=0, std=1)
-        
-        if dist == "mvn":
-            nn.init.constant_(self.sk, 0)
-            self.sk.requires_grad = False
-            self.parameter_size = self.parameter_size[:-1]
-            self.sk.data = torch.zeros_like(self.sk.data)
-        
-        if cov == "diag":
-            nn.init.constant_(self.tl, 0)
-            self.tl.requires_grad = False
-            self.parameter_size = self.parameter_size[:-1]
-            self.tl.data = torch.zeros_like(self.tl.data)
-        
-        if batch_norm:
-            self.bn = BatchNormTransform(x_dim, momentum=0.1)
-            self.bn.gamma.requires_grad = False
-            self.bn.beta.requires_grad = False
-        
-    def __repr__(self):
-        s = "{}(x_dim={}, z_dim={}, class={}, cov={})".format(
-            self.__class__.__name__, self.x_dim, self.z_dim, self.dist, self.cov
-        )
-        return s
+        if self.cov == "diag":
+            self.b_tl.requires_grad = False
+            self.b_tl.data = torch.zeros_like(self.b_tl.data)
 
-    def transform_parameters(self, params):
         if self.dist == "mvn":
-            if self.cov == "full":
-                mu, lv, tl = torch.split(params, self.parameter_size, dim=-1)
-                mu = mu.view(-1, self.z_dim, self.x_dim)
-                lv = lv.view(-1, self.z_dim, self.x_dim)
-                tl = tl.view(-1, self.z_dim, self.x_dim, self.x_dim)
-                sk = torch.zeros(len(params), self.z_dim, self.x_dim)
-            elif self.cov == "diag":
-                mu, lv = torch.split(params, self.parameter_size, dim=-1)
-                mu = mu.view(-1, self.z_dim, self.x_dim)
-                lv = lv.view(-1, self.z_dim, self.x_dim)
-                tl = torch.zeros(len(params), self.z_dim, self.x_dim, self.x_dim)
-                sk = torch.zeros(len(params), self.z_dim, self.x_dim)
-            else:
-                raise NotImplementedError
-        elif self.dist == "mvsn":
-            if self.cov == "full":
-                mu, lv, tl, sk = torch.split(params, self.parameter_size, dim=-1)
-                mu = mu.view(-1, self.z_dim, self.x_dim)
-                lv = lv.view(-1, self.z_dim, self.x_dim)
-                tl = tl.view(-1, self.z_dim, self.x_dim, self.x_dim)
-                sk = sk.view(-1, self.z_dim, self.x_dim)
-            elif self.cov == "diag":
-                mu, lv, sk = torch.split(params, self.parameter_size, dim=-1)
-                mu = mu.view(-1, self.z_dim, self.x_dim)
-                lv = lv.view(-1, self.z_dim, self.x_dim)
-                tl = torch.zeros(len(params), self.z_dim, self.x_dim, self.x_dim)
-            else:
-                raise NotImplementedError
-        else:
-            raise NotImplementedError
-        
-        return mu, lv, tl, sk
+            self.b_sk.requires_grad = False
+            self.b_sk.data = torch.zeros_like(self.b_sk.data)
     
-    def get_distribution_class(self, pi, params=None):
+    def get_mixture_distribution_class(self, pi, params=None):
         if params is not None:
             [mu, lv, tl, sk] = self.transform_parameters(params)
         else:
             [mu, lv, tl, sk] = self.mu, self.lv, self.tl, self.sk
         
-        # mix action units
-        pi_ = pi.unsqueeze(-1)
-        mu_ = torch.sum(pi_ * mu, dim=-2)
-        lv_ = torch.sum(pi_ * lv * mu.abs(), dim=-2)
-        tl_ = torch.sum(pi_.unsqueeze(-1) * tl * mu.abs().unsqueeze(-1), dim=-3)
-        sk_ = torch.sum(pi_ * sk, dim=-2)
+        mu_ = torch.sum(pi[..., None] * mu, dim=-2)
+        lv_ = torch.sum(pi[..., None] * (lv * mu.abs() + self.b_lv), dim=-2)
+        tl_ = torch.sum(pi[..., None, None] * (tl * mu.abs()[..., None] + self.b_tl), dim=-3)
+        sk_ = torch.sum(pi[..., None] * (sk + self.b_sk), dim=-2)
         L = make_covariance_matrix(lv_, tl_, cholesky=True)
         
         if self.dist == "mvn":
@@ -319,29 +255,52 @@ class GeneralizedLinearModel(nn.Module):
         if self.batch_norm:
             distribution = SimpleTransformedModule(distribution, [self.bn])
         return distribution
+
+    def mixture_log_prob(self, pi, x, params=None):
+        distribution = self.get_mixture_distribution_class(pi, params)
+        return distribution.log_prob(x)
+
+    def ancestral_sample(self, pi, num_samples, params=None):
+        distribution = self.get_mixture_distribution_class(pi, params)
+        return distribution.sample((num_samples,))
+        
+
+class MixtureDensityNetwork(ConditionalDistribution):
+    def __init__(self, x_dim, z_dim, dist="mvn", cov="full", batch_norm=False):
+        super().__init__(x_dim, z_dim, dist, cov, batch_norm)
+        self.fc1 = nn.Linear(z_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.head = nn.Linear(64, x_dim * 2)
+        self.act = nn.SiLU()
+
+    def forward(self, x):
+        x = self.act(self.fc1(x))
+        x = self.act(self.fc2(x))
+        x = self.act(self.fc3(x))
+        x = self.head(x)
+        [mu, lv] = torch.split(x, self.x_dim, dim=-1)
+        return mu, lv
     
-    def mean(self, pi, params=None):
-        distribution = self.get_distribution_class(pi, params)
+    def get_mixture_distribution_class(self, pi, params=None):
+        mu, lv = self.forward(pi)
+        tl = torch.zeros(lv.shape[:-1] + (self.x_dim,) * 2)
+        L = make_covariance_matrix(lv, tl, cholesky=True)
+        
+        distribution = MultivariateNormal(mu, scale_tril=L)
+
+        if self.batch_norm:
+            distribution = SimpleTransformedModule(distribution, [self.bn])
+        return distribution
+
+    def mixture_log_prob(self, pi, x, params=None):
+        distribution = self.get_mixture_distribution_class(pi, params)
+        return distribution.log_prob(x)
+
+    def bayesian_average(self, pi, params=None):
+        distribution = self.get_mixture_distribution_class(pi, params)
         return distribution.mean
-    
-    def variance(self, pi, params=None):
-        distribution = self.get_distribution_class(pi, params)
-        return distribution.variance
-    
-    def entropy(self, pi, params=None):
-        distribution = self.get_distribution_class(pi, params)
-        return distribution.entropy()
-    
-    def log_prob(self, x, pi, params=None):
-        """
-        Args:
-            x (torch.tensor): observation [batch_size, x_dim]
-            pi (torch.tensor): mixing weights [batch_size, z_dim]
-            params (torch.tensor, optional): parameter vector. Defaults to None.
-        """
-        distribution = self.get_distribution_class(pi, params)
-        return distribution.log_prob(x.unsqueeze(-2))
-    
-    def sample(self, sample_shape, pi, params=None):
-        distribution = self.get_distribution_class(pi, params)
-        return distribution.sample(sample_shape)
+
+    def ancestral_sample(self, pi, num_samples, params=None):
+        distribution = self.get_mixture_distribution_class(pi, params)
+        return distribution.sample((num_samples, ))
