6ff19a221b28373cabf7d292953ccac4cc78789a
Wei Ran <rw422@tamu.edu>
2022-04-22 16:18:00 -0500

    add log variance rectification function to distributions/utils.py, update make_covariance_matrix accordingly

--- src/distributions/utils.py
+++ src/distributions/utils.py
@@ -1,18 +1,36 @@
 import math
 import torch
+import torch.nn.functional as F
 
-def make_covariance_matrix(logvar, tril, cholesky=True):
+def rectify(x, method="exp", low=1e-6, high=1e6):
+    """ Rectify x to positive 
+    Args:
+        x (torch.tensor): tensor to be rectified
+        method (str, optional): rectification method ["exp", "elu]. Defaults to "exp"
+        low (float, optional): lower bound for rectified value. Defaults to 1e-6
+        high (float, optional): upper bound for rectified value. Defaults to 1e6
+    """
+    assert method in ["exp", "elu"]
+    if method == "exp":
+        low, high = math.log(low), math.log(high)
+        out = torch.exp(x.clip(low, high))
+    elif method == "elu":
+        low, high = math.log(low + 1), high
+        out = F.elu(x.clip(low, high)) + 1
+    return out
+
+def make_covariance_matrix(logvar, tril, cholesky=True, lv_rectify="exp"):
     """ Make full covarance matrix
     
     Args:
         logvar (torch.tensor): log variance vector [batch_size, dim]
         tril (torch.tensor): unmaksed lower triangular matrix [batch_size, dim, dim]
-        cholesky (bool, optional): return cholesky decomposition, default=False
-    
+        cholesky (bool, optional): return cholesky decomposition. Defaults to False.
+        lv_rectify (str, optional): variance rectification method ["exp", "elu], Defaults to "exp".
     Returns:
         L (torch.tensor): scale_tril or cov [batch_size, dim, dim]
     """
-    var = torch.exp(logvar.clip(math.log(1e-6), math.log(1e6)))
+    var = rectify(logvar, method=lv_rectify)
     L = torch.tril(tril, diagonal=-1)
     L = L + torch.diag_embed(var)
     
