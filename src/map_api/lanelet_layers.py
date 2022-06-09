8095edda716a92bae34ccb3a70ff4f20f9f3e4ab
Wei Ran <rw422@tamu.edu>
2022-06-09 17:00:18 -0500

    add get_frenet_coords method to Lane class

--- src/map_api/lanelet_layers.py
+++ src/map_api/lanelet_layers.py
@@ -2,7 +2,9 @@ import numpy as np
 import networkx as nx
 from shapely.geometry import Point, LineString, Polygon
 from shapely import ops
-from src.data.geometry import get_heading, get_cardinal_direction, mid_point
+from scipy.interpolate import CubicSpline
+from src.data.geometry import (
+    get_heading, get_cardinal_direction, mid_point, dist_two_points, wrap_angles)
 
 class L2Point:
     def __init__(self, id_, metric_point, geo_point, type_, point_subtype):
@@ -21,9 +23,27 @@ class L2Linestring:
         self.subtype = subtype
         
         self.lanelet_references = []
+
+        self.interpolator = None
+        self.cubic_spline = None
+        self.spline_heading = None
     
     def add_reference(self, lanelet_id):
         self.lanelet_references.append(lanelet_id)
+
+    def _interpolate(self, step=1):
+        coords = np.array(list(self.linestring.coords))
+        coords_sorted = coords[np.argsort(coords[:, 0])]
+        
+        self.interpolator = CubicSpline(coords_sorted[:, 0], coords_sorted[:, 1])
+        
+        # interpolate
+        num_grids = np.abs((coords[0, 0] - coords[-1, 0]) / step).astype(int)
+        x_grid = np.linspace(coords[0, 0], coords[-1, 0], num_grids)
+        y_grid = self.interpolator(x_grid)
+        self.cubic_spline = np.stack([x_grid, y_grid]).T
+        self.spline_heading = get_heading(x_grid[:-1], y_grid[:-1], x_grid[1:], y_grid[1:])
+        self.spline_heading = np.insert(self.spline_heading, -1, self.spline_heading[-1])
 
      
 class L2Polygon:
@@ -324,3 +344,67 @@ class Lane:
         self.left_bound = L2Linestring(self.id_, left_bound_linestr, None, None)
         self.right_bound = L2Linestring(self.id_, right_bound_linestr, None, None)
         self.centerline = L2Linestring(self.id_, centerline_linestr, None, None)
+        
+        self.left_bound._interpolate(step=1)
+        self.right_bound._interpolate(step=1)
+        self.centerline._interpolate(step=1)
+    
+    def get_frenet_coords(self, x, y):
+        """ Get the lane centric frenet coordinates of point (x, y)
+
+        Args:
+            x (float): x coord of the target point
+            y (float): y coord of the target point
+        
+        Returns:
+            x_tan (float): x coord of the tangent point 
+            y_tan (float): y coord of the tangent point
+            psi_tan (float): heading of the trangent line
+            centerline_dist (float): signed distance to the center line. 
+                Left to the center line is positive
+            left_bound_dist (float): signed distance to the left bound. 
+                Left to the left bound is positive
+            right_bound_dist (float): signed distance to the right bound. 
+                Left to the right bound is positive
+        """
+        # find the tangent point
+        p = Point([x, y])
+        x_grid = np.linspace(x - 10, x + 10, 50) 
+        y_grid = self.centerline.interpolator(x_grid)
+        grid_linestr = LineString(np.stack([x_grid, y_grid]).T)
+        p_proj = grid_linestr.interpolate(grid_linestr.project(p))
+        x_tan, y_tan = p_proj.x, p_proj.y
+        
+        # get the heading of the closet point on centerline
+        centerline_spline = self.centerline.cubic_spline
+        centerline_heading = self.centerline.spline_heading
+        d = dist_two_points(x_tan, y_tan, centerline_spline[:, 0], centerline_spline[:, 1])
+        psi_adj = centerline_heading[np.argmin(d)]
+        
+        # get tanget line heading
+        slope = self.centerline.interpolator(p_proj.x, 1)
+        psi_tan = np.arctan2(slope, 1)
+        if np.abs(wrap_angles(psi_tan - psi_adj)) > np.deg2rad(30):
+            psi_tan = np.arctan2(slope, -1)
+
+        # get the normal line
+        slope_norm = -1 / slope
+        normal_linestring = LineString([
+            (x_tan - 10, y_tan - 10 * slope_norm), (x_tan + 10, y_tan + 10 * slope_norm)
+        ])
+        left_bound_pt = normal_linestring.intersection(self.left_bound.linestring)
+        right_bound_pt = normal_linestring.intersection(self.right_bound.linestring)
+        
+        # get signed lane distances
+        centerline_dist = dist_two_points(x, y, x_tan, y_tan)
+        left_bound_dist = dist_two_points(x, y, left_bound_pt.x, left_bound_pt.y)
+        right_bound_dist = dist_two_points(x, y, right_bound_pt.x, right_bound_pt.y)
+
+        centerline_card = get_cardinal_direction(x_tan, y_tan, psi_tan, x, y)
+        left_bound_card = get_cardinal_direction(left_bound_pt.x, left_bound_pt.y, psi_tan, x, y)
+        right_bound_card = get_cardinal_direction(right_bound_pt.x, right_bound_pt.y, psi_tan, x, y)
+        
+        centerline_dist *= 2 * np.heaviside(centerline_card, 1) - 1
+        left_bound_dist *= 2 * np.heaviside(left_bound_card, 1) - 1
+        right_bound_dist *= 2 * np.heaviside(right_bound_card, 1) - 1
+        return x_tan, y_tan, psi_tan, centerline_dist, left_bound_dist, right_bound_dist
