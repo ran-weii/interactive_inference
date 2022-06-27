5260e4c64f71af7cfb09ea5ccad374d48781a278
Wei Ran <rw422@tamu.edu>
2022-06-27 10:34:11 -0500

    update invalid_lane_ids argument parsing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,7 +1,5 @@
 import argparse
 import os
-import glob
-import time
 import pickle
 import numpy as np
 import pandas as pd
@@ -10,8 +8,7 @@ from tqdm.auto import tqdm
 
 from src.map_api.lanelet import MapReader
 from src.data.kalman_filter import BatchKalmanFilter
-from src.data.agent_filter import (
-    get_neighbor_vehicle_ids, get_car_following_episode)
+from src.data.agent_filter import get_neighbor_vehicle_ids
 from src.data.data_filter import (
     get_trajectory_segment_id, filter_segment_by_length,
     classify_tail_merging)
@@ -34,23 +31,16 @@ def parse_args():
     parser.add_argument("--kf_path", type=str, default="../exp/kalman_filter")
     parser.add_argument("--scenario", type=str, default="DR_CHN_Merging_ZS")
     parser.add_argument("--filename", type=str, default="vehicle_tracks_007.csv", 
-        help="track filename. default=vehicle_tracks_007")
+        help="track filename. default=vehicle_tracks_007.csv")
     parser.add_argument("--task", type=str, choices=["kalman_filter", "neighbors", "features", "train_labels"])
-    # parser.add_argument("--track_id", type=str, default="007", help="track id. Use all if process all. default=007")
-    # parser.add_argument("--use_kf", type=bool_, default=True, 
-    #                     help="use kalman filter, default=False")
-    # parser.add_argument("--frame", type=str, choices=["frenet", "carte"], default="frenet", 
-    #                     help="lane matching frrame, one of [frenet, carte], default=frenet")
     parser.add_argument("--cell_len", type=float, default=10, 
                         help="length of drivable cells, default=10")
-    # parser.add_argument("--max_cells", type=int, default=5, 
-    #                     help="maximum number of lookahead cells, default=5")
     parser.add_argument("--max_dist", type=float, default=50., 
                         help="max dist to be considered neighbor, default=50")
     parser.add_argument("--min_seg_len", type=int, default=50, 
                         help="minimum trajectory segment length, default=50")
-    parser.add_argument("--invalid_lane_ids", type=list, default=[1, 6], 
-                        help="invalid lane ids to be filtered, default=[1, 6]")
+    parser.add_argument("--invalid_lane_ids", type=str, default="1,6", 
+                        help="invalid lane ids to be filtered. str no space separated by comma default=1,6")
     parser.add_argument("--train_ratio", type=float, default=0.7, 
                         help="train ratio, default=0.7")
     parser.add_argument("--parallel", type=bool_, default=True, 
@@ -95,81 +85,12 @@ def smooth_one_track(df_one_track, kf):
     )
     return df_s_mean
 
-# def match_lane(map_data, x, y, max_cells, frame="frenet"):
-#     """ Match point (x, y) to lane """
-#     if frame == "carte":
-#         (lane_id, cell_id, left_bound_dist, right_bound_dist, 
-#         center_line_dist, cell_headings) = map_data.match(x, y, max_cells=max_cells)
-#         map_obs = {
-#             "lane_id": lane_id, 
-#             "cell_id": cell_id, 
-#             "left_bound_dist": left_bound_dist,
-#             "right_bound_dist": right_bound_dist,
-#             "center_line_dist": center_line_dist
-#         }
-#         cell_heading_dict = {f"cell_heading_{i}": cell_headings[i][2] for i in range(max_cells)}
-#     elif frame == "frenet":
-#         (lane_id, psi_tan, center_line_dist, left_bound_dist, right_bound_dist,
-#         cell_coords, cell_headings) = map_data.match_frenet(x, y, max_cells=max_cells)
-#         map_obs = {
-#             "lane_id": lane_id, 
-#             "psi_tan": psi_tan, 
-#             "left_bound_dist": left_bound_dist,
-#             "right_bound_dist": right_bound_dist,
-#             "center_line_dist": center_line_dist
-#         }
-#         cell_heading_dict = {f"cell_heading_{i}": cell_headings[i] for i in range(max_cells)}
-#     map_obs.update(cell_heading_dict)
-#     df_lane_pos = pd.Series(map_obs)
-#     return df_lane_pos
-
 def parallel_apply(df, f, parallel, axis=1, desc=""):
     if parallel:
         return df.swifter.progress_bar(True, desc=desc).apply(f, axis=axis)
     else:
         print(desc)
         return df.progress_apply(f, axis=axis)
-
-# def preprocess_pipeline(df_track, map_data, max_cells, max_dist, dt, kf_filter, parallel=False):
-#     """ Preprocess pipeline: 
-#     1. Identify ego lane
-#     2. Identify lead vehicle id
-#     3. Identify car following episode
-#     4. Derive acceleration
-#     5. Filter vehicle dynamics
-#     """
-#     df_track["psi_rad"] = np.clip(df_track["psi_rad"], -np.pi, np.pi)
-    
-#     # identify ego lane
-#     f_ego_lane = lambda x: match_lane(map_data, x["x"], x["y"], max_cells, frame=arglist.frame)
-#     df_ego_lane = parallel_apply(
-#         df_track, f_ego_lane, parallel, axis=1, desc="identify ego lane"
-#     )
-#     df_track = pd.concat([df_track, df_ego_lane], axis=1)
-    
-#     # identify lead vehicle
-#     f_neighbor_vehicle = lambda x: get_neighbor_vehicle_ids(x, df_track, map_data, max_dist)
-#     df_lead_vehicle = parallel_apply(
-#         df_track, f_neighbor_vehicle, parallel, axis=1, desc="identify lead vehicle"
-#     ).reset_index(drop=True)
-    
-#     print("identify car following episode")
-#     df_processed = pd.concat([df_ego_lane, df_lead_vehicle], axis=1)
-#     df_processed.insert(0, "track_id", df_track["track_id"])
-#     df_processed.insert(1, "frame_id", df_track["frame_id"])
-#     df_processed["car_follow_eps"] = get_car_following_episode(df_processed)
-    
-#     print("derive acceleration")
-#     df_track = derive_acc(df_track, dt)
-#     df_processed["ax"] = df_track["vx_grad"]
-#     df_processed["ay"] = df_track["vy_grad"]
-    
-#     print("filter vehicle dynamics")
-#     df_track_smooth = df_track.groupby("track_id").progress_apply(
-#             lambda x: smooth_one_track(x, kf_filter)
-#     ).reset_index().drop(columns=["level_1", "track_id"])
-#     df_processed = pd.concat([df_processed, df_track_smooth], axis=1)
-#     return df_processed
 
 def run_kalman_filter(df, dt, kf_filter):
     """ Return dataframe with fields 
@@ -342,7 +263,7 @@ def get_train_labels(df, train_ratio, min_seg_len, invalid_lane_ids):
 
 def main(arglist):
     np.random.seed(arglist.seed)
-
+    
     # make save path
     data_path = os.path.join(arglist.data_path, "processed_trackfiles")
     task_path = os.path.join(data_path, arglist.task)
@@ -371,14 +292,14 @@ def main(arglist):
     if arglist.task == "kalman_filter":
         if arglist.debug:
             df = df.loc[df["track_id"] <= 30]
-
+    
         # load kalman filter 
         with open(os.path.join(arglist.kf_path, "model.p"), "rb") as f:
             kf = pickle.load(f)
             dt = 0.1
         
         df = run_kalman_filter(df, dt, kf)
-
+    
     elif arglist.task == "neighbors":
         map_data = MapReader(cell_len=arglist.cell_len)
         map_data.parse(map_path, verbose=True)
@@ -387,7 +308,7 @@ def main(arglist):
     elif arglist.task == "features":
         map_data = MapReader(cell_len=arglist.cell_len)
         map_data.parse(map_path, verbose=True)
-
+        
         # load neighbor df
         kf_path = os.path.join(
             data_path, "kalman_filter", arglist.scenario, arglist.filename
@@ -401,7 +322,7 @@ def main(arglist):
         df = df.merge(df_neighbor, on=["track_id", "frame_id"], how="right")
         if arglist.debug:
             df = df.loc[df["track_id"] <= 30]
-
+        
         df = compute_features(df, map_data, arglist.min_seg_len, arglist.parallel)
     
     elif arglist.task == "train_labels":
@@ -418,83 +339,15 @@ def main(arglist):
         if arglist.debug:
             df = df.loc[df["track_id"] <= 30]
         
+        invalid_lane_ids = [int(i) for i in arglist.invalid_lane_ids.split(",")]
         df = get_train_labels(
-            df, arglist.train_ratio, arglist.min_seg_len, arglist.invalid_lane_ids
+            df, arglist.train_ratio, arglist.min_seg_len, invalid_lane_ids
         )
-    """ TODO: properly parse invalid_lane_ids for other maps """
+    
     if arglist.save:
         df.to_csv(os.path.join(save_path, arglist.filename), index=False)
-    
-    print(f"processed file saved at: {save_path}/{arglist.filename}")
-
-# def main(arglist):
-#     # make save path
-#     save_path = os.path.join(arglist.data_path, "processed_trackfiles")
-#     if not os.path.exists(save_path):
-#         os.mkdir(save_path)
         
-#     # load kalman filter 
-#     with open(os.path.join(arglist.kf_path, "model.p"), "rb") as f:
-#         kf = pickle.load(f)
-#         dt = 0.1
-    
-#     # load data
-#     scenario_paths = glob.glob(
-#         os.path.join(arglist.data_path, "recorded_trackfiles", "*/")
-#     )
-    
-#     num_processed = 0
-#     for i, scenario_path in enumerate(scenario_paths):
-#         scenario = os.path.basename(os.path.dirname(scenario_path))
-        
-#         if arglist.scenario in scenario:
-#             print(f"\nScenario {i+1}: {scenario}")
-            
-#             track_paths = glob.glob(
-#                 os.path.join(scenario_path, "*.csv")
-#             )
-#             if arglist.track_id != "all":
-#                 track_paths = [p for p in track_paths if arglist.track_id in p]
-            
-#             map_data = MapReader(cell_len=arglist.cell_len)
-#             map_data.parse(
-#                 os.path.join(arglist.data_path, "maps", f"{scenario}.osm"), 
-#                 verbose=True
-#             )
-        
-#             for j, track_path in enumerate(track_paths):
-#                 track_filename = os.path.basename(track_path)
-#                 track_record_id = track_filename.replace(".csv", "").split("_")[-1]
-#                 print(f"num_processed: {num_processed}, track_file: {track_filename}")
-                
-#                 df_track = pd.read_csv(track_path)
-                
-#                 if arglist.debug:
-#                     df_track = df_track.iloc[:5000]
-                
-#                 start_time = time.time()
-#                 df_processed = preprocess_pipeline(
-#                     df_track, map_data, arglist.max_cells, 
-#                     arglist.max_dist, dt, kf, arglist.parallel
-#                 )
-#                 df_processed.insert(0, "scenario", scenario)
-#                 df_processed.insert(1, "record_id", track_record_id)
-#                 print(f"time: {np.round(time.time() - start_time, 2)}")
-                
-#                 if arglist.save:
-#                     scenario_save_path = os.path.join(save_path, scenario)
-#                     if not os.path.exists(scenario_save_path):
-#                         os.mkdir(scenario_save_path)
-                        
-#                     df_processed.to_csv(
-#                         os.path.join(scenario_save_path, track_filename), index=False
-#                     )
-            
-#                 num_processed += 1
-#                 if num_processed > 0:
-#                     return 
-#                 if arglist.debug and num_processed > 0:
-#                     return
+        print(f"processed file saved at: {save_path}/{arglist.filename}")
 
 if __name__ == "__main__":
     arglist = parse_args()
