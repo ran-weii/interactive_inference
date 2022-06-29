13916ca2bb15ac42e37bfeb8e25047d15489d1f2
Wei Ran <rw422@tamu.edu>
2022-06-28 22:01:49 -0500

    track utils.py

---
+++ src/algo/utils.py
@@ -0,0 +1,22 @@
+import time
+import pandas as pd
+
+def train(model, train_loader, test_loader, epochs, verbose=1):
+    history = []
+    start_time = time.time()
+    for e in range(epochs):
+        train_stats = model.run_epoch(train_loader, train=True)
+        test_stats = model.run_epoch(test_loader, train=False)
+        
+        tnow = time.time() - start_time
+        train_stats.update({"epoch": e, "time": tnow})
+        test_stats.update({"epoch": e, "time": tnow})
+        history.append(train_stats)
+        history.append(test_stats)
+
+        if (e + 1) % verbose == 0:
+            s = model.stdout(train_stats, test_stats)
+            print("e: {}, {}, t: {:.2f}".format(e + 1, s, tnow))
+
+    df_history = pd.DataFrame(history)
+    return model, df_history
