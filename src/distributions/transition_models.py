a55f1414923a0160c620c55089b95296b7d2c47f
Wei Ran <rw422@tamu.edu>
2022-06-29 17:59:26 -0500

    commit cp and tucker decomposition tensors, update DiscreteMC

--- src/distributions/transition_models.py
+++ src/distributions/transition_models.py
@@ -1,6 +1,49 @@
 import torch
 import torch.nn as nn
 
+class CPDecomposition(nn.Module):
+    """ 3D matrix created using CP tensor decomposition """
+    def __init__(self, state_dim, act_dim, rank):
+        super().__init__()
+        self.e = nn.Parameter(torch.randn(1, act_dim, 1, 1, rank)) # action embedding
+        self.w1 = nn.Parameter(torch.randn(1, 1, state_dim, 1, rank)) # source state embedding
+        self.w2 = nn.Parameter(torch.randn(1, 1, 1, state_dim, rank)) # sink state embedding
+
+        nn.init.xavier_normal_(self.w1, gain=1.)
+        nn.init.xavier_normal_(self.w2, gain=1.)
+        nn.init.xavier_normal_(self.e, gain=1.)
+    
+    def forward(self):
+        """ Return matrix size=[1, act_dim, state_dim, state_dim] """
+        w = torch.sum(self.w1 * self.w2 * self.e, dim=-1)
+        return w
+
+
+class TuckerDecomposition(nn.Module):
+    """ 3D matrix created using Tucker decomposition """
+    def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim):
+        super().__init__()
+        self.state_embedding = nn.Parameter(torch.randn(1, state_dim, state_embed_dim))
+        self.act_embedding = nn.Parameter(torch.randn(1, act_dim, act_embed_dim))
+        self.core_tensor = nn.Parameter(torch.randn(1, act_embed_dim, state_embed_dim, state_embed_dim))
+        
+        nn.init.xavier_normal_(self.state_embedding, gain=1.)
+        nn.init.xavier_normal_(self.act_embedding, gain=1.)
+        nn.init.xavier_normal_(self.core_tensor, gain=1.)
+    
+    def forward(self):
+        """ Return matrix size=[1, act_dim, state_dim, state_dim] """
+        source_embedding = self.state_embedding.unsqueeze(-1).unsqueeze(-4)
+        sink_embedding = self.state_embedding.transpose(-1, -2).unsqueeze(-3).unsqueeze(-3)
+        act_embedding = self.act_embedding.unsqueeze(-1).unsqueeze(-1)
+        core_tensor = self.core_tensor.unsqueeze(-3)
+        
+        out = torch.sum(source_embedding * core_tensor, dim=-2)
+        out = torch.sum(out.unsqueeze(-1) * sink_embedding, dim=-2)
+        out = torch.sum(out.unsqueeze(-4) * act_embedding, dim=-3)
+        return out
+
+
 class DiscreteMC(nn.Module):
     """ Discrete Markov chain for discrete actions """
     def __init__(self, state_dim, act_dim, rank=0):
@@ -23,13 +66,8 @@ class DiscreteMC(nn.Module):
             self.w = nn.Parameter(torch.randn(1, act_dim, state_dim, state_dim))
             nn.init.xavier_normal_(self.w, gain=1.)
         else:
-            self.e = nn.Parameter(torch.randn(1, act_dim, 1, 1, rank))
-            self.w1 = nn.Parameter(torch.randn(1, 1, state_dim, 1, rank))
-            self.w2 = nn.Parameter(torch.randn(1, 1, 1, state_dim, rank))
+            self.w = CPDecomposition(state_dim, act_dim, rank)
 
-            nn.init.xavier_normal_(self.w1, gain=1.)
-            nn.init.xavier_normal_(self.w2, gain=1.)
-            nn.init.xavier_normal_(self.e, gain=1.)
     def __repr__(self):
         s = "{}(rank={})".format(self.__class__.__name__, self.rank)
         return s
@@ -50,7 +88,7 @@ class DiscreteMC(nn.Module):
         if self.rank == 0:
             w = self.w
         else:
-            w = torch.sum(self.w1 * self.w2 * self.e, dim=-1)
+            w = self.w()
         return torch.softmax(w, dim=-1)
 
 
