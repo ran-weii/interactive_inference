7a0cd8c29eda8609138893362e8141cc48c23689
Wei Ran <rw422@tamu.edu>
2022-06-29 14:33:35 -0500

    commit plot_time_series

--- src/visualization/utils.py
+++ src/visualization/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -36,5 +37,44 @@ def plot_history(df_history, plot_keys):
         ax[i].set_ylabel(plot_keys[i])
         ax[i].grid()
     
+    plt.tight_layout()
+    return fig, ax
+
+def plot_time_series(x, feature_names, x_sample=None, num_cols=5, figsize=(6, 2)):
+    """ Plot time series features for a single episode
+    
+    Args:
+        x (torch.tensor): features. size=[T, f_dim]
+        feature_names (list): list of feature names.
+        x_sample (torch.tensor, optional): samples of x to plot error shade. 
+            size=[num_samples, T, f_dim]. Default=None
+        num_cols (int, optional): number of columns. Default=5
+        figsize (tuple, optional): figure size. Default=(6, 2)
+    
+    Returns:
+        fig (plt.figure): figure object
+        ax (plt.axes): flattened axes object
+    """
+    assert len(x.shape) == 2
+    assert x.shape[-1] == len(feature_names)
+    
+    f_dim = x.shape[-1]
+    num_cols = min(f_dim, num_cols)
+    num_rows = np.ceil(f_dim / num_cols).astype(int)
+    fig, ax = plt.subplots(num_rows, num_cols, figsize=figsize)
+    if len(ax) == 1:
+        ax = [ax]
+    else:
+        ax = ax.flat
+    
+    t = np.arange(len(x))
+    for i in range(f_dim):
+        ax[i].plot(t, x[:, i])
+        if x_sample is not None:
+            mu = x_sample[:, :, i].mean(0)
+            std = x_sample[:, :, i].std(0)
+            ax[i].fill_between(t, mu + std, mu - std, alpha=0.4)
+        ax[i].set_xlabel("time")
+        ax[i].set_title(feature_names[i])
     plt.tight_layout()
     return fig, ax
