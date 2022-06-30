620109dda18f8cac22f9bbcbdaec5a13c83f5f62
Wei Ran <rw422@tamu.edu>
2022-06-30 10:18:39 -0500

    add title to plots

--- src/visualization/utils.py
+++ src/visualization/utils.py
@@ -40,7 +40,7 @@ def plot_history(df_history, plot_keys):
     plt.tight_layout()
     return fig, ax
 
-def plot_time_series(x, feature_names, x_sample=None, num_cols=5, figsize=(6, 2)):
+def plot_time_series(x, feature_names, x_sample=None, num_cols=5, figsize=(6, 2), title=""):
     """ Plot time series features for a single episode
     
     Args:
@@ -50,6 +50,7 @@ def plot_time_series(x, feature_names, x_sample=None, num_cols=5, figsize=(6, 2)
             size=[num_samples, T, f_dim]. Default=None
         num_cols (int, optional): number of columns. Default=5
         figsize (tuple, optional): figure size. Default=(6, 2)
+        title (str, optional): title to be set on ax[0]
     
     Returns:
         fig (plt.figure): figure object
@@ -76,10 +77,11 @@ def plot_time_series(x, feature_names, x_sample=None, num_cols=5, figsize=(6, 2)
             ax[i].fill_between(t, mu + std, mu - std, alpha=0.4)
         ax[i].set_xlabel("time")
         ax[i].set_title(feature_names[i])
+    ax[0].set_title(title)
     plt.tight_layout()
     return fig, ax
 
-def plot_scatter(x, x_label=None, y_label=None, figsize=(6, 6)):
+def plot_scatter(x, x_label=None, y_label=None, figsize=(6, 6), title=""):
     """ Create a single scatter plot
 
     Args:
@@ -94,4 +96,5 @@ def plot_scatter(x, x_label=None, y_label=None, figsize=(6, 6)):
     ax.plot(x[0].T, x[1].T, "o", ms=2)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    ax.set_title(title)
     return fig, ax
