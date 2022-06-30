c473ee159f37e2d928baca2ef09cf93f2cfb82f2
Wei Ran <rw422@tamu.edu>
2022-06-29 21:44:37 -0500

    commit tensor decomposition classes

--- src/distributions/transition_models.py
+++ src/distributions/transition_models.py
@@ -9,9 +9,9 @@ class CPDecomposition(nn.Module):
         self.w1 = nn.Parameter(torch.randn(1, 1, state_dim, 1, rank)) # source state embedding
         self.w2 = nn.Parameter(torch.randn(1, 1, 1, state_dim, rank)) # sink state embedding
 
-        nn.init.xavier_normal_(self.w1, gain=1.)
-        nn.init.xavier_normal_(self.w2, gain=1.)
-        nn.init.xavier_normal_(self.e, gain=1.)
+        # nn.init.xavier_normal_(self.w1, gain=1.)
+        # nn.init.xavier_normal_(self.w2, gain=1.)
+        # nn.init.xavier_normal_(self.e, gain=1.)
     
     def forward(self):
         """ Return matrix size=[1, act_dim, state_dim, state_dim] """
@@ -21,22 +21,38 @@ class CPDecomposition(nn.Module):
 
 class TuckerDecomposition(nn.Module):
     """ 3D matrix created using Tucker decomposition """
-    def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim):
+    def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim, core_rank=0):
         super().__init__()
+        self.core_rank = core_rank
+        
         self.state_embedding = nn.Parameter(torch.randn(1, state_dim, state_embed_dim))
         self.act_embedding = nn.Parameter(torch.randn(1, act_dim, act_embed_dim))
-        self.core_tensor = nn.Parameter(torch.randn(1, act_embed_dim, state_embed_dim, state_embed_dim))
         
         nn.init.xavier_normal_(self.state_embedding, gain=1.)
         nn.init.xavier_normal_(self.act_embedding, gain=1.)
-        nn.init.xavier_normal_(self.core_tensor, gain=1.)
+
+        if core_rank == 0:
+            self.w = nn.Parameter(torch.randn(1, act_embed_dim, state_embed_dim, state_embed_dim))
+            nn.init.xavier_normal_(self.w, gain=1.)
+        else:
+            self.w = CPDecomposition(
+                state_embed_dim, act_embed_dim, core_rank
+            )
+    
+    @property
+    def core(self):
+        if self.core_rank == 0:
+            w = self.w
+        else:
+            w = self.w()
+        return w
     
     def forward(self):
         """ Return matrix size=[1, act_dim, state_dim, state_dim] """
         source_embedding = self.state_embedding.unsqueeze(-1).unsqueeze(-4)
         sink_embedding = self.state_embedding.transpose(-1, -2).unsqueeze(-3).unsqueeze(-3)
         act_embedding = self.act_embedding.unsqueeze(-1).unsqueeze(-1)
-        core_tensor = self.core_tensor.unsqueeze(-3)
+        core_tensor = self.core.unsqueeze(-3)
         
         out = torch.sum(source_embedding * core_tensor, dim=-2)
         out = torch.sum(out.unsqueeze(-1) * sink_embedding, dim=-2)
@@ -89,6 +105,56 @@ class DiscreteMC(nn.Module):
             w = self.w
         else:
             w = self.w()
+        return torch.softmax(w, dim=-1)
+
+
+class EmbeddedDiscreteMC(nn.Module):
+    """ Discrete Markov chain for discrete actions with embeddings """
+    def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim, rank=0):
+        """
+        Args:
+            state_dim (int): state dimension
+            act_dim (int): discrete action dimension
+            state_embed_dim (int): state embedding dimension
+            act_embed_dim (int): act embedding dimension
+            rank (int, optional): core tensor rank. Dense matrix if rank=0, 
+                else use CP tensor decomposition. Default=0
+        """
+        super().__init__()
+        self.state_dim = state_dim
+        self.act_dim = act_dim
+        self.state_embed_dim = state_embed_dim
+        self.act_embed_dim = act_embed_dim
+        self.rank = rank
+        
+        self.s0 = nn.Parameter(torch.randn(1, state_dim))
+        nn.init.xavier_normal_(self.s0, gain=1.)
+        
+        self.w = TuckerDecomposition(
+            state_dim, act_dim, state_embed_dim, act_embed_dim, core_rank=rank
+        )
+
+    def __repr__(self):
+        s = "{}(state_embedding={}, act_embedding={}, core_rank={})".format(
+            self.__class__.__name__, self.state_embed_dim, 
+            self.act_embed_dim, self.rank
+        )
+        return s
+
+    def get_initial_state(self):
+        """
+        Returns:
+            s0 (torch.tensor): initial state distribution. size=[batch_size, state_dim]
+        """
+        s0 = torch.softmax(self.s0.view(-1, self.state_dim), dim=-1)
+        return s0
+    
+    def get_transition_matrix(self):
+        """
+        Returns:
+            w (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+        """
+        w = self.w()
         return torch.softmax(w, dim=-1)
 
 
