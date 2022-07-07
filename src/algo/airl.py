0738cf9ebcf189ff0788b07496058c98af56114f
Wei Ran <rw422@tamu.edu>
2022-07-07 18:29:53 -0500

    update AIRL with state based discriminator and value

--- src/algo/airl.py
+++ src/algo/airl.py
@@ -5,9 +5,15 @@ from copy import deepcopy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from src.distributions.nn_models import Model, GRUMLP
+
+# rollout imports
 from src.evaluation.online import eval_episode
-from src.simulation.controllers import AgentWrapper
+from src.simulation.controllers import AgentWrapper, DataWrapper
+
+# model imports
+from src.distributions.nn_models import Model, MLP
+from src.algo.rl_utils import ReplayBuffer
+
 
 class AIRL(Model):
     """ Adversarial IRL algorithm """
@@ -25,6 +31,7 @@ class AIRL(Model):
         buffer_size=1e5, 
         rnn_steps=10, 
         batch_size=128, 
+        max_eps_len=200,
         d_steps=10,
         ac_steps=10,
         lr_agent=1e-3, 
@@ -55,6 +62,7 @@ class AIRL(Model):
         self.gamma = gamma
         self.rnn_steps = rnn_steps
         self.batch_size = batch_size
+        self.max_eps_len = max_eps_len
         self.d_steps = d_steps
         self.ac_steps = ac_steps
         self.lr_agent = lr_agent
@@ -66,33 +74,31 @@ class AIRL(Model):
         
         self.agent = agent
         self.real_buffer = ReplayBuffer(
-            agent.obs_dim, agent.ctl_dim, buffer_size, rnn_steps
+            agent.state_dim, agent.ctl_dim, agent.obs_dim, buffer_size
         )
         self.fake_buffer = ReplayBuffer(
-            agent.obs_dim, agent.ctl_dim, buffer_size, rnn_steps
+            agent.state_dim, agent.ctl_dim, agent.obs_dim, buffer_size
         )
-        self.discriminator = GRUMLP(
-            input_dim=agent.obs_dim + agent.ctl_dim, 
-            output_dim=1, 
-            hidden_dim=hidden_dim, 
-            gru_layers=gru_layers, 
-            mlp_layers=mlp_layers, 
+        input_dim = self.agent.state_dim + self.agent.obs_dim + self.agent.ctl_dim
+        self.discriminator = MLP(
+            input_dim=input_dim,
+            output_dim=1,
+            hidden_dim=hidden_dim,
+            num_hidden=mlp_layers,
             activation="silu"
         )
-        self.q1 = GRUMLP(
-            input_dim=agent.obs_dim + agent.ctl_dim, 
-            output_dim=1, 
-            hidden_dim=hidden_dim, 
-            gru_layers=gru_layers, 
-            mlp_layers=mlp_layers, 
+        self.q1 = MLP(
+            input_dim=input_dim,
+            output_dim=1,
+            hidden_dim=hidden_dim,
+            num_hidden=mlp_layers,
             activation="silu"
         )
-        self.q2 = GRUMLP(
-            input_dim=agent.obs_dim + agent.ctl_dim, 
-            output_dim=1, 
-            hidden_dim=hidden_dim, 
-            gru_layers=gru_layers, 
-            mlp_layers=mlp_layers, 
+        self.q2 = MLP(
+            input_dim=input_dim,
+            output_dim=1,
+            hidden_dim=hidden_dim,
+            num_hidden=mlp_layers,
             activation="silu"
         )
         self.q1_target = deepcopy(self.q1)
@@ -115,7 +121,7 @@ class AIRL(Model):
         s = "{}(buffer_size={}, rnn_steps={}, batch_size={}, "\
             "lr_agent={}, lr_d={}, lr_q={}, decay={}, grad_clip={}, "\
             "d_steps={}, ac_steps={},\nagent={})".format(
-            self.__class__.__name__, self.fake_buffer.max_size, self.fake_buffer.rnn_steps, 
+            self.__class__.__name__, self.fake_buffer.max_size, self.rnn_steps, 
             self.batch_size, self.lr_agent, self.lr_d, self.lr_q, self.decay, self.grad_clip,
             self.d_steps, self.ac_steps, s_agent
         )
@@ -130,19 +136,24 @@ class AIRL(Model):
     def normalize(self, x, mean, var):
         """ Normalize a batch of sequences """
         std = torch.sqrt(var)
-        x_ = (x - mean.view(1, 1, -1)) / std.view(1, 1, -1)
+        x_ = (x - mean.view(1, -1)) / std.view(1, -1)
         return x_
 
-    def discriminator_loss(self, real_batch, fake_batch):
+    def discriminator_loss(self):
+        real_batch = self.real_buffer.sample_random(self.batch_size)
+        fake_batch = self.fake_buffer.sample_random(self.batch_size)
+
+        real_state = real_batch["state"]
         real_obs = self.normalize(real_batch["obs"], self.obs_mean, self.obs_var)
         real_act = self.normalize(real_batch["act"], self.act_mean, self.act_var)
-
+        
+        fake_state = fake_batch["state"]
         fake_obs = self.normalize(fake_batch["obs"], self.obs_mean, self.obs_var)
         fake_act = self.normalize(fake_batch["act"], self.act_mean, self.act_var)
         
-        real_inputs = torch.cat([real_obs, real_act], dim=-1)
-        fake_inputs = torch.cat([fake_obs, fake_act], dim=-1)
-        inputs = torch.cat([real_inputs, fake_inputs], dim=1)
+        real_inputs = torch.cat([real_state, real_obs, real_act], dim=-1)
+        fake_inputs = torch.cat([fake_state, fake_obs, fake_act], dim=-1)
+        inputs = torch.cat([real_inputs, fake_inputs], dim=0)
 
         real_labels = torch.zeros(self.batch_size, 1)
         fake_labels = torch.ones(self.batch_size, 1)
@@ -152,20 +163,24 @@ class AIRL(Model):
         loss = F.binary_cross_entropy(out, labels)
         return loss
     
-    def critic_loss(self, fake_batch):
+    def critic_loss(self):
+        fake_batch = self.fake_buffer.sample_random(self.batch_size)
+
+        state = fake_batch["state"]
         obs = self.normalize(fake_batch["obs"], self.obs_mean, self.obs_var)
         act = self.normalize(fake_batch["act"], self.act_mean, self.act_var)
+        next_state = fake_batch["next_state"]
         next_obs = self.normalize(fake_batch["next_obs"], self.obs_mean, self.obs_var)
-        next_act = torch.cat([act[1:], torch.zeros(1, self.batch_size, self.agent.ctl_dim)], dim=0)
-        inputs = torch.cat([obs, act], dim=-1)
+
+        # sample next action
+        with torch.no_grad():
+            self.agent.reset()
+            next_alpha_a = self.agent.plan(next_state).unsqueeze(0)
+            next_act = self.agent.hmm.ctl_ancestral_sample(next_alpha_a, 1).view(self.batch_size, -1)
+        inputs = torch.cat([state, obs, act], dim=-1)
+        next_inputs = torch.cat([next_state, next_obs, next_act], dim=-1)
         
         with torch.no_grad():
-            # sample next action
-            self.agent.reset()
-            out = self.agent.choose_action_batch(next_obs, next_act).squeeze(0)
-            next_act = torch.cat([act[1:], out[-1].unsqueeze(0)], dim=0)
-            next_inputs = torch.cat([next_obs, next_act], dim=-1)
-
             # compute reward
             log_r = self.discriminator(inputs)
             
@@ -182,49 +197,46 @@ class AIRL(Model):
         q_loss = (q1_loss + q2_loss) / 2
         return q_loss
     
-    def actor_loss(self, fake_batch):
-        obs = self.normalize(fake_batch["obs"], self.obs_mean, self.obs_var)
-        act = self.normalize(fake_batch["act"], self.act_mean, self.act_var)
+    def actor_loss(self):
+        batch_size = min(self.fake_buffer.num_eps, int(self.batch_size / 3))
+        obs, ctl, mask = self.fake_buffer.sample_episodes(batch_size, self.max_eps_len)
         
-        # sample actions
-        act_sample = self.agent.hmm.ctl_model.sample((self.batch_size,)).squeeze(1)
+        self.agent.reset()
+        alpha_b, alpha_a = self.agent(obs, ctl)
         
-        # compute value for each strata
+        # compute value for each action strata
+        ctl_sample = self.agent.hmm.ctl_model.sample((len(obs), batch_size,)).squeeze(-3)
+
         q = [torch.empty(0)] * self.agent.act_dim
         for i in range(self.agent.act_dim):
-            act_i = torch.cat([act[:-1], act_sample[:, i].unsqueeze(0)], dim=0)
-            inputs_i = torch.cat([obs, act_i], dim=-1)
+            inputs_i = torch.cat([alpha_b, obs, ctl_sample[:, :, i]], dim=-1)
             q1 = self.q1(inputs_i)
             q2 = self.q2(inputs_i)
             q[i] = torch.max(q1, q2)
-        q = torch.stack(q).transpose(0, 1).squeeze(-1)
-
+        q = torch.stack(q).squeeze(-1).permute(1, 2, 0)
+        
         # compute stratified loss
-        self.agent.reset()
-        alpha_b, alpha_a = self.agent(obs, act)
-        a_loss = torch.sum(alpha_a[-1] * q, dim=-1).mean()
+        a_loss = torch.sum(alpha_a * q, dim=-1)
+        a_loss = torch.sum(a_loss * mask, dim=0) / mask.sum(0, keepdim=True)
+        a_loss = a_loss.mean()
         return a_loss
 
     def take_gradient_step(self):
         # train discriminator 
         for i in range(self.d_steps):
-            real_batch = self.real_buffer.sample_batch(self.batch_size)
-            fake_batch = self.fake_buffer.sample_batch(self.batch_size)
-            
-            d_loss = self.discriminator_loss(real_batch, fake_batch)
+            d_loss = self.discriminator_loss()
             d_loss.backward()
             if self.grad_clip is not None:
                 nn.utils.clip_grad_norm_(self.discriminator.parameters(), self.grad_clip)
             self.d_optimizer.step()
             self.d_optimizer.zero_grad()
             # print("d_loss", d_loss.data.item())
+        # exit()
         
         # train critic
         for i in range(self.ac_steps):
-            fake_batch = self.fake_buffer.sample_batch(self.batch_size)
-
             # train critic
-            q_loss = self.critic_loss(fake_batch)
+            q_loss = self.critic_loss()
             q_loss.backward()
             if self.grad_clip is not None:
                 nn.utils.clip_grad_norm_(
@@ -234,7 +246,7 @@ class AIRL(Model):
             self.q_optimizer.zero_grad()
 
             # train actor
-            a_loss = self.actor_loss(fake_batch)
+            a_loss = self.actor_loss()
             a_loss.backward()
             # for n, p in self.agent.named_parameters():
             #     if p.grad is not None:
@@ -265,71 +277,6 @@ class AIRL(Model):
         return stats
 
 
-class ReplayBuffer:
-    """ Replay buffer for recurrent policy """
-    def __init__(self, obs_dim, act_dim, max_size, rnn_steps):
-        """
-        Args:
-            obs_dim (int): observation dimension
-            act_dim (int): action dimension
-            max_size (int): maximum buffer size
-            rnn_steps (int): number of recurrent steps to store
-        """
-        self.obs_dim = obs_dim
-        self.act_dim = act_dim
-        
-        self.size = 0
-        self.max_size = max_size
-        self.rnn_steps = rnn_steps
-        
-        self.obs = np.array([]).reshape(0, rnn_steps, obs_dim)
-        self.act = np.array([]).reshape(0, rnn_steps, act_dim)
-        self.next_obs = np.array([]).reshape(0, rnn_steps, obs_dim)
-        
-        self.obs_eps = [] # store a single episode
-        self.act_eps = [] # store a single episode
-    
-    def __call__(self, controller):
-        """ Append episode history """
-        self.obs_eps.append(controller._obs.data.numpy())
-        self.act_eps.append(controller._prev_act.data.numpy())
-    
-    def push(self, obs_eps=None, act_eps=None):
-        """ Pack episode into chunk 
-        
-        Args:
-            obs_eps (np.array): array of episode observations. size=[T, obs_dim]
-            act_eps (np.array): array of episode actions. size=[T, act_dim]
-        """
-        if obs_eps is None and act_eps is None:
-            obs_eps = self.obs_eps
-            act_eps = self.act_eps
-
-        T = len(obs_eps) - self.rnn_steps - 1
-        for t in range(T):
-            obs = np.vstack(obs_eps[t:t+self.rnn_steps]).reshape(1, self.rnn_steps, self.obs_dim)
-            act = np.vstack(act_eps[t:t+self.rnn_steps]).reshape(1, self.rnn_steps, self.act_dim)
-            next_obs = np.vstack(obs_eps[t+1:t+self.rnn_steps+1]).reshape(1, self.rnn_steps, self.obs_dim)
-            
-            self.obs = np.concatenate([obs, self.obs], axis=0)
-            self.act = np.concatenate([act, self.act], axis=0)
-            self.next_obs = np.concatenate([next_obs, self.next_obs], axis=0)
-
-        if len(self.obs) > self.max_size:
-            self.obs = self.obs[:self.max_size]
-            self.act = self.act[:self.max_size]
-            self.next_obs = self.next_obs[:self.max_size]
-        self.size = len(self.obs)
-        self.obs_eps = []
-        self.act_eps = []
-    
-    def sample_batch(self, batch_size=32):
-        idx = np.random.randint(0, self.size, size=batch_size)
-        batch = dict(obs=self.obs[idx], act=self.act[idx], next_obs=self.next_obs[idx])
-        return {k: torch.from_numpy(v).transpose(0, 1).to(torch.float32) for k, v in batch.items()}
-
-
-""" TODO: finish replay buffer, define discriminator on state or observation? """
 def train(env, observer, model, dataset, action_set, num_eps, epochs, max_steps, verbose=1):
     """ Training loop for adversarial inverse reinforcment learning 
     
@@ -344,28 +291,29 @@ def train(env, observer, model, dataset, action_set, num_eps, epochs, max_steps,
         max_steps (int): maximum number of steps per episode
         verbose (int, optional): verbose interval. Default=1
     """
-
     start = time.time()
     history = []
     for e in range(epochs):
-        controller = AgentWrapper(observer, model.agent, action_set, "ace")
-        
         eps_ids = np.random.choice(np.arange(len(env.dataset)), num_eps)
         for i, eps_id in enumerate(eps_ids):
+            fake_controller = AgentWrapper(observer, model.agent, action_set, "ace")
+            real_controller = DataWrapper(dataset[eps_id], observer, model.agent, action_set, "ace")
+
             # rollout fake episode
             sim_states, sim_acts, track_data = eval_episode(
-                env, controller, eps_id, max_steps, model.fake_buffer
+                env, fake_controller, eps_id, max_steps, model.fake_buffer
             )
             model.fake_buffer.push()
-            
-            # get real episode data
-            real_obs = dataset[eps_id]["ego"].data.numpy()
-            real_act = dataset[eps_id]["act"].data.numpy()
-            model.real_buffer.push(real_obs, real_act)
+
+            # rollout real episode
+            sim_states, sim_acts, track_data = eval_episode(
+                env, real_controller, eps_id, max_steps, model.real_buffer
+            )
+            model.real_buffer.push()
         
         stats = model.take_gradient_step()
         tnow = time.time() - start
-        stats.update({"epoch": e, "time": tnow})
+        stats.update({"epoch": e, "time": tnow, "train": 1})
         history.append(stats)
 
         if (e + 1) % verbose == 0:
