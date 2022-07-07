b3e9577e32498aa083e1276a4d8dd77511b44bf7
Wei Ran <rw422@tamu.edu>
2022-07-07 14:37:31 -0500

    add callback to eval_episode for online training

--- src/evaluation/online.py
+++ src/evaluation/online.py
@@ -21,7 +21,7 @@ class Evaluator:
         self._track["loom_x"].append(obs[0, 9].item())
 
 
-def eval_episode(env, controller, eps_id, max_steps=1000):
+def eval_episode(env, controller, eps_id, max_steps=1000, callback=None):
     """ Evaluate episode
     
     Args:
@@ -29,18 +29,22 @@ def eval_episode(env, controller, eps_id, max_steps=1000):
         controller (): controller with reset and choose_action method
         eps_id (int): episode id
         max_steps (int, optional): maximum number of steps
+        callback (class, optional): controller callback
 
     Returns:
         sim_states (np.array): simulated states [T, num_agents, 5]
         sim_acts (np.array): simulated actions [T, 2]
         track_data (dict): recorded track data
+        callback (class): updated callback. return if callback is not None
     """
     controller.reset()
     obs = env.reset(eps_id)
     for t in range(max_steps):
         act = controller.choose_action(obs)
+        if callback is not None:
+            callback(controller)
         obs, _, done, _ = env.step(act)
-        if done:
+        if done or t >= max_steps:
             break
     
     sim_states = env._sim_states[:t+1]
