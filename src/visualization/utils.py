a11e3ec5f0ee8b3e9ef0385f7066a8ea0585a3cf
Wei Ran <rw422@tamu.edu>
2022-07-07 18:28:08 -0500

    update plot_hisotry() make test learning curve optional

--- src/visualization/utils.py
+++ src/visualization/utils.py
@@ -23,15 +23,18 @@ def plot_history(df_history, plot_keys):
         fig (plt.figure)
         ax (plt.axes)
     """
+    train_unique = list(df_history["train"].unique())
     df_train = df_history.loc[df_history["train"] == 1]
-    df_test = df_history.loc[df_history["train"] == 0]
+    if 0 in train_unique:
+        df_test = df_history.loc[df_history["train"] == 0]
     
     num_cols = len(plot_keys)
     width = min(4 * num_cols, 15)
     fig, ax = plt.subplots(1, num_cols, figsize=(width, 4))
     for i in range(num_cols):
         ax[i].plot(df_train["epoch"], df_train[plot_keys[i]], label="train")
-        ax[i].plot(df_test["epoch"], df_test[plot_keys[i]], label="test")
+        if 0 in train_unique:
+            ax[i].plot(df_test["epoch"], df_test[plot_keys[i]], label="test")
         ax[i].legend()
         ax[i].set_xlabel("epoch")
         ax[i].set_ylabel(plot_keys[i])
