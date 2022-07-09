ace9ede55b93457d94558ff36d577d9a99f3d98e
Wei Ran <rw422@tamu.edu>
2022-07-09 17:45:23 -0500

    remove old RecurrentBuffer and rename the new one

--- src/algo/replay_buffers.py
+++ src/algo/replay_buffers.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from src.data.ego_dataset import collate_fn, sample_sequence
 
-class ReplayBuffer:  
+class RecurrentBuffer:  
     """ Replay buffer with hidden state """  
     def __init__(self, state_dim, act_dim, obs_dim, max_size):
         """
@@ -95,66 +95,3 @@ class ReplayBuffer:
         out = collate_fn(batch)
         return out
         
-        
-class RecurrentBuffer:
-    """ Replay buffer for recurrent policy """
-    def __init__(self, obs_dim, act_dim, max_size, rnn_steps):
-        """
-        Args:
-            obs_dim (int): observation dimension
-            act_dim (int): action dimension
-            max_size (int): maximum buffer size
-            rnn_steps (int): number of recurrent steps to store
-        """
-        self.obs_dim = obs_dim
-        self.act_dim = act_dim
-        
-        self.size = 0
-        self.max_size = max_size
-        self.rnn_steps = rnn_steps
-        
-        self.obs = np.array([]).reshape(0, rnn_steps, obs_dim)
-        self.act = np.array([]).reshape(0, rnn_steps, act_dim)
-        self.next_obs = np.array([]).reshape(0, rnn_steps, obs_dim)
-        
-        self.obs_eps = [] # store a single episode
-        self.act_eps = [] # store a single episode
-    
-    def __call__(self, controller):
-        """ Append episode history """
-        self.obs_eps.append(controller._obs.data.numpy())
-        self.act_eps.append(controller._prev_act.data.numpy())
-    
-    def push(self, obs_eps=None, act_eps=None):
-        """ Pack episode into chunk 
-        
-        Args:
-            obs_eps (np.array): array of episode observations. size=[T, obs_dim]
-            act_eps (np.array): array of episode actions. size=[T, act_dim]
-        """
-        if obs_eps is None and act_eps is None:
-            obs_eps = self.obs_eps
-            act_eps = self.act_eps
-
-        T = len(obs_eps) - self.rnn_steps - 1
-        for t in range(T):
-            obs = np.vstack(obs_eps[t:t+self.rnn_steps]).reshape(1, self.rnn_steps, self.obs_dim)
-            act = np.vstack(act_eps[t:t+self.rnn_steps]).reshape(1, self.rnn_steps, self.act_dim)
-            next_obs = np.vstack(obs_eps[t+1:t+self.rnn_steps+1]).reshape(1, self.rnn_steps, self.obs_dim)
-            
-            self.obs = np.concatenate([obs, self.obs], axis=0)
-            self.act = np.concatenate([act, self.act], axis=0)
-            self.next_obs = np.concatenate([next_obs, self.next_obs], axis=0)
-
-        if len(self.obs) > self.max_size:
-            self.obs = self.obs[:self.max_size]
-            self.act = self.act[:self.max_size]
-            self.next_obs = self.next_obs[:self.max_size]
-        self.size = len(self.obs)
-        self.obs_eps = []
-        self.act_eps = []
-    
-    def sample_batch(self, batch_size=32):
-        idx = np.random.randint(0, self.size, size=batch_size)
-        batch = dict(obs=self.obs[idx], act=self.act[idx], next_obs=self.next_obs[idx])
-        return {k: torch.from_numpy(v).transpose(0, 1).to(torch.float32) for k, v in batch.items()}
