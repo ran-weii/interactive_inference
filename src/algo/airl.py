1ae195519c88d3e11c7521194cbada60cf00462d
Wei Ran <rw422@tamu.edu>
2022-07-13 10:08:36 -0500

    add obs normalization stats and repr

--- src/algo/airl.py
+++ src/algo/airl.py
@@ -11,34 +11,42 @@ from src.distributions.nn_models import Model, MLP
 from src.algo.rl import DoubleQNetwork
 from src.algo.replay_buffers import ReplayBuffer
 
-""" TODO: modify this to work with recurrent agent """
 class DAC(Model):
     """ Discriminator actor critic """
     def __init__(
-        self, agent, hidden_dim, num_hidden, gamma=0.9, beta=0.2, polyak=0.995, 
+        self, agent, hidden_dim, num_hidden, gamma=0.9, beta=0.2, polyak=0.995, norm_obs=False,
         buffer_size=int(1e6), batch_size=100, d_steps=50, a_steps=50, lr=1e-3, decay=0, 
-        grad_penalty=10, grad_clip=None
+        grad_clip=None, grad_penalty=1., 
         ):
         """
         Args:
-            agent (Agent): agent object
-            gamma (float): discount factor. Default=0.9
-            buffer_size (int): replay buffer max size. Default=1e5
-            rnn_steps (int): number of recurrent steps to store in buffer. Default=10
-            batch_size (int): training batch size. Default=128
-            lr_agent (float): agent learning rate. Default=1e-3
-            lr_algo (float): value and discriminator learning rate. Default=1e-3
-            decay (float): weight decay. Default==0
-            grad_clip (float, None): gradient clipping norm. Default=None
+            agent (Agent): actor agent
+            hidden_dim (int): value network hidden dim
+            num_hidden (int): value network hidden layers
+            gamma (float, optional): discount factor. Default=0.9
+            beta (float, optional): softmax temperature. Default=0.2
+            polyak (float, optional): target network polyak averaging factor. Default=0.995
+            norm_obs (bool, optional): whether to normalize observations for critic. Default=False
+            buffer_size (int, optional): replay buffer size. Default=1e6
+            batch_size (int, optional): training batch size. Default=100
+            d_steps (int, optional): discriminator update steps per training step. Default=50
+            a_steps (int, optional): actor critic update steps per training step. Default=50
+            lr (float, optional): learning rate. Default=1e-3
+            decay (float, optional): weight decay. Default=0
+            grad_clip (float, optional): gradient clipping. Default=None
+            grad_penalty (float, optional): discriminator gradient penalty. Default=1.
         """
         super().__init__()
         self.gamma = gamma
         self.beta = beta
         self.polyak = polyak
+        self.norm_obs = norm_obs
 
         self.batch_size = batch_size
         self.d_steps = d_steps
         self.a_steps = a_steps
+        self.lr = lr
+        self.decay = decay
         self.grad_clip = grad_clip
         self.grad_penalty = grad_penalty
 
@@ -72,25 +80,25 @@ class DAC(Model):
 
         self.real_buffer = ReplayBuffer(agent.obs_dim, agent.ctl_dim, buffer_size)
         self.replay_buffer = ReplayBuffer(agent.obs_dim, agent.ctl_dim, buffer_size)
+
+        self.obs_mean = nn.Parameter(torch.zeros(agent.obs_dim), requires_grad=False)
+        self.obs_variance = nn.Parameter(torch.ones(agent.obs_dim), requires_grad=False)
         
         self.plot_keys = ["eps_return_avg", "d_loss_avg", "critic_loss_avg", "actor_loss_avg"]
     
-    # def __repr__(self):
-    #     s_agent = self.agent.__repr__()
-    #     s = "{}(buffer_size={}, rnn_steps={}, batch_size={}, "\
-    #         "lr_agent={}, lr_d={}, lr_q={}, decay={}, grad_clip={}, "\
-    #         "d_steps={}, ac_steps={},\nagent={})".format(
-    #         self.__class__.__name__, self.replay_buffer.max_size, self.rnn_steps, 
-    #         self.batch_size, self.lr_agent, self.lr_d, self.lr_q, self.decay, self.grad_clip,
-    #         self.d_steps, self.ac_steps, s_agent
-    #     )
-    #     return s
-    
-    # def stdout(self, stats):
-    #     s = "loss_d: {:.4f}, loss_q: {:.4f}, loss_a: {:.4f}".format(
-    #         stats["loss_d"], stats["loss_q"], stats["loss_a"]
-    #     )
-    #     return s
+    def __repr__(self):
+        s_agent = self.agent.__repr__()
+        s_critic = self.critic.__repr__()
+        s_discriminator = self.discriminator.__repr__()
+        s = "{}(gamma={}, beta={}, polyak={}, norm_obs={}, "\
+            "buffer_size={}, batch_size={}, a_steps={}, d_steps={}, "\
+            "lr={}, decay={}, grad_clip={}, grad_penalty={}"\
+            "\n    discriminator={}\n    agent={}, \n    critic={}\n)".format(
+            self.__class__.__name__, self.gamma, self.beta, self.polyak, self.norm_obs,
+            self.replay_buffer.max_size, self.batch_size, self.a_steps, self.d_steps,
+            self.lr, self.decay, self.grad_clip, self.grad_penalty, s_discriminator, s_agent, s_critic
+        )
+        return s
     
     def fill_real_buffer(self, dataset):
         for i in range(len(dataset)):
@@ -102,17 +110,24 @@ class DAC(Model):
             self.real_buffer.push(obs, ctl, rwd, done)
 
     def normalize_obs(self, obs):
-        # mu = torch.from_numpy(self.replay_buffer.moving_mean).to(torch.float32)
-        # std = torch.from_numpy(self.replay_buffer.moving_variance**0.5).to(torch.float32)
-        # obs_norm = (obs - mu) / std
-        obs_norm = obs
+        obs_norm = (obs - self.obs_mean) / self.obs_variance**0.5
         return obs_norm
+    
+    def update_normalization_stats(self):
+        if self.norm_obs:
+            mean = torch.from_numpy(self.replay_buffer.moving_mean).to(torch.float32)
+            variance = torch.from_numpy(self.replay_buffer.moving_variance).to(torch.float32)
+
+            self.obs_mean.data = mean
+            self.obs_variance.data = variance
+
+            self.agent.obs_mean.data = mean
+            self.agent.obs_variance.data = variance
     
     def reset(self):
         self.agent.reset()
 
     def choose_action(self, obs):
-        obs = self.normalize_obs(obs)
         prev_ctl = self.agent._prev_ctl
 
         with torch.no_grad():
@@ -151,8 +166,12 @@ class DAC(Model):
         fake_obs = fake_batch["obs"]
         fake_ctl = fake_batch["ctl"]
         
-        real_inputs = torch.cat([real_obs, real_ctl], dim=-1)
-        fake_inputs = torch.cat([fake_obs, fake_ctl], dim=-1)
+        # normalize obs
+        real_obs_norm = self.normalize_obs(real_obs)
+        fake_obs_norm = self.normalize_obs(fake_obs)
+
+        real_inputs = torch.cat([real_obs_norm, real_ctl], dim=-1)
+        fake_inputs = torch.cat([fake_obs_norm, fake_ctl], dim=-1)
         inputs = torch.cat([real_inputs, fake_inputs], dim=0)
 
         real_labels = torch.zeros(self.batch_size, 1)
@@ -174,8 +193,8 @@ class DAC(Model):
         done = batch["done"]      
         
         # normalize observation
-        obs = self.normalize_obs(obs)
-        next_obs = self.normalize_obs(next_obs)
+        obs_norm = self.normalize_obs(obs)
+        next_obs_norm = self.normalize_obs(next_obs)
 
         # sample next action
         with torch.no_grad():
@@ -184,14 +203,14 @@ class DAC(Model):
         
         with torch.no_grad():
             # compute reward
-            r = self.compute_reward(obs, ctl)
+            r = self.compute_reward(obs_norm, ctl)
 
             # compute value target
-            q1_next, q2_next = self.critic_target(next_obs, next_ctl)
+            q1_next, q2_next = self.critic_target(next_obs_norm, next_ctl)
             q_next = torch.min(q1_next, q2_next)
             q_target = r + (1 - done) * self.gamma * (q_next - self.beta * logp)
 
-        q1, q2 = self.critic(obs, ctl)
+        q1, q2 = self.critic(obs_norm, ctl)
         q1_loss = torch.pow(q1 - q_target, 2).mean()
         q2_loss = torch.pow(q2 - q_target, 2).mean()
         q_loss = (q1_loss + q2_loss) / 2
@@ -203,12 +222,12 @@ class DAC(Model):
         ctl = batch["ctl"]
 
         # normalize observation
-        obs = self.normalize_obs(obs)
+        obs_norm = self.normalize_obs(obs)
 
         ctl_sample, logp = self.agent.choose_action(obs, ctl)
         ctl_sample = ctl_sample.squeeze(0)
         
-        q1, q2 = self.critic(obs, ctl_sample)
+        q1, q2 = self.critic(obs_norm, ctl_sample)
         q = torch.min(q1, q2)
         a_loss = torch.mean(self.beta * logp - q)
         return a_loss
@@ -216,6 +235,7 @@ class DAC(Model):
     def take_gradient_step(self, logger=None):
         self.critic.train()
         self.agent.train()
+        self.update_normalization_stats()
         
         d_loss_epoch = []
         for i in range(self.d_steps):
