15b93e4264987a0cfd6f6855cab72c90c38a3276
Wei Ran <rw422@tamu.edu>
2022-07-14 15:51:27 -0500

    remove embedded conditional gaussian test case

--- tests/test_prob_models.py
+++ tests/test_prob_models.py
@@ -94,38 +94,38 @@ def test_logistic_gaussian_hmm():
     
     print("test_gaussian_hmm passed")
 
-def test_embedded_conditional_gaussian():
-    torch.manual_seed(seed)
-    state_dim = 10
-    act_dim = 5
-    obs_dim = 12
-    ctl_dim = 3
+# def test_embedded_conditional_gaussian():
+#     torch.manual_seed(seed)
+#     state_dim = 10
+#     act_dim = 5
+#     obs_dim = 12
+#     ctl_dim = 3
 
-    # synthetic data
-    T = 30
-    batch_size = 32
-    x = torch.randn(T, batch_size, obs_dim)
-    u = torch.softmax(torch.randn(T, batch_size, ctl_dim), dim=-1)
-    mask = (torch.randn(T, batch_size) < 1) * 1
+#     # synthetic data
+#     T = 30
+#     batch_size = 32
+#     x = torch.randn(T, batch_size, obs_dim)
+#     u = torch.softmax(torch.randn(T, batch_size, ctl_dim), dim=-1)
+#     mask = (torch.randn(T, batch_size) < 1) * 1
     
-    state_embed_dim = 30
-    act_embed_dim = 30
-    rank = 10
-    hmm = EmbeddedContinuousGaussianHMM(
-        state_dim, act_dim, obs_dim, ctl_dim, 
-        state_embed_dim, act_embed_dim, rank=rank
-    )
+#     state_embed_dim = 30
+#     act_embed_dim = 30
+#     rank = 10
+#     hmm = EmbeddedContinuousGaussianHMM(
+#         state_dim, act_dim, obs_dim, ctl_dim, 
+#         state_embed_dim, act_embed_dim, rank=rank
+#     )
     
-    x_sample, u_sample, alpha_b, alpha_a = hmm.predict(x, u, inference=True)
-    assert list(x_sample.shape) == [1, T, batch_size, obs_dim]
-    assert list(u_sample.shape) == [1, T-1, batch_size, ctl_dim]
-    assert list(alpha_b.shape) == [T, batch_size, state_dim]
-    assert list(alpha_a.shape) == [T-1, batch_size, act_dim]
+#     x_sample, u_sample, alpha_b, alpha_a = hmm.predict(x, u, inference=True)
+#     assert list(x_sample.shape) == [1, T, batch_size, obs_dim]
+#     assert list(u_sample.shape) == [1, T-1, batch_size, ctl_dim]
+#     assert list(alpha_b.shape) == [T, batch_size, state_dim]
+#     assert list(alpha_a.shape) == [T-1, batch_size, act_dim]
 
-    print("test_embedded_conditional_gaussian passed")
+#     print("test_embedded_conditional_gaussian passed")
 
 if __name__ == "__main__":
     # test_discrete_gaussian_hmm()
-    # test_continuous_gaussian_hmm()
+    test_continuous_gaussian_hmm()
     # test_logistic_gaussian_hmm()
-    test_embedded_conditional_gaussian()
+    # test_embedded_conditional_gaussian()
