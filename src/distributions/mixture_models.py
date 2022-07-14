68cb6883adba89a78f458b17b8454aa6e08945fa
Wei Ran <rw422@tamu.edu>
2022-07-14 15:50:26 -0500

    remove embedded models due to bad stability

--- src/distributions/mixture_models.py
+++ src/distributions/mixture_models.py
@@ -2,12 +2,13 @@ import torch
 import torch.nn as nn
 import torch.distributions as torch_dist
 from src.distributions.flows import SimpleTransformedModule, BatchNormTransform
+from src.distributions.flows import TanhTransform
 from src.distributions.utils import make_covariance_matrix, straight_through_sample
 from src.distributions.nn_models import Model, MLP
 
 class ConditionalGaussian(Model):
     """ Conditional gaussian distribution used to create mixture distributions """
-    def __init__(self, x_dim, z_dim, cov="full", batch_norm=True):
+    def __init__(self, x_dim, z_dim, cov="full", batch_norm=True, use_tanh=False, limits=None):
         """
         Args:
             x_dim (int): observed output dimension
@@ -21,6 +22,7 @@ class ConditionalGaussian(Model):
         self.z_dim = z_dim
         self.cov = cov
         self.batch_norm = batch_norm
+        self.use_tanh = use_tanh
         
         self.mu = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
         self.lv = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
@@ -37,6 +39,9 @@ class ConditionalGaussian(Model):
         
         if batch_norm:
             self.bn = BatchNormTransform(x_dim, momentum=0.1, affine=False)
+
+        if use_tanh:
+            self.tanh_transform = TanhTransform(limits)
         
     def __repr__(self):
         s = "{}(x_dim={}, z_dim={}, cov={})".format(
@@ -57,8 +62,14 @@ class ConditionalGaussian(Model):
 
         distribution = torch_dist.MultivariateNormal(mu, scale_tril=L)
         
-        if self.batch_norm and transform:
-            distribution = SimpleTransformedModule(distribution, [self.bn])
+        transforms = []
+        if self.batch_norm:
+            transforms.append(self.bn)
+        if self.use_tanh:
+            transforms.append(self.tanh_transform)
+        distribution = SimpleTransformedModule(distribution, transforms)
+        # if self.batch_norm and transform:
+        #     distribution = SimpleTransformedModule(distribution, [self.bn])
         return distribution
     
     def mean(self, params=None):
@@ -146,172 +157,172 @@ class ConditionalGaussian(Model):
         return x
 
 
-class EmbeddedConditionalGaussian(Model):
-    """ Conditional gaussian distribution operate on embeddings"""
-    def __init__(self, x_dim, z_dim, embed_dim, cov="full", batch_norm=True):
-        """
-        Args:
-            x_dim (int): observed output dimension
-            z_dim (int): latent conditonal dimension
-            cov (str): covariance type ["diag", "full"]
-            batch_norm (bool, optional): whether to use input batch normalization. default=True
-        """
-        super().__init__()
-        assert cov in ["diag", "full"]
-        self.x_dim = x_dim
-        self.z_dim = z_dim
-        self.embed_dim = embed_dim
-        self.cov = cov
-        self.batch_norm = batch_norm
+# class EmbeddedConditionalGaussian(Model):
+#     """ Conditional gaussian distribution operate on embeddings"""
+#     def __init__(self, x_dim, z_dim, embed_dim, cov="full", batch_norm=True):
+#         """
+#         Args:
+#             x_dim (int): observed output dimension
+#             z_dim (int): latent conditonal dimension
+#             cov (str): covariance type ["diag", "full"]
+#             batch_norm (bool, optional): whether to use input batch normalization. default=True
+#         """
+#         super().__init__()
+#         assert cov in ["diag", "full"]
+#         self.x_dim = x_dim
+#         self.z_dim = z_dim
+#         self.embed_dim = embed_dim
+#         self.cov = cov
+#         self.batch_norm = batch_norm
         
-        # self.mu = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
-        # self.lv = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
-        self.tl = nn.Parameter(torch.randn(1, z_dim, x_dim, x_dim), requires_grad=True)
+#         # self.mu = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
+#         # self.lv = nn.Parameter(torch.randn(1, z_dim, x_dim), requires_grad=True)
+#         self.tl = nn.Parameter(torch.randn(1, z_dim, x_dim, x_dim), requires_grad=True)
         
-        # nn.init.normal_(self.mu, mean=0, std=1)
-        # nn.init.normal_(self.lv, mean=0, std=0.01)
-        nn.init.normal_(self.tl, mean=0, std=0.01)
+#         # nn.init.normal_(self.mu, mean=0, std=1)
+#         # nn.init.normal_(self.lv, mean=0, std=0.01)
+#         nn.init.normal_(self.tl, mean=0, std=0.01)
 
-        self.w_mu = nn.Linear(embed_dim, x_dim)
-        self.w_lv = nn.Linear(embed_dim, x_dim)
-        # self.w_tl = nn.Linear(embed_dim, x_dim ** 2)
+#         self.w_mu = nn.Linear(embed_dim, x_dim)
+#         self.w_lv = nn.Linear(embed_dim, x_dim)
+#         # self.w_tl = nn.Linear(embed_dim, x_dim ** 2)
 
-        # nn.init.normal_(self.w_tl.weight, mean=0, std=0.01)
-        # nn.init.normal_(self.w_tl.bias, mean=0, std=0.001)
+#         # nn.init.normal_(self.w_tl.weight, mean=0, std=0.01)
+#         # nn.init.normal_(self.w_tl.bias, mean=0, std=0.001)
 
-        # self.w_mu = MLP(embed_dim, x_dim, 32, 2, "silu")
-        # self.w_lv = MLP(embed_dim, x_dim, 32, 2, "silu")
-        # self.w_tl = MLP(embed_dim, x_dim**2, 32, 2, "silu")
+#         # self.w_mu = MLP(embed_dim, x_dim, 32, 2, "silu")
+#         # self.w_lv = MLP(embed_dim, x_dim, 32, 2, "silu")
+#         # self.w_tl = MLP(embed_dim, x_dim**2, 32, 2, "silu")
         
-        if cov == "diag":
-            del self.tl
-            self.parameter_size = self.parameter_size[:-1]
-            self.tl = torch.zeros(1, z_dim, x_dim, x_dim).to(self.device)
+#         if cov == "diag":
+#             del self.tl
+#             self.parameter_size = self.parameter_size[:-1]
+#             self.tl = torch.zeros(1, z_dim, x_dim, x_dim).to(self.device)
         
-        if batch_norm:
-            self.bn = BatchNormTransform(x_dim, momentum=0.1, affine=False)
+#         if batch_norm:
+#             self.bn = BatchNormTransform(x_dim, momentum=0.1, affine=False)
         
-    def __repr__(self):
-        s = "{}(x_dim={}, z_dim={}, embedding_dim={}, cov={})".format(
-            self.__class__.__name__, self.x_dim, self.z_dim, self.embed_dim, self.cov
-        )
-        return s
+#     def __repr__(self):
+#         s = "{}(x_dim={}, z_dim={}, embedding_dim={}, cov={})".format(
+#             self.__class__.__name__, self.x_dim, self.z_dim, self.embed_dim, self.cov
+#         )
+#         return s
 
-    def get_distribution_class(self, z, transform=True, requires_grad=True):
-        """ Get distribution class object
+#     def get_distribution_class(self, z, transform=True, requires_grad=True):
+#         """ Get distribution class object
         
-        Args:
-            z (torch.tensor): hidden state embedding. size=[batch_size, state_dim, embed_dim]
-            transform (bool, optional): apply batchnorm transformation. Default=True
-            requires_grad (bool, optional): requires gradient on parameters. Default=True
-        """
-        mu = self.w_mu(z)
-        lv = self.w_lv(z)
-        # tl = self.tl if self.cov == "diag" else self.w_tl(z)
-        # tl = tl.view(-1, self.z_dim, self.x_dim, self.x_dim)
-        tl = self.tl
+#         Args:
+#             z (torch.tensor): hidden state embedding. size=[batch_size, state_dim, embed_dim]
+#             transform (bool, optional): apply batchnorm transformation. Default=True
+#             requires_grad (bool, optional): requires gradient on parameters. Default=True
+#         """
+#         mu = self.w_mu(z)
+#         lv = self.w_lv(z)
+#         # tl = self.tl if self.cov == "diag" else self.w_tl(z)
+#         # tl = tl.view(-1, self.z_dim, self.x_dim, self.x_dim)
+#         tl = self.tl
 
-        L = make_covariance_matrix(lv, tl, cholesky=True, lv_rectify="exp")
+#         L = make_covariance_matrix(lv, tl, cholesky=True, lv_rectify="exp")
         
-        if requires_grad is False:
-            mu, L = mu.data, L.data
+#         if requires_grad is False:
+#             mu, L = mu.data, L.data
 
-        distribution = torch_dist.MultivariateNormal(mu, scale_tril=L)
+#         distribution = torch_dist.MultivariateNormal(mu, scale_tril=L)
         
-        if self.batch_norm and transform:
-            distribution = SimpleTransformedModule(distribution, [self.bn])
-        return distribution
+#         if self.batch_norm and transform:
+#             distribution = SimpleTransformedModule(distribution, [self.bn])
+#         return distribution
 
-    def mean(self, z):
-        distribution = self.get_distribution_class(z)
-        return distribution.mean
+#     def mean(self, z):
+#         distribution = self.get_distribution_class(z)
+#         return distribution.mean
     
-    def variance(self, z):
-        distribution = self.get_distribution_class(z)
-        return distribution.variance
+#     def variance(self, z):
+#         distribution = self.get_distribution_class(z)
+#         return distribution.variance
     
-    def entropy(self, z):
-        distribution = self.get_distribution_class(z)
-        return distribution.entropy()
+#     def entropy(self, z):
+#         distribution = self.get_distribution_class(z)
+#         return distribution.entropy()
 
-    def log_prob(self, z, x):
-        """ Component log probabilities 
+#     def log_prob(self, z, x):
+#         """ Component log probabilities 
 
-        Args:
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
-            x (torch.tensor): size=[batch_size, x_dim]
-        """
-        distribution = self.get_distribution_class(z)
-        return distribution.log_prob(x.unsqueeze(-2))
+#         Args:
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
+#             x (torch.tensor): size=[batch_size, x_dim]
+#         """
+#         distribution = self.get_distribution_class(z)
+#         return distribution.log_prob(x.unsqueeze(-2))
 
-    def mixture_log_prob(self, pi, z, x):
-        """ Compute mixture log probabilities 
+#     def mixture_log_prob(self, pi, z, x):
+#         """ Compute mixture log probabilities 
         
-        Args:
-            pi (torch.tensor): mixing weights. size=[..., z_dim]
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
-            x (torch.tensor): observervations. size[..., x_dim]
-        """
-        logp_pi = torch.log(pi + 1e-6)
-        logp_x = self.log_prob(z, x)
-        logp = torch.logsumexp(logp_pi + logp_x, dim=-1)
-        return logp
+#         Args:
+#             pi (torch.tensor): mixing weights. size=[..., z_dim]
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
+#             x (torch.tensor): observervations. size[..., x_dim]
+#         """
+#         logp_pi = torch.log(pi + 1e-6)
+#         logp_x = self.log_prob(z, x)
+#         logp = torch.logsumexp(logp_pi + logp_x, dim=-1)
+#         return logp
 
-    def sample(self, z, sample_shape=torch.Size()):
-        """ Sample components 
+#     def sample(self, z, sample_shape=torch.Size()):
+#         """ Sample components 
         
-        Args:
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
-            sample_shape (torch.size, optional). sample shape. Default=torch.Size()
-        """
-        distribution = self.get_distribution_class(z)
-        return distribution.rsample(sample_shape)
+#         Args:
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
+#             sample_shape (torch.size, optional). sample shape. Default=torch.Size()
+#         """
+#         distribution = self.get_distribution_class(z)
+#         return distribution.rsample(sample_shape)
     
-    def infer(self, prior, x, z=None, logp_x=None):
-        """ Compute posterior distributions
+#     def infer(self, prior, x, z=None, logp_x=None):
+#         """ Compute posterior distributions
 
-        Args:
-            prior (torch.tensor): prior probabilities. size=[..., z_dim]
-            x (torch.tensor): observations. size=[..., x_dim]
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
-            logp_x (torch.tensor, None, optional): log likelihood to avoid
-                computing again. default=None
+#         Args:
+#             prior (torch.tensor): prior probabilities. size=[..., z_dim]
+#             x (torch.tensor): observations. size=[..., x_dim]
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
+#             logp_x (torch.tensor, None, optional): log likelihood to avoid
+#                 computing again. default=None
 
-        Returns:
-            post (torch.tensor): posterior probabilities. size=[..., z_dim]
-        """
-        if logp_x is None:
-            logp_x = self.log_prob(z, x)
-        post = torch.softmax(torch.log(prior + 1e-6) + logp_x, dim=-1)
-        return post
+#         Returns:
+#             post (torch.tensor): posterior probabilities. size=[..., z_dim]
+#         """
+#         if logp_x is None:
+#             logp_x = self.log_prob(z, x)
+#         post = torch.softmax(torch.log(prior + 1e-6) + logp_x, dim=-1)
+#         return post
 
-    def bayesian_average(self, pi, z):
-        """ Compute weighted average of component means 
+#     def bayesian_average(self, pi, z):
+#         """ Compute weighted average of component means 
         
-        Args:
-            pi (torch.tensor): mixing weights. size=[..., z_dim]
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
-        """
-        mu = self.mean(z)
-        x = torch.sum(pi.unsqueeze(-1) * mu.unsqueeze(0), dim=-2)
-        return x
+#         Args:
+#             pi (torch.tensor): mixing weights. size=[..., z_dim]
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim]
+#         """
+#         mu = self.mean(z)
+#         x = torch.sum(pi.unsqueeze(-1) * mu.unsqueeze(0), dim=-2)
+#         return x
     
-    def ancestral_sample(self, pi, z, num_samples=1, sample_mean=False):
-        """ Ancestral sampling
+#     def ancestral_sample(self, pi, z, num_samples=1, sample_mean=False):
+#         """ Ancestral sampling
         
-        Args:
-            pi (torch.tensor): mixing weights. size=[T, batch_size, z_dim]
-            z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim] (torch.tensor): state embedding. size=[batch_size, z_dim, embedding_dim]
-            num_samples (int, optional): number of samples to draw. Default=1
-            sample_mean (bool, optional): whether to sample component mean. Default=False
-        """
-        z_ = torch_dist.RelaxedOneHotCategorical(1, pi).rsample((num_samples,))
-        z_ = straight_through_sample(z_, dim=-1).unsqueeze(-1)
+#         Args:
+#             pi (torch.tensor): mixing weights. size=[T, batch_size, z_dim]
+#             z (torch.tensor): hidden state embedding. size=[batch_size, z_dim, embedding_dim] (torch.tensor): state embedding. size=[batch_size, z_dim, embedding_dim]
+#             num_samples (int, optional): number of samples to draw. Default=1
+#             sample_mean (bool, optional): whether to sample component mean. Default=False
+#         """
+#         z_ = torch_dist.RelaxedOneHotCategorical(1, pi).rsample((num_samples,))
+#         z_ = straight_through_sample(z_, dim=-1).unsqueeze(-1)
         
-        # sample component
-        if sample_mean:
-            x_ = self.mean(z)
-        else:
-            x_ = self.sample(z, (num_samples, pi.shape[0])).squeeze(1)
-        x = torch.sum(z_ * x_, dim=-2)
-        return x
+#         # sample component
+#         if sample_mean:
+#             x_ = self.mean(z)
+#         else:
+#             x_ = self.sample(z, (num_samples, pi.shape[0])).squeeze(1)
+#         x = torch.sum(z_ * x_, dim=-2)
+#         return x
--- src/distributions/transition_models.py
+++ src/distributions/transition_models.py
@@ -108,54 +108,54 @@ class DiscreteMC(nn.Module):
         return torch.softmax(w, dim=-1)
 
 
-class EmbeddedDiscreteMC(nn.Module):
-    """ Discrete Markov chain for discrete actions with embeddings """
-    def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim, rank=0):
-        """
-        Args:
-            state_dim (int): state dimension
-            act_dim (int): discrete action dimension
-            state_embed_dim (int): state embedding dimension
-            act_embed_dim (int): act embedding dimension
-            rank (int, optional): core tensor rank. Dense matrix if rank=0, 
-                else use CP tensor decomposition. Default=0
-        """
-        super().__init__()
-        self.state_dim = state_dim
-        self.act_dim = act_dim
-        self.state_embed_dim = state_embed_dim
-        self.act_embed_dim = act_embed_dim
-        self.rank = rank
+# class EmbeddedDiscreteMC(nn.Module):
+#     """ Discrete Markov chain for discrete actions with embeddings """
+#     def __init__(self, state_dim, act_dim, state_embed_dim, act_embed_dim, rank=0):
+#         """
+#         Args:
+#             state_dim (int): state dimension
+#             act_dim (int): discrete action dimension
+#             state_embed_dim (int): state embedding dimension
+#             act_embed_dim (int): act embedding dimension
+#             rank (int, optional): core tensor rank. Dense matrix if rank=0, 
+#                 else use CP tensor decomposition. Default=0
+#         """
+#         super().__init__()
+#         self.state_dim = state_dim
+#         self.act_dim = act_dim
+#         self.state_embed_dim = state_embed_dim
+#         self.act_embed_dim = act_embed_dim
+#         self.rank = rank
         
-        self.s0 = nn.Parameter(torch.randn(1, state_dim))
-        nn.init.xavier_normal_(self.s0, gain=1.)
+#         self.s0 = nn.Parameter(torch.randn(1, state_dim))
+#         nn.init.xavier_normal_(self.s0, gain=1.)
         
-        self.w = TuckerDecomposition(
-            state_dim, act_dim, state_embed_dim, act_embed_dim, core_rank=rank
-        )
+#         self.w = TuckerDecomposition(
+#             state_dim, act_dim, state_embed_dim, act_embed_dim, core_rank=rank
+#         )
 
-    def __repr__(self):
-        s = "{}(state_embedding={}, act_embedding={}, core_rank={})".format(
-            self.__class__.__name__, self.state_embed_dim, 
-            self.act_embed_dim, self.rank
-        )
-        return s
+#     def __repr__(self):
+#         s = "{}(state_embedding={}, act_embedding={}, core_rank={})".format(
+#             self.__class__.__name__, self.state_embed_dim, 
+#             self.act_embed_dim, self.rank
+#         )
+#         return s
 
-    def get_initial_state(self):
-        """
-        Returns:
-            s0 (torch.tensor): initial state distribution. size=[batch_size, state_dim]
-        """
-        s0 = torch.softmax(self.s0.view(-1, self.state_dim), dim=-1)
-        return s0
+#     def get_initial_state(self):
+#         """
+#         Returns:
+#             s0 (torch.tensor): initial state distribution. size=[batch_size, state_dim]
+#         """
+#         s0 = torch.softmax(self.s0.view(-1, self.state_dim), dim=-1)
+#         return s0
     
-    def get_transition_matrix(self):
-        """
-        Returns:
-            w (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
-        """
-        w = self.w()
-        return torch.softmax(w, dim=-1)
+#     def get_transition_matrix(self):
+#         """
+#         Returns:
+#             w (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+#         """
+#         w = self.w()
+#         return torch.softmax(w, dim=-1)
 
 
 class LogisticMC(nn.Module):
