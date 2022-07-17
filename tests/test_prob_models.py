d406e26c3ea74668a45a2cb7d5b43fa1559fc8b3
Wei Ran <rw422@tamu.edu>
2022-07-17 17:23:22 -0500

    add qmdp layer test case

--- tests/test_prob_models.py
+++ tests/test_prob_models.py
@@ -7,6 +7,7 @@ from src.distributions.legacy.hmm import LogisticGaussianHMM
 # from src.distributions.hmm import EmbeddedContinuousGaussianHMM
 
 seed = 0
+torch.manual_seed(seed)
 
 def test_discrete_mc():
     torch.manual_seed(seed)
@@ -20,24 +21,11 @@ def test_discrete_mc():
     batch_size = 12
     b = torch.softmax(torch.randn(batch_size, state_dim), dim=-1)
     a = torch.softmax(torch.randn(batch_size, act_dim), dim=-1)
-
+    
     transition = cmc.transition
     assert torch.all(torch.abs(transition.sum(-1) - 1) < 1e-5)
     
     b_next = cmc._forward(b, a)
-    b_next_tensor = cmc._tensor_forward(b, a)
-
-    error = torch.abs(b_next - b_next_tensor).sum()
-    assert error < 1e-5
-
-    # test backward
-    m = torch.randn(state_dim)
-
-    m_next = cmc._backward(m)
-    m_next_tensor = cmc._tensor_backward(m)
-
-    error = torch.abs(m_next - m_next_tensor).sum()
-    assert error < 1e-5
 
     print("test_discrete_mc passed")
 
@@ -155,8 +143,41 @@ def test_logistic_gaussian_hmm():
 
 #     print("test_embedded_conditional_gaussian passed")
 
+def test_qmdp_layer():
+    from src.distributions.hmm import QMDPLayer
+    state_dim = 10
+    act_dim = 5
+    rank = 7
+    horizon = 15
+
+    qmdp_layer = QMDPLayer(state_dim, act_dim, rank, horizon)
+    transition = qmdp_layer.transition
+    
+    # synthetic data
+    T = 12
+    batch_size = 32
+    logp_o = torch.randn(T, batch_size, state_dim)
+    logp_u = torch.randn(T, batch_size, act_dim)
+    b = torch.softmax(torch.randn(batch_size, state_dim), dim=-1)
+    a = torch.softmax(torch.randn(batch_size, act_dim), dim=-1)
+    reward = torch.randn(batch_size, act_dim, state_dim)
+    
+    t = 0
+    b_next = qmdp_layer.update_belief(logp_o[t], transition, b, a)
+    assert torch.all(torch.isclose(b_next.sum(-1), torch.ones(batch_size)))
+
+    a_next = qmdp_layer.update_action(logp_u[t], a)
+    assert torch.all(torch.isclose(a_next.sum(-1), torch.ones(batch_size)))
+
+    alpha_b, alpha_a = qmdp_layer(logp_o, logp_u, b, a, reward)
+    assert torch.all(torch.isclose(alpha_b.sum(-1), torch.ones(T, batch_size)))
+    assert torch.all(torch.isclose(alpha_a.sum(-1), torch.ones(T, batch_size)))
+    
+    print("test_qmdp_layer passed")
+
 if __name__ == "__main__":
-    test_discrete_mc() 
+    # test_discrete_mc() 
+    test_qmdp_layer()
     # test_continuous_gaussian_hmm()
     # test_discrete_gaussian_hmm()
     # test_logistic_gaussian_hmm()
