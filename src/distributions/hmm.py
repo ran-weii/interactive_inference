cd6f04140767b263063fbdaf8d7a19c27df1dadc
Wei Ran <rw422@tamu.edu>
2022-07-17 17:18:23 -0500

    commit QMDPLayer

--- src/distributions/hmm.py
+++ src/distributions/hmm.py
@@ -1,10 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.jit as jit
 from src.distributions.nn_models import Model
 from src.distributions.mixture_models import ConditionalGaussian
 from src.distributions.transition_models import DiscreteMC
 # from src.distributions.mixture_models import ConditionalGaussian, EmbeddedConditionalGaussian
 # from src.distributions.transition_models import DiscreteMC, EmbeddedDiscreteMC
+
+import math
+from typing import List, Tuple
+from torch import Tensor
 
 class ContinuousGaussianHMM(Model):
     """Input-output hidden markov model with:
@@ -185,6 +190,105 @@ class ContinuousGaussianHMM(Model):
             
             return x_sample, u_sample, alpha_b, alpha_a
 
+
+class QMDPLayer(jit.ScriptModule):
+    def __init__(self, state_dim, act_dim, rank, horizon):
+        super().__init__()
+        self.state_dim = state_dim
+        self.act_dim = act_dim
+        self.rank = rank
+        self.horizon = horizon
+        self.eps = 1e-6
+        
+        self.u = nn.Parameter(torch.randn(1, rank, state_dim)) # source tensor
+        self.v = nn.Parameter(torch.randn(1, rank, state_dim)) # sink tensor
+        self.w = nn.Parameter(torch.randn(1, rank, act_dim)) # action tensor
+        self.tau = nn.Parameter(torch.randn(1, 1))
+
+        nn.init.xavier_normal_(self.u, gain=1.)
+        nn.init.xavier_normal_(self.v, gain=1.)
+        nn.init.xavier_normal_(self.w, gain=1.)
+        nn.init.uniform_(self.tau, a=-1, b=1)
+    
+    @property
+    def transition(self):
+        """ Return transition matrix. size=[1, act_dim, state_dim, state_dim] """
+        w = torch.einsum("nri, nrj, nrk -> nkij", self.u, self.v, self.w)
+        return torch.softmax(w, dim=-1)
+    
+    @jit.script_method
+    def compute_value(self, transition: Tensor, reward: Tensor):
+        q = [torch.empty(0)] * (self.horizon + 1)
+        q[0] = reward
+        for t in range(self.horizon):
+            v_next = reward + torch.logsumexp(q[t], dim=-2, keepdim=True)
+            q[t+1] = torch.einsum("nkij, nkj -> nki", transition, v_next)
+        return torch.stack(q[1:])
+    
+    @jit.script_method
+    def plan(self, b, value):
+        tau = torch.exp(self.tau.clip(math.log(1e-6), math.log(1e3)))
+        tau = poisson_pdf(tau, self.horizon)
+        if tau.shape[0] != b.shape[0]:
+            tau = torch.repeat_interleave(tau, b.shape[0], 0)
+        
+        a = torch.softmax(torch.einsum("ni, hnki -> hnk", b, value), dim=-1)
+        a = torch.einsum("hnk, nh -> nk", a, tau)
+        return a
+
+    @jit.script_method
+    def update_belief(self, logp_o, transition, b, a):
+        s_next = torch.einsum("mkij, ni, nk -> nj", transition, b, a)
+        logp_s = torch.log(s_next + self.eps)
+        b_post = torch.softmax(logp_s + logp_o, dim=-1)
+        return b_post
+    
+    @jit.script_method
+    def update_action(self, logp_u, a):
+        logp_a = torch.log(a + self.eps)
+        a_post = torch.softmax(logp_a + logp_u, dim=-1)
+        return a_post
+    
+    @jit.script_method
+    def update_cell(
+        self, logp_o: Tensor, logp_u: Tensor, b: Tensor, a: Tensor, 
+        transition: Tensor, value: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+        a_post = self.update_action(logp_u, a)
+        b_post = self.update_belief(logp_o, transition, b, a_post)
+        a_next = self.plan(b_post, value)
+        return (b_post, a_next)
+    
+    @jit.script_method
+    def forward(
+        self, logp_o: Tensor, logp_u: Tensor, b: Tensor, a: Tensor, reward: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+        transition = self.transition
+        value = self.compute_value(transition, reward)
+        T = len(logp_o)
+        
+        alpha_b = [b] + [torch.empty(0)] * (T)
+        alpha_a = [a] + [torch.empty(0)] * (T)
+        for t in range(T):
+            alpha_b[t+1], alpha_a[t+1] = self.update_cell(
+                logp_o[t], logp_u[t], alpha_b[t], alpha_a[t], transition, value
+            )
+        return torch.stack(alpha_b[1:]), torch.stack(alpha_a[1:])
+
+
+def poisson_pdf(rate: Tensor, K: int) -> Tensor:
+    """ 
+    Args:
+        rate (torch.tensor): poission arrival rate [batch_size, 1]
+        K (int): number of bins
+
+    Returns:
+        pdf (torch.tensor): truncated poisson pdf [batch_size, K]
+    """
+    Ks = 1 + torch.arange(K).to(rate.device)
+    poisson_logp = Ks.xlogy(rate) - rate - (Ks + 1).lgamma()
+    pdf = torch.softmax(poisson_logp, dim=-1)
+    return pdf
 
 # class EmbeddedContinuousGaussianHMM(Model):
 #     """ Input-output hidden markov model with:
