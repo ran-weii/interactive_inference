7e367458ce2fa0f0bf85834ea213e7943dfda657
Wei Ran <rw422@tamu.edu>
2022-07-18 09:14:23 -0500

    add variable typing and docstring to QMDPLayer

--- src/distributions/hmm.py
+++ src/distributions/hmm.py
@@ -105,7 +105,7 @@ class ContinuousGaussianHMM(Model):
                 pi0 = self.prior_policy
                 a = torch.sum(b.unsqueeze(-1) * pi0, dim=-1)
             a_t = self.ctl_model.infer(a, u, logp_u)
-            logp_z = torch.log(self.transition_model._tensor_forward(b, a_t) + self.eps)
+            logp_z = torch.log(self.transition_model._forward(b, a_t) + self.eps)
         
         if logp_x is None:
             logp_x = self.obs_model.log_prob(x)
@@ -203,7 +203,7 @@ class QMDPLayer(jit.ScriptModule):
         self.b0 = nn.Parameter(torch.randn(1, state_dim))
         self.u = nn.Parameter(torch.randn(1, rank, state_dim)) # source tensor
         self.v = nn.Parameter(torch.randn(1, rank, state_dim)) # sink tensor
-        self.w = nn.Parameter(torch.randn(1, rank, act_dim)) # action tensor
+        self.w = nn.Parameter(torch.randn(1, rank, act_dim)) # action tensor 
         self.tau = nn.Parameter(torch.randn(1, 1))
         
         nn.init.xavier_normal_(self.b0, gain=1.)
@@ -219,7 +219,16 @@ class QMDPLayer(jit.ScriptModule):
         return torch.softmax(w, dim=-1)
     
     @jit.script_method
-    def compute_value(self, transition: Tensor, reward: Tensor):
+    def compute_value(self, transition: Tensor, reward: Tensor) -> Tensor:
+        """ Compute expected value using value iteration
+
+        Args:
+            transition (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+            reward (torch.tensor): reward matrix. size=[batch_size, act_dim, state_dim]
+        
+        Returns:
+            q (torch.tensor): state q value. size=[horizon, batch_size, act_dim, state_dim]
+        """        
         q = [torch.empty(0)] * (self.horizon + 1)
         q[0] = reward
         for t in range(self.horizon):
@@ -228,7 +237,16 @@ class QMDPLayer(jit.ScriptModule):
         return torch.stack(q[1:])
     
     @jit.script_method
-    def plan(self, b, value):
+    def plan(self, b: Tensor, value: Tensor) -> Tensor:
+        """ Compute the belief action distribution 
+        
+        Args:
+            b (torch.tensor): current belief. size=[batch_size, state_dim]
+            value (torch.tensor): state q value. size=[horizon, batch_size, act_dim, state_dim]
+
+        Returns:
+            a (torch.tensor): action distribution. size=[batch_size, act_dim]
+        """
         tau = torch.exp(self.tau.clip(math.log(1e-6), math.log(1e3)))
         tau = poisson_pdf(tau, self.horizon)
         if tau.shape[0] != b.shape[0]:
@@ -239,14 +257,34 @@ class QMDPLayer(jit.ScriptModule):
         return a
 
     @jit.script_method
-    def update_belief(self, logp_o, transition, b, a):
-        s_next = torch.einsum("mkij, ni, nk -> nj", transition, b, a)
+    def update_belief(self, logp_o: Tensor, transition: Tensor, b: Tensor, a: Tensor) -> Tensor:
+        """ Compute state posterior
+        
+        Args:
+            logp_o (torch.tensor): log probability of current observation. size=[batch_size, state_dim]
+            transition (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+            b (torch.tensor): prior belief. size=[batch_size, state_dim]
+            a (torch.tensor): action posterior. size=[batch_size, act_dim]
+
+        Returns:
+            b_post (torch.tensor): state posterior. size=[batch_size, state_dim]
+        """
+        s_next = torch.einsum("nkij, ni, nk -> nj", transition, b, a)
         logp_s = torch.log(s_next + self.eps)
         b_post = torch.softmax(logp_s + logp_o, dim=-1)
         return b_post
     
     @jit.script_method
-    def update_action(self, logp_u, a):
+    def update_action(self, logp_u: Tensor, a: Tensor) -> Tensor:
+        """ Compute action posterior 
+        
+        Args:
+            logp_u (torch.tensor): log probability of previous control. size=[batch_size, act_dim]
+            a (torch.tensor): action prior. size=[batch_size, act_dim]
+
+        Returns:
+            a_post (torch.tensor): action posterior. size=[batch_size, act_dim]
+        """ 
         logp_a = torch.log(a + self.eps)
         a_post = torch.softmax(logp_a + logp_u, dim=-1)
         return a_post
@@ -256,13 +294,14 @@ class QMDPLayer(jit.ScriptModule):
         self, logp_o: Tensor, logp_u: Tensor, b: Tensor, a: Tensor, 
         transition: Tensor, value: Tensor
         ) -> Tuple[Tensor, Tensor]:
+        """ Compute action and state posterior. Then compute the next action distribution """
         a_post = self.update_action(logp_u, a)
         b_post = self.update_belief(logp_o, transition, b, a_post)
         a_next = self.plan(b_post, value)
         return (b_post, a_next)
     
     @jit.script_method
-    def init_hidden(self, logp_o, value):
+    def init_hidden(self, logp_o: Tensor, value: Tensor) -> Tuple[Tensor, Tensor]:
         b0 = torch.softmax(self.b0, dim=-1)
         logp_s = torch.log(b0 + self.eps)
         b = torch.softmax(logp_s + logp_o, dim=-1)
@@ -273,11 +312,24 @@ class QMDPLayer(jit.ScriptModule):
         self, logp_o: Tensor, logp_u: Union[Tensor, None], reward: Tensor,
         b: Union[Tensor, None], a: Union[Tensor, None]
         ) -> Tuple[Tensor, Tensor]:
+        """
+        Args:
+            logp_o (torch.tensor): sequence of observation probabilities. size=[T, batch_size, state_dim]
+            logp_u (torch.tensor): sequence of control probabilities. Should be offset -1 if b is None.
+                size=[T, batch_size, act_dim]
+            reward (torch.tensor): reward matrix. size=[batch_size, act_dim, state_dim]
+            b ([torch.tensor, None], optional): prior belief. size=[batch_size, state_dim]
+            a ([torch.tensor, None], optional): prior action. size=[batch_size, act_dim]
+        
+        Returns:
+            alpha_b (torch.tensor): sequence of posterior belief. size=[T, batch_size, state_dim]
+            alpha_a (torch.tensor): sequence of action distribution. size=[T, batch_size, act_dim]
+        """
         transition = self.transition
         value = self.compute_value(transition, reward)
+        t_offset = 0 if b is not None else -1 # offset for offline prediction
         T = len(logp_o)
-        t_offset = 1 if b is None else 0 # offset for offline prediction
-        
+
         alpha_b = [b] + [torch.empty(0)] * (T)
         alpha_a = [a] + [torch.empty(0)] * (T)
         for t in range(T):
@@ -285,7 +337,7 @@ class QMDPLayer(jit.ScriptModule):
                 alpha_b[t+1], alpha_a[t+1] = self.init_hidden(logp_o[t], value)
             else:
                 alpha_b[t+1], alpha_a[t+1] = self.update_cell(
-                    logp_o[t], logp_u[t-t_offset], 
+                    logp_o[t], logp_u[t+t_offset], 
                     alpha_b[t], alpha_a[t], transition, value
                 )
         return torch.stack(alpha_b[1:]), torch.stack(alpha_a[1:])
