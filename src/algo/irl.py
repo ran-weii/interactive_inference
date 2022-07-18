4ced61200c68eb0732abccf4d9ad18e3988d2d40
Wei Ran <rw422@tamu.edu>
2022-07-18 09:15:52 -0500

    add truncated backpropagation through time to BehaviorCloning

--- src/algo/irl.py
+++ src/algo/irl.py
@@ -4,9 +4,12 @@ import torch.nn as nn
 from src.distributions.nn_models import Model
 
 class BehaviorCloning(Model):
-    """ Supervised behavior cloning algorithm """
-    def __init__(self, agent, obs_penalty=0, lr=1e-3, decay=0, grad_clip=None):
+    """ Supervised behavior cloning algorithm 
+    with truncated backpropagation through time
+    """
+    def __init__(self, agent, bptt_steps, obs_penalty=0, lr=1e-3, decay=0, grad_clip=None):
         super().__init__()
+        self.bptt_steps = bptt_steps
         self.obs_penalty = obs_penalty
         self.lr = lr
         self.decay = decay
@@ -19,8 +22,8 @@ class BehaviorCloning(Model):
     
     def __repr__(self):
         s_agent = self.agent.__repr__()
-        s = "{}(obs_penalty={}, lr={}, decay={}, grad_clip={},\nagent={})".format(
-            self.__class__.__name__, self.obs_penalty, self.lr, 
+        s = "{}(bptt_steps={}, obs_penalty={}, lr={}, decay={}, grad_clip={},\nagent={})".format(
+            self.__class__.__name__, self.bptt_steps, self.obs_penalty, self.lr, 
             self.decay, self.grad_clip, s_agent
         )
         return s
@@ -48,25 +51,42 @@ class BehaviorCloning(Model):
             u = u.to(self.device)
             mask = mask.to(self.device)
             
-            out = self.agent(o, u)
-            loss_u, stats_u = self.agent.act_loss(o, u, mask, out)
-            loss_o, stats_o = self.agent.obs_loss(o, u, mask, out)
+            hidden = None
+            for t, batch_t in enumerate(zip(
+                o.split(self.bptt_steps, dim=0),
+                u.split(self.bptt_steps, dim=0),
+                mask.split(self.bptt_steps, dim=0)
+                )):
+                o_t, u_t, mask_t = batch_t
+                
+                if hidden is None:
+                    out, hidden = self.agent(o_t, u_t, hidden)
+                else:
+                    # concat previous ctl
+                    u_t_cat = torch.cat([u_t_prev[-1:], u_t[1:]], dim=0)
 
-            loss = torch.mean(loss_u + self.obs_penalty * loss_o)
+                    hidden = [h[-1].detach() for h in hidden]
+                    out, hidden = self.agent(o_t, u_t_cat, hidden)
+                u_t_prev = u_t.clone()
+                
+                loss_u, stats_u = self.agent.act_loss(o_t, u_t, mask_t, out)
+                loss_o, stats_o = self.agent.obs_loss(o_t, u_t, mask_t, out)
 
-            if train:
-                loss.backward()
-                if self.grad_clip is not None:
-                    nn.utils.clip_grad_norm_(self.parameters(), self.grad_clip)
+                loss = torch.mean(loss_u + self.obs_penalty * loss_o)
+                    
+                if train:
+                    loss.backward()
+                    if self.grad_clip is not None:
+                        nn.utils.clip_grad_norm_(self.parameters(), self.grad_clip)
 
-                self.optimizer.step()
-            self.optimizer.zero_grad()
-            
-            epoch_stats.append({
-                "train": 1 if train else 0,
-                "loss": loss.cpu().data.item(),
-                **stats_u, **stats_o,
-            })
+                    self.optimizer.step()
+                self.optimizer.zero_grad()
+                
+                epoch_stats.append({
+                    "train": 1 if train else 0,
+                    "loss": loss.cpu().data.item(),
+                    **stats_u, **stats_o,
+                })
         
         stats = pd.DataFrame(epoch_stats).mean().to_dict()
         return stats
