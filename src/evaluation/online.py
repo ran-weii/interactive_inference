c15d62bdbd889ff4e32bcf3cb05217e6f2bda1c3
Wei Ran <rw422@tamu.edu>
2022-07-18 16:22:03 -0500

    add code to change device in eval_episode

--- src/evaluation/online.py
+++ src/evaluation/online.py
@@ -1,3 +1,5 @@
+import torch
+
 class Evaluator:
     def __init__(self):
         self._track = {
@@ -21,12 +23,12 @@ class Evaluator:
         self._track["loom_x"].append(obs[0, 9].item())
 
 
-def eval_episode(env, controller, eps_id, max_steps=1000, callback=None):
+def eval_episode(env, agent, eps_id, max_steps=1000, callback=None):
     """ Evaluate episode
     
     Args:
         env (Env): gym style simulator
-        controller (): controller with reset and choose_action method
+        agent (Agent): agent class
         eps_id (int): episode id
         max_steps (int, optional): maximum number of steps
         callback (class, optional): controller callback
@@ -37,17 +39,22 @@ def eval_episode(env, controller, eps_id, max_steps=1000, callback=None):
         track_data (dict): recorded track data
         callback (class): updated callback. return if callback is not None
     """
-    controller.reset()
+    agent.eval()
+    agent.reset()
     obs = env.reset(eps_id)
+
+    rewards = []
     for t in range(max_steps):
-        act = controller.choose_action(obs)
-        if callback is not None:
-            callback(controller)
-        obs, _, done, _ = env.step(act)
+        """ TODO: figure out a better way to choose recurrent action """
+        with torch.no_grad():
+            ctl, _ = agent.choose_action(obs.to(agent.device))
+            ctl = ctl.cpu().data.view(1, -1)
+        obs, r, done, _ = env.step(ctl)
         if done or t >= max_steps:
             break
+        rewards.append(r)
     
     sim_states = env._sim_states[:t+1]
     sim_acts = env._sim_acts[:t+1]
     track_data = {k:v[:t+1] for (k, v) in env._track_data.items()}
-    return sim_states, sim_acts, track_data
+    return sim_states, sim_acts, track_data, rewards
