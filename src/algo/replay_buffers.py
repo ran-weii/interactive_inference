6c2068c8b955c3d08c1a3af0d3a8e0aea7fc854b
Wei Ran <rw422@tamu.edu>
2022-07-25 10:15:43 -0500

    add hidden state to replay_buffer for recurrent agents

--- src/algo/replay_buffers.py
+++ src/algo/replay_buffers.py
@@ -3,17 +3,18 @@ import torch
 from src.data.ego_dataset import collate_fn, sample_sequence
 
 class ReplayBuffer:
-    def __init__(self, obs_dim, ctl_dim, max_size, momentum=0.1):
+    def __init__(self, obs_dim, ctl_dim, state_dim, max_size, momentum=0.1):
         """
         Args:
-            state_dim (int): hidden state dimension
-            ctl_dim (int): action dimension
             obs_dim (int): observation dimension
+            ctl_dim (int): action dimension
+            state_dim (int): hidden state dimension
             max_size (int): maximum buffer size
             momentum (float): moving stats momentum
         """
         self.obs_dim = obs_dim
         self.ctl_dim = ctl_dim
+        self.state_dim = state_dim
 
         self.episodes = []
         self.eps_len = []
@@ -24,6 +25,7 @@ class ReplayBuffer:
         
         self.obs_eps = [] # store a single episode
         self.ctl_eps = [] # store a single episode
+        self.state_eps = [] # store a single episode
         self.rwd_eps = [] # store a single episode
         self.done_eps = [] # store a single episode
         
@@ -32,27 +34,37 @@ class ReplayBuffer:
         self.moving_mean_square = np.zeros((obs_dim,))
         self.moving_variance = np.ones((obs_dim, ))
 
-    def __call__(self, obs, ctl, rwd, done=False):
+    def __call__(self, obs, ctl, state, rwd, done=False):
         """ Append episode history """ 
         self.obs_eps.append(obs.data.numpy())
         self.ctl_eps.append(ctl.data.numpy())
+        self.state_eps.append(state.data.numpy())
         self.rwd_eps.append(np.array(rwd).reshape(1, 1))
         self.done_eps.append(np.array([int(done)]).reshape(1, 1))
-
-    def push(self, obs=None, ctl=None, rwd=None, done=None):
+    
+    def clear(self):
+        self.episodes = []
+        self.eps_len = []
+        self.num_eps = 0
+        self.size = 0
+        
+    def push(self, obs=None, ctl=None, state=None, rwd=None, done=None):
         """ Store episode """
         if obs is None and ctl is None:
             obs = np.vstack(self.obs_eps)
             ctl = np.vstack(self.ctl_eps)
+            state = np.vstack(self.state_eps)
             rwd = np.vstack(self.rwd_eps)
             done = np.vstack(self.done_eps)
         
         self.episodes.append({ 
             "obs": obs[:-1],
             "ctl": ctl[:-1],
+            "state": state[:-1],
             "rwd": rwd[:-1],
             "next_obs": obs[1:],
             "next_ctl": ctl[1:],
+            "next_state": state[1:],
             "done": done[1:]
         })
         self.update_obs_stats(obs)
@@ -70,57 +82,75 @@ class ReplayBuffer:
                 self.num_eps = len(self.eps_len)
         self.obs_eps = []
         self.ctl_eps = [] 
+        self.state_eps = []
         self.rwd_eps = []
         self.done_eps = []
 
-    def sample_random(self, batch_size):
+    def sample_random(self, batch_size, prioritize=False):
         """ sample random steps """ 
         obs = np.vstack([e["obs"] for e in self.episodes])
         ctl = np.vstack([e["ctl"] for e in self.episodes])
+        state = np.vstack([e["state"] for e in self.episodes])
         rwd = np.vstack([e["rwd"] for e in self.episodes])
         next_obs = np.vstack([e["next_obs"] for e in self.episodes])
         next_ctl = np.vstack([e["next_ctl"] for e in self.episodes])
+        next_state = np.vstack([e["next_state"] for e in self.episodes])
         done = np.vstack([e["done"] for e in self.episodes])
         
-        idx = np.random.randint(0, self.size, size=batch_size)
+        # prioritize new data for sampling
+        if prioritize:
+            idx = np.random.randint(max(0, self.size - batch_size * 100), self.size, size=batch_size)
+        else:
+            idx = np.random.randint(0, self.size, size=batch_size)
         batch = dict(
             obs=obs[idx], 
             ctl=ctl[idx], 
+            state=state[idx],
             rwd=rwd[idx], 
             next_obs=next_obs[idx], 
             next_ctl=next_ctl[idx],
+            next_state=next_state[idx],
             done=done[idx]
         )
         return {k: torch.from_numpy(v).to(torch.float32) for k, v in batch.items()}
     
-    def sample_episodes(self, batch_size, max_len=200):
+    def sample_episodes(self, batch_size, max_len=200, prioritize=False):
         """ sample random episodes """
-        idx = np.random.randint(0, self.num_eps, size=batch_size)
+        if prioritize:
+            idx = np.random.randint(max(0, self.num_eps - batch_size * 5), self.num_eps, size=batch_size)
+        else:
+            idx = np.random.randint(0, self.num_eps, size=batch_size)
         
         batch = []
         for i in idx:
             obs = torch.from_numpy(self.episodes[i]["obs"]).to(torch.float32)
             ctl = torch.from_numpy(self.episodes[i]["ctl"]).to(torch.float32)
+            state = torch.from_numpy(self.episodes[i]["state"]).to(torch.float32)
             rwd = torch.from_numpy(self.episodes[i]["rwd"]).to(torch.float32)
             next_obs = torch.from_numpy(self.episodes[i]["next_obs"]).to(torch.float32)
             next_ctl = torch.from_numpy(self.episodes[i]["next_ctl"]).to(torch.float32)
+            next_state = torch.from_numpy(self.episodes[i]["next_state"]).to(torch.float32)
             done = torch.from_numpy(self.episodes[i]["done"]).to(torch.float32)
 
             # truncate sequence
             sample_ids = sample_sequence(len(obs), max_len, gamma=1.)
             obs = obs[sample_ids]
             ctl = ctl[sample_ids]
+            state = state[sample_ids]
             rwd = rwd[sample_ids]
             next_obs = next_obs[sample_ids]
             next_ctl = next_ctl[sample_ids]
+            next_state = next_state[sample_ids]
             done = done[sample_ids]
 
             batch.append({
                 "obs": obs, 
                 "ctl": ctl, 
+                "state": state,
                 "rwd": rwd, 
                 "next_obs": next_obs, 
                 "next_ctl": next_ctl,
+                "next_state": next_state,
                 "done": done
             })
         
