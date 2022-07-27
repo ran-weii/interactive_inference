f100c5cb17b272b7289635c15dc1c30480364c53
Wei Ran <rw422@tamu.edu>
2022-07-27 09:50:32 -0500

    commit HyperQMDPLayer

--- src/agents/qmdp_layer.py
+++ src/agents/qmdp_layer.py
@@ -178,3 +178,166 @@ class QMDPLayer(jit.ScriptModule):
                     alpha_b[t], alpha_a[t], transition, value
                 )
         return torch.stack(alpha_b[1:]), torch.stack(alpha_a[1:])
+
+
+class HyperQMDPLayer(jit.ScriptModule):
+    """ Hypernet version of qmdp layer """
+    def __init__(self, state_dim, act_dim, rank, horizon, hyper_dim):
+        super().__init__()
+        self.state_dim = state_dim
+        self.act_dim = act_dim
+        self.rank = rank
+        self.horizon = horizon
+        self.hyper_dim = hyper_dim
+        self.eps = 1e-6
+        
+        self._b0 = nn.Linear(hyper_dim, state_dim)
+        self._u = nn.Linear(hyper_dim, rank * state_dim) # source tensor
+        self._v = nn.Linear(hyper_dim, rank * state_dim) # sink tensor
+        self._w = nn.Linear(hyper_dim, rank * act_dim) # action tensor
+        self._tau = nn.Linear(hyper_dim, 1)
+    
+    def __repr__(self):
+        s = "{}(state_dim={}, act_dim={}, rank={}, horizon={}, hyper_dim={})".format(
+            self.__class__.__name__, self.state_dim, self.act_dim, 
+            self.rank, self.horizon, self.hyper_dim
+        )
+        return s
+
+    @property
+    def transition(self):
+        """ Return transition matrix. size=[1, act_dim, state_dim, state_dim] """
+        z = torch.zeros(1, self.hyper_dim).to(self._b0.bias.device)
+        return self.compute_transition(z)
+    
+    @jit.script_method
+    def compute_transition(self, z):
+        u = self._u(z).view(-1, self.rank, self.state_dim)
+        v = self._v(z).view(-1, self.rank, self.state_dim)
+        w = self._w(z).view(-1, self.rank, self.act_dim)
+        core = torch.einsum("nri, nrj, nrk -> nkij", u, v, w)
+        return torch.softmax(core, dim=-1)
+
+    @jit.script_method
+    def compute_value(self, transition: Tensor, reward: Tensor) -> Tensor:
+        """ Compute expected value using value iteration
+
+        Args:
+            transition (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+            reward (torch.tensor): reward matrix. size=[batch_size, act_dim, state_dim]
+        
+        Returns:
+            q (torch.tensor): state q value. size=[horizon, batch_size, act_dim, state_dim]
+        """        
+        q = [torch.empty(0)] * (self.horizon)
+        q[0] = reward
+        for t in range(self.horizon - 1):
+            v_next = torch.logsumexp(q[t], dim=-2, keepdim=True)
+            q[t+1] = reward + torch.einsum("nkij, nkj -> nki", transition, v_next)
+        return torch.stack(q)
+    
+    @jit.script_method
+    def plan(self, b: Tensor, z: Tensor, value: Tensor) -> Tensor:
+        """ Compute the belief action distribution 
+        
+        Args:
+            b (torch.tensor): current belief. size=[batch_size, state_dim]
+            z (torch.tensor): hyper vector. size=[batch_size, hyper_dim]
+            value (torch.tensor): state q value. size=[horizon, batch_size, act_dim, state_dim]
+
+        Returns:
+            a (torch.tensor): action distribution. size=[batch_size, act_dim]
+        """
+        tau = torch.exp(self._tau(z).clip(math.log(1e-6), math.log(1e3)))
+        tau = poisson_pdf(tau, self.horizon)
+        
+        a = torch.softmax(torch.einsum("ni, hnki -> hnk", b, value), dim=-1)
+        a = torch.einsum("hnk, nh -> nk", a, tau)
+        return a
+
+    @jit.script_method
+    def update_belief(self, logp_o: Tensor, transition: Tensor, b: Tensor, a: Tensor) -> Tensor:
+        """ Compute state posterior
+        
+        Args:
+            logp_o (torch.tensor): log probability of current observation. size=[batch_size, state_dim]
+            transition (torch.tensor): transition matrix. size=[batch_size, act_dim, state_dim, state_dim]
+            b (torch.tensor): prior belief. size=[batch_size, state_dim]
+            a (torch.tensor): action posterior. size=[batch_size, act_dim]
+
+        Returns:
+            b_post (torch.tensor): state posterior. size=[batch_size, state_dim]
+        """
+        s_next = torch.einsum("nkij, ni, nk -> nj", transition, b, a)
+        logp_s = torch.log(s_next + self.eps)
+        b_post = torch.softmax(logp_s + logp_o, dim=-1)
+        return b_post
+    
+    @jit.script_method
+    def update_action(self, logp_u: Tensor, a: Tensor) -> Tensor:
+        """ Compute action posterior 
+        
+        Args:
+            logp_u (torch.tensor): log probability of previous control. size=[batch_size, act_dim]
+            a (torch.tensor): action prior. size=[batch_size, act_dim]
+
+        Returns:
+            a_post (torch.tensor): action posterior. size=[batch_size, act_dim]
+        """ 
+        logp_a = torch.log(a + self.eps)
+        a_post = torch.softmax(logp_a + logp_u, dim=-1)
+        return a_post
+    
+    @jit.script_method
+    def update_cell(
+        self, logp_o: Tensor, logp_u: Tensor, z: Tensor, b: Tensor, a: Tensor, 
+        transition: Tensor, value: Tensor
+        ) -> Tuple[Tensor, Tensor]:
+        """ Compute action and state posterior. Then compute the next action distribution """
+        a_post = self.update_action(logp_u, a)
+        b_post = self.update_belief(logp_o, transition, b, a_post)
+        a_next = self.plan(b_post, z, value)
+        return (b_post, a_next)
+    
+    @jit.script_method
+    def init_hidden(self, logp_o: Tensor, z: Tensor, value: Tensor) -> Tuple[Tensor, Tensor]:
+        b0 = torch.softmax(self._b0(z), dim=-1)
+        logp_s = torch.log(b0 + self.eps)
+        b = torch.softmax(logp_s + logp_o, dim=-1)
+        a = self.plan(b, z, value)
+        return b, a
+    
+    def forward(
+        self, logp_o: Tensor, logp_u: Union[Tensor, None], reward: Tensor, 
+        z: Tensor, b: Union[Tensor, None], a: Union[Tensor, None]
+        ) -> Tuple[Tensor, Tensor]:
+        """
+        Args:
+            logp_o (torch.tensor): sequence of observation probabilities. size=[T, batch_size, state_dim]
+            logp_u (torch.tensor): sequence of control probabilities. Should be offset -1 if b is None.
+                size=[T, batch_size, act_dim]
+            reward (torch.tensor): reward matrix. size=[batch_size, act_dim, state_dim]
+            z (torch.tensor): hyper vector, size=[batch_size, hyper_dim]
+            b ([torch.tensor, None], optional): prior belief. size=[batch_size, state_dim]
+            a ([torch.tensor, None], optional): prior action. size=[batch_size, act_dim]
+        
+        Returns:
+            alpha_b (torch.tensor): sequence of posterior belief. size=[T, batch_size, state_dim]
+            alpha_a (torch.tensor): sequence of action distribution. size=[T, batch_size, act_dim]
+        """
+        transition = self.compute_transition(z)
+        value = self.compute_value(transition, reward)
+        t_offset = 0 if b is not None else -1 # offset for offline prediction
+        T = len(logp_o)
+        
+        alpha_b = [b] + [torch.empty(0)] * (T)
+        alpha_a = [a] + [torch.empty(0)] * (T)
+        for t in range(T):
+            if alpha_b[t] is None:
+                alpha_b[t+1], alpha_a[t+1] = self.init_hidden(logp_o[t], z, value)
+            else:
+                alpha_b[t+1], alpha_a[t+1] = self.update_cell(
+                    logp_o[t], logp_u[t+t_offset], z,
+                    alpha_b[t], alpha_a[t], transition, value
+                )
+        return torch.stack(alpha_b[1:]), torch.stack(alpha_a[1:])
