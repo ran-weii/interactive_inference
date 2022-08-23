1a176d5dab13f09610536f9175295c309a8d71b9
Wei Ran <rw422@tamu.edu>
2022-08-23 15:58:01 -0500

    decouple state and action in dynamics model

--- src/simulation/dynamics.py
+++ src/simulation/dynamics.py
@@ -3,20 +3,42 @@ import numpy as np
 class ConstantAcceleration:
     def __init__(self, dt=0.1):
         """
-        Constant acceleration dynamics model with state variables
-            [x, y, xv, xy, ax, ay]
+        Constant acceleration dynamics model with state and action variables
+            [x, y, xv, xy], [ax, ay]
         """
         self.dt = dt
 
-        self.A = np.array(
-            [[1, 0, dt,  0,  0.5 * dt**2,  0],
-            [0, 1,  0, dt,  0,  0.5 * dt**2],
-            [0, 0,  1,  0, dt,  0],
-            [0, 0,  0,  1,  0, dt],
-            [0, 0,  0,  0,  1,  0],
-            [0, 0,  0,  0,  0,  1]]
-        ) 
+        # self.A = np.array(
+        #     [[1, 0, dt,  0,  0.5 * dt**2,  0],
+        #     [0, 1,  0, dt,  0,  0.5 * dt**2],
+        #     [0, 0,  1,  0, dt,  0],
+        #     [0, 0,  0,  1,  0, dt],
+        #     [0, 0,  0,  0,  1,  0],
+        #     [0, 0,  0,  0,  0,  1]]
+        # ) 
 
-    def step(self, state):
-        next_state = np.matmul(self.A, state)
+        self.A = np.array(
+            [[1, 0, dt,  0],
+            [0, 1,  0, dt],
+            [0, 0,  1,  0],
+            [0, 0,  0,  1]]
+        ) 
+        
+        self.B = np.array(
+            [[0.5 * dt**2, 0],
+            [0, 0.5 * dt**2],
+            [dt, 0],
+            [0, dt]]
+        )
+    def step(self, state, action):
+        """
+        Args:
+            state (np.array): size=[batch_size, 4]
+            action (np.array): size=[batch_size, 2]
+
+        Returns:
+            next_state (np.array): size=[batch_size, 4]
+        """
+        # next_state = np.matmul(self.A, state)
+        next_state = np.matmul(self.A, state) + np.matmul(self.B, action)
         return next_state
