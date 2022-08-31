ff4e47976d14bfb05c8dbf2dd3d97cb30eb2baed
HFML <rw422@tamu.edu>
2022-08-31 13:46:20 -0500

    update mask creation in collate_fn

--- src/data/ego_dataset.py
+++ src/data/ego_dataset.py
@@ -13,7 +13,7 @@ def collate_fn(batch):
     assert isinstance(batch[0], dict)
     keys = list(batch[0].keys())
     pad_batch = {k: pad_sequence([b[k] for b in batch]) for k in keys}
-    mask = torch.all(pad_batch[keys[0]] != 0, dim=-1).to(torch.float32)
+    mask = pad_sequence([torch.ones(len(b[keys[0]])) for b in batch])
     return pad_batch, mask
     
 
