a9f6ac794782040ee00792b3a2d7fa254f3ccb7c
HFML <rw422@tamu.edu>
2022-08-31 13:56:48 -0500

    make full sensor_obs input to reward model

--- src/simulation/reward.py
+++ src/simulation/reward.py
@@ -6,22 +6,26 @@ class CarfollowingReward:
     1 relative distance: 0 * (s_rel >= min_s_rel) - s_rel_penalty * (s_rel < min_s_rel)
     2 looming: -(loom - loom_target)^2, loom_target = 0 
     """
-    def __init__(self, feature_names):
-        self.feature_names = feature_names
-        self.idx_d = feature_names.index("ego_d")
-        self.idx_s_rel = feature_names.index("lv_s_rel")
-        self.idx_loom = feature_names.index("lv_inv_tau")
+    def __init__(self, sensors):
+        sensor_names = [s.__class__.__name__ for s in sensors]
+        ego_sensor_idx = sensor_names.index("EgoSensor")
+        lv_sensor_idx = sensor_names.index("LeadVehicleSensor")
+
+        self.idx_d = sensors[ego_sensor_idx].feature_names.index("ego_d")
+        self.idx_s_rel = sensors[lv_sensor_idx].feature_names.index("lv_s_rel")
+        self.idx_loom = sensors[lv_sensor_idx].feature_names.index("lv_inv_tau")
 
         self.d_target = 0.
         self.min_s_rel = 3.
         self.s_rel_penalty = 10.
         self.loom_target = 0.
 
-    def __call__(self, obs, ctl):
-        obs = obs.flatten().clone()
-        d = obs[self.idx_d]
-        s_rel = obs[self.idx_s_rel]
-        loom_s = obs[self.idx_loom]
+    def __call__(self, sensor_obs, ctl):
+        ego_obs = sensor_obs["EgoSensor"]
+        lv_obs = sensor_obs["LeadVehicleSensor"]
+        d = ego_obs[self.idx_d]
+        s_rel = lv_obs[self.idx_s_rel]
+        loom_s = lv_obs[self.idx_loom]
 
         f1 = -(d - self.d_target) ** 2
         f2 = (s_rel >= self.min_s_rel) * 0 - self.s_rel_penalty * (s_rel < self.min_s_rel)
