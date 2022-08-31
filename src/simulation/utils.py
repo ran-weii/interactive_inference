67289dd9fb3b14046a6bd81a6c3446cb3ecfab25
HFML <rw422@tamu.edu>
2022-08-31 13:51:15 -0500

    add eps_ids to svt

--- src/simulation/utils.py
+++ src/simulation/utils.py
@@ -3,17 +3,20 @@ import pandas as pd
 from tqdm import tqdm
 
 class VehicleTrajectoreis:
-    def __init__(self, svt, track_ids, ego_track_ids, t_range):
+    def __init__(self, svt, track_ids, ego_track_ids, eps_ids, t_range):
         """
         Properties:
             svt (list): stack vehicle states for each frame. Each item in the list 
                 consists of all vehicles in the frame. size=[num_frames]
             track_ids (list): vehicle track ids in each frame. size=[num_frames]
-            t_range (list): start and end frame index of each vehicle. size=[num_tracks, 2]
+            ego_track_ids (np.array): track id of each ego vehicle. size=[num_eps]
+            eps_ids (np.array): episode id of each trajectory. size=[num_eps]
+            t_range (list): start and end frame index of each vehicle. size=[num_eps, 2]
         """
         self.svt = svt
         self.track_ids = track_ids
         self.ego_track_ids = ego_track_ids
+        self.eps_ids = eps_ids
         self.t_range = t_range
 
 
@@ -47,7 +50,8 @@ def create_svt_from_df(df, eps_id_col="track_id"):
     df_eps = df_eps_head.merge(df_eps_tail, on="eps_id")
     df_eps = df_eps.loc[df_eps["eps_id"].isna() == False].sort_values(by="eps_id").reset_index(drop=True)
     ego_track_ids = df_eps["track_id_x"].values.astype(int)
+    eps_ids = df_eps["eps_id"].values.astype(int)
     t_range = df_eps[["frame_id_x", "frame_id_y"]].values.astype(int)
     
-    svt_object = VehicleTrajectoreis(svt, track_ids, ego_track_ids, t_range)
+    svt_object = VehicleTrajectoreis(svt, track_ids, ego_track_ids, eps_ids, t_range)
     return svt_object
