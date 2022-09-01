a1d4a20f86ef1eca367cefa15cb4099607c52dc4
HFML <rw422@tamu.edu>
2022-09-01 08:33:00 -0500

    update feature feeding with sensors

--- scripts/train_agent_offline.py
+++ scripts/train_agent_offline.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import json
+import pickle
 import datetime
 import numpy as np
 import pandas as pd
@@ -8,18 +9,20 @@ import matplotlib.pyplot as plt
 import torch 
 
 # setup imports
-from src.simulation.observers import ACTION_SET, FEATURE_SET
+from src.simulation.observers import ACTION_SET, FEATURE_SET, Observer
 from src.data.train_utils import load_data, train_test_split, count_parameters
+from src.data.data_filter import filter_segment_by_length
 from src.data.ego_dataset import RelativeDataset, aug_flip_lr, collate_fn
+from src.simulation.sensors import EgoSensor, LeadVehicleSensor, LidarSensor
 
 # model imports
-from src.agents.vin_agent import VINAgent
+from src.agents.vin_agent import VINAgent, VINAgent2
 from src.agents.hyper_vin_agent import HyperVINAgent
-from src.agents.tensor_vin_agent import TensorVINAgent
 from src.agents.rule_based import IDM
 from src.agents.mlp_agents import MLPAgent
 from src.algo.irl import BehaviorCloning, HyperBehaviorCloning
 from src.algo.irl import ReverseBehaviorCloning
+from src.algo.dagger import Dagger
 
 # training imports
 from src.algo.utils import train
@@ -30,6 +33,7 @@ warnings.filterwarnings("ignore")
 
 def parse_args():
     bool_ = lambda x: x if isinstance(x, bool) else x == "True"
+    str_list_ = lambda x: x.split(",")
     
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -37,21 +41,26 @@ def parse_args():
     )
     parser.add_argument("--exp_path", type=str, default="../exp")
     parser.add_argument("--scenario", type=str, default="DR_CHN_Merging_ZS")
-    parser.add_argument("--filename", type=str, default="vehicle_tracks_007.csv")
+    parser.add_argument("--filenames", type=str_list_, default=["vehicle_tracks_007.csv"])
     parser.add_argument("--checkpoint_path", type=str, default="none", 
         help="if entered train agent from check point")
+    parser.add_argument("--expert_exp_name", type=str, default="none", 
+        help="expert exp path for dagger. Assume mlp expert")
     # agent args
-    parser.add_argument("--agent", type=str, choices=["vin", "hvin", "tvin", "idm", "mlp"], default="vin", help="agent type, default=vin")
+    parser.add_argument("--agent", type=str, choices=["vin", "hvin", "vin", "idm", "mlp"], default="vin", help="agent type, default=vin")
     parser.add_argument("--state_dim", type=int, default=30, help="agent state dimension, default=30")
     parser.add_argument("--act_dim", type=int, default=45, help="agent action dimension, default=45")
     parser.add_argument("--horizon", type=int, default=30, help="agent planning horizon, default=30")
     parser.add_argument("--obs_cov", type=str, default="full", help="agent observation covariance, default=full")
     parser.add_argument("--ctl_cov", type=str, default="full", help="agent control covariance, default=full")
     parser.add_argument("--hmm_rank", type=int, default=32, help="agent hmm rank, default=32")
+    parser.add_argument("--alpha", type=float, default=1., help="agent entropy reward coefficient, default=1.")
     parser.add_argument("--action_set", type=str, choices=["ego", "frenet", "disc"], default="frenet", help="agent action set, default=frenet")
-    parser.add_argument("--discrete_action", type=bool_, default=False)
     parser.add_argument("--use_tanh", type=bool_, default=True, help="whether to use tanh transform, default=True")
+    parser.add_argument("--use_lidar", type=bool_, default=False, help="whether to use lidar features, default=False")
     parser.add_argument("--hyper_dim", type=int, default=4, help="number of latent factor, default=4")
+    parser.add_argument("--detach", type=bool_, default=True, help="whether to detach dynamics model, default=True")
+    parser.add_argument("--causal", type=bool_, default=True)
     # nn args
     parser.add_argument("--hidden_dim", type=int, default=64, help="nn hidden dimension, default=64")
     parser.add_argument("--num_hidden", type=int, default=2, help="number of hidden layers, default=2")
@@ -60,13 +69,14 @@ def parse_args():
     parser.add_argument("--norm_obs", type=bool_, default=False, help="whether to normalize observation for discriminator, default=False")
     parser.add_argument("--use_state", type=bool_, default=False, help="whether to use state for discriminator, default=False")
     # algo args
-    parser.add_argument("--algo", type=str, choices=["bc", "hbc", "rbc"], default="bc", help="training algorithm, default=bc")
+    parser.add_argument("--algo", type=str, choices=["bc", "hbc", "rbc", "dag"], default="bc", help="training algorithm, default=bc")
     parser.add_argument("--d_batch_size", type=int, default=200, help="discriminator batch size, default=200")
     parser.add_argument("--bptt_steps", type=int, default=30, help="bptt truncation steps, default=30")
     parser.add_argument("--d_steps", type=int, default=50, help="discriminator steps, default=50")
     parser.add_argument("--grad_penalty", type=float, default=1., help="discriminator gradient penalty, default=1.")
     parser.add_argument("--bc_penalty", type=float, default=0., help="behavior cloning penalty, default=0.")
     parser.add_argument("--obs_penalty", type=float, default=0., help="observation penalty, default=0.")
+    parser.add_argument("--cf_penalty", type=float, default=1., help="counterfactual action penalty for dagger. default=1.")
     # training args
     parser.add_argument("--min_eps_len", type=int, default=50, help="min track length, default=50")
     parser.add_argument("--max_eps_len", type=int, default=200, help="max track length, default=200")
@@ -118,6 +128,14 @@ class SaveCallback:
         
         # save history
         df_history = pd.DataFrame(history)
+        self.save_history(model, df_history)
+        
+        # save model
+        cpu_state_dict = {k: v.cpu() for k, v in model.state_dict().items()}
+        torch.save(cpu_state_dict, os.path.join(self.model_path, f"model_{self.iter}.pt"))
+        print(f"\ncheckpoint saved at: {self.save_path}\n")
+    
+    def save_history(self, model, df_history):
         if self.cp_history is not None:
             df_history["epoch"] += self.cp_history["epoch"].values[-1] + 1
             df_history["time"] += self.cp_history["time"].values[-1]
@@ -127,32 +145,42 @@ class SaveCallback:
         # save history plot
         fig_history, _ = plot_history(df_history, model.loss_keys)
         fig_history.savefig(os.path.join(self.save_path, "history.png"), dpi=100)
-        
+
         plt.clf()
         plt.close()
-        
-        # save model
-        cpu_state_dict = {k: v.cpu() for k, v in model.state_dict().items()}
-        torch.save(cpu_state_dict, os.path.join(self.model_path, f"model_{self.iter}.pt"))
-        print(f"\ncheckpoint saved at: {self.save_path}\n")
-    
+
     def save_model(self, model):
         cpu_state_dict = {k: v.cpu() for k, v in model.state_dict().items()}
         torch.save(cpu_state_dict, os.path.join(self.save_path, "model.pt"))
 
+def get_new_eps_id(df_track):
+    """ Combine record_id and eps_id """
+    df_track["record_id"] = df_track["record_id"].astype(int)
+    num_digits = len(str(np.nanmax(df_track["eps_id"]).astype(int)))
+    new_eps_id = df_track["record_id"] * 10**num_digits + df_track["eps_id"]
+    return new_eps_id
+
 def main(arglist):
     torch.manual_seed(arglist.seed)
     
-    df_track = load_data(arglist.data_path, arglist.scenario, arglist.filename)
-    df_track = df_track.loc[df_track["is_train"] == 1]
-    
-    """ TODO: add code to adapt input feature set """
-    # define feature set
-    ego_features = ["d", "ds", "dd", "kappa_r", "psi_error_r", ]
-    relative_features = ["s_rel", "d_rel", "ds_rel", "dd_rel", "loom_s"]
-    feature_set = ego_features + relative_features
-    assert set(ego_features).issubset(set(FEATURE_SET["ego"]))
-    assert set(relative_features).issubset(set(FEATURE_SET["relative"]))
+    # load data files
+    df_track = []
+    for filename in arglist.filenames:
+        df_track.append(load_data(arglist.data_path, arglist.scenario, filename))
+    df_track = pd.concat(df_track, axis=0)
+    df_track = df_track.assign(eps_id=get_new_eps_id(df_track))
+     
+    # """ debug car following only """
+    # df_track.loc[df_track["is_head_merging"] == 1] = np.nan
+    # """ debug car following only """
+
+    # filter episode length
+    eps_id, df_track["eps_len"] = filter_segment_by_length(
+        df_track["eps_id"].values, arglist.min_eps_len
+    )
+    df_track = df_track.assign(eps_id=eps_id.astype(float))    
+
+    df_track = df_track.loc[(df_track["is_train"] == 1) & (df_track["eps_id"] != np.nan)]
     
     # define action set
     if arglist.action_set == "frenet":
@@ -160,11 +188,22 @@ def main(arglist):
     elif arglist.action_set == "ego":
         action_set = ["ax_ego", "ay_ego"]
     
-    action_bins = None
-    if arglist.discrete_action:
-        action_bins = arglist.act_dim
+    # define feature set
+    ego_sensor = EgoSensor(None)
+    lv_sensor = LeadVehicleSensor(None)
+    lidar_sensor = LidarSensor()
+    sensors = [ego_sensor, lv_sensor]
+    if arglist.use_lidar:
+        sensors.append(lidar_sensor)
 
-    assert set(action_set).issubset(set(ACTION_SET))
+    observer = Observer(None, sensors, action_set)
+    feature_set = observer.feature_names
+
+    # """ debug car following only """
+    # arglist.car_follow_only = True
+    # action_set = ["dds"]
+    # feature_set = ["ego_ds"] + lv_sensor.feature_names[:3]
+    # """ debug car follow only """
     
     # compute obs and ctl mean and variance stats
     obs_mean = torch.from_numpy(df_track.loc[df_track["is_train"] == 1][feature_set].mean().values).to(torch.float32)
@@ -177,30 +216,36 @@ def main(arglist):
     ctl_lim = torch.max(torch.abs(ctl_max), torch.abs(ctl_min)) * 1.2
     
     dataset = RelativeDataset(
-        df_track, feature_set, action_set, action_bins=action_bins, train_labels_col="is_train",
-        max_eps_len=arglist.max_eps_len, augmentation=[aug_flip_lr], seed=arglist.seed
+        df_track, feature_set, action_set, train_labels_col="is_train",
+        max_eps_len=arglist.max_eps_len, augmentation=[], max_eps=10000, seed=arglist.seed
     )
     train_loader, test_loader = train_test_split(
         dataset, arglist.train_ratio, arglist.batch_size, 
         collate_fn=collate_fn, seed=arglist.seed
     )
-    obs_dim, ctl_dim = len(feature_set), 2 
+    obs_dim, ctl_dim = len(feature_set), len(action_set)
 
     print(f"feature set: {feature_set}")
     print(f"action set: {action_set}")
     print(f"train size: {len(train_loader.dataset)}, test size: {len(test_loader.dataset)}")
-
+    
     # init agent
     if arglist.agent == "vin":
-        agent = VINAgent(
-            arglist.state_dim, arglist.act_dim, obs_dim, ctl_dim, arglist.hmm_rank,
-            arglist.horizon, obs_cov=arglist.obs_cov, ctl_cov=arglist.ctl_cov, 
-            use_tanh=arglist.use_tanh, ctl_lim=ctl_lim
-        )
+        if arglist.causal:
+            agent = VINAgent(
+                arglist.state_dim, arglist.act_dim, obs_dim, ctl_dim, arglist.hmm_rank,
+                arglist.horizon, alpha=arglist.alpha, obs_cov=arglist.obs_cov, ctl_cov=arglist.ctl_cov, 
+                use_tanh=arglist.use_tanh, ctl_lim=ctl_lim, detach=arglist.detach
+            )
+        else:
+            agent = VINAgent2(
+                arglist.state_dim, arglist.act_dim, obs_dim, ctl_dim, arglist.hmm_rank,
+                arglist.horizon, alpha=arglist.alpha, obs_cov=arglist.obs_cov, ctl_cov=arglist.ctl_cov, 
+                use_tanh=arglist.use_tanh, ctl_lim=ctl_lim, detach=arglist.detach
+            )
         agent.obs_model.init_batch_norm(obs_mean, obs_var)
         if not arglist.use_tanh:
             agent.ctl_model.init_batch_norm(ctl_mean, ctl_var)
-
     elif arglist.agent == "hvin":
         agent = HyperVINAgent(
             arglist.state_dim, arglist.act_dim, obs_dim, ctl_dim, arglist.hmm_rank,
@@ -212,17 +257,8 @@ def main(arglist):
         agent.obs_model.init_batch_norm(obs_mean, obs_var)
         if not arglist.use_tanh:
             agent.ctl_model.init_batch_norm(ctl_mean, ctl_var)
-    
-    elif arglist.agent == "tvin":
-        agent = TensorVINAgent(
-            arglist.state_dim, arglist.act_dim, obs_dim, ctl_dim, arglist.hmm_rank,
-            arglist.horizon, obs_cov=arglist.obs_cov
-        )
-        agent.obs_model.init_batch_norm(obs_mean, obs_var)
-
     elif arglist.agent == "idm":
         agent = IDM(std=ctl_var)
-
     elif arglist.agent == "mlp":
         agent = MLPAgent(
             obs_dim, ctl_dim, arglist.hidden_dim, arglist.num_hidden, 
@@ -253,7 +289,33 @@ def main(arglist):
             model.obs_mean.data = obs_mean
             model.obs_variance.data = obs_var
         model.fill_buffer(train_loader.dataset)
+    elif arglist.algo == "dag":
+        # load expert
+        assert arglist.expert_exp_name is not "none"
+        expert_path = os.path.join(arglist.exp_path, "agents", "mlp", arglist.expert_exp_name)
         
+        with open(os.path.join(expert_path, "args.json"), "rb") as f:
+            expert_config = json.load(f)
+        
+        with open(os.path.join(expert_path, "buffer.p"), "rb") as f:
+            buffer = pickle.load(f)
+        
+        expert_state_dict = torch.load(os.path.join(expert_path, "model.pt"), map_location=torch.device("cpu"))
+        expert_state_dict = {k.replace("agent.", ""): v for (k, v) in expert_state_dict.items() if "agent." in k}
+        
+        expert = MLPAgent(
+            obs_dim, ctl_dim, expert_config["hidden_dim"], expert_config["num_hidden"],
+            expert_config["activation"], use_tanh=expert_config["use_tanh"], 
+            ctl_limits=ctl_lim, norm_obs=expert_config["norm_obs"]
+        )
+        expert.load_state_dict(expert_state_dict, strict=True)
+
+        expert = IDM(feature_set)
+        
+        model = Dagger(
+            agent, expert, buffer, arglist.bptt_steps, obs_penalty=arglist.obs_penalty,
+            cf_penalty=arglist.cf_penalty, lr=arglist.lr, decay=arglist.decay, grad_clip=arglist.grad_clip
+        )
     print(f"num parameters: {count_parameters(model)}")
     print(model)
 
@@ -273,11 +335,16 @@ def main(arglist):
         model.load_state_dict(state_dict)
         print(f"loaded checkpoint from {cp_path}")
     
-    callback = SaveCallback(arglist, cp_history)
+    callback = None
+    if arglist.save:
+        callback = SaveCallback(arglist, cp_history)
+
     model, df_history = train(
         model, train_loader, test_loader, arglist.epochs, callback=callback, verbose=1
     )
-    callback.save_model(model)
+    if arglist.save:
+        callback.save_model(model)
+        callback.save_history(model, df_history)
     
 if __name__ == "__main__":
     arglist = parse_args()
