bug fix rule_based choose_action_batch masking, add hyper_vin test case

src/agents/rule_based.py
@@ -120,9 +120,8 @@ class IDM(AbstractAgent):
 
     def act_loss(self, o, u, mask, forward_out):
         mu, lv = forward_out
-        mu *= mask.unsqueeze(-1)
         
-        logp_u = torch_dist.Normal(mu, rectify(lv)).log_prob(u).sum(-1)
+        logp_u = torch_dist.Normal(mu * mask.unsqueeze(-1), rectify(lv)).log_prob(u).sum(-1)
         
         loss = -torch.sum(logp_u * mask, dim=0) / (mask.sum(0) + 1e-6)
         

tests/test_agents.py
@@ -5,7 +5,9 @@ from src.distributions.nn_models import PopArt
 from src.agents.rule_based import IDM
 from src.agents.nn_agents import MLPAgent, RNNAgent
 from src.agents.vin_agent import VINAgent
+from src.agents.hyper_vin_agent import HyperVINAgent
 from src.algo.bc import BehaviorCloning, RecurrentBehaviorCloning
+from src.algo.hyper_bc import HyperBehaviorCloning
 
 def test_popart_layer():
     state_dim = 10
@@ -68,7 +70,7 @@ def test_idm_agent():
 
     pi_t = torch.stack(pi_t)
     u_sample_t = torch.stack(u_sample_t).squeeze(1)
-    
+
     assert list(pi_t.shape) == [T, batch_size, ctl_dim]
     assert list(u_sample_t.shape) == [T, batch_size, ctl_dim]
     assert torch.all(torch.abs(pi_t - out[0]) < 1e-5)    
@@ -269,6 +271,75 @@ def test_vin_agent():
 
     print("test_vin_agent passed")
 
+def test_hvin_agent():
+    obs_dim = 10
+    state_dim = 12
+    act_dim = 5
+    ctl_dim = 3
+    rank = 0
+    horizon = 15
+    hyper_dim = 4
+    hidden_dim = 32
+    num_hidden = 1
+    gru_layers = 1
+    activation = "relu"
+    
+    agent = HyperVINAgent(
+        state_dim, act_dim, obs_dim, ctl_dim, rank, horizon,
+        hyper_dim, hidden_dim, num_hidden, gru_layers, activation, obs_model="gmm"
+    )
+    agent.eval()
+
+    # generate synthetic data
+    T = 7
+    batch_size = 32
+    o = torch.randn(T, batch_size, obs_dim)
+    u = torch.randn(T, batch_size, ctl_dim)
+    seq_len = torch.randint(1, T, (batch_size - 1,))
+    seq_len = torch.cat([seq_len, T * torch.ones(1)]).long()
+    mask = pad_sequence([torch.ones(s) for s in seq_len])
+    
+    # test offline
+    with torch.no_grad():
+        z = agent.encode(o, u, mask)
+        [b, pi, value], hidden = agent.forward(o, u, z)
+        act_loss, stats = agent.act_loss(o, u, z, mask, hidden)
+        u_sample, logp = agent.choose_action_batch(o, u, z)
+
+    assert list(pi.shape) == [T, batch_size, act_dim]
+    assert list(act_loss.shape) == [batch_size]
+    assert list(u_sample.shape) == [1, T, batch_size, ctl_dim]
+    assert list(logp.shape) == [1, T, batch_size]
+
+    # test online
+    agent.reset(z)
+    b_t = []
+    pi_t = []
+    u_sample_t = []
+    for t in range(T):
+        with torch.no_grad():
+            u_sample_t.append(agent.choose_action(o[t])[0])
+            b_t.append(agent._state["b"])
+            pi_t.append(agent._state["pi"])
+            agent._prev_ctl = u[t].unsqueeze(0)
+    
+    b_t = torch.stack(b_t)
+    pi_t = torch.stack(pi_t)
+    u_sample_t = torch.stack(u_sample_t).squeeze(1)
+    
+    assert list(b_t.shape) == [T, batch_size, state_dim]
+    assert list(pi_t.shape) == [T, batch_size, act_dim]
+    assert list(u_sample_t.shape) == [T, batch_size, ctl_dim]
+    assert torch.abs(b_t - b).mean() < 1e-3
+    assert torch.abs(pi_t - pi).mean() < 1e-3
+    
+    # test training
+    loader = [[{"obs": o, "act": u}, mask]]
+    trainer = HyperBehaviorCloning(agent, train_mode="prior", bptt_steps=5)
+    trainer.run_epoch(loader)
+
+    print("test_vin_agent passed")
+
 if __name__ == "__main__":
     torch.manual_seed(0)
     
@@ -276,3 +347,4 @@ if __name__ == "__main__":
     test_mlp_agent()
     test_rnn_agent()
     test_vin_agent()
+    test_hvin_agent()